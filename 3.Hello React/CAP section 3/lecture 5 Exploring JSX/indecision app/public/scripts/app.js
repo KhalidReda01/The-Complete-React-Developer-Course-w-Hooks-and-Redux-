e9b602c98fc2515865975361bc9c4e3054ffcfab
khalidreda <khalidaziz2198@gmail.com>
2022-02-06 04:03:29 +0200

    Challenge 01 solved

--- 3.Hello React/CAP section 3/lecture 5 Exploring JSX/indecision app/public/scripts/app.js
+++ 3.Hello React/CAP section 3/lecture 5 Exploring JSX/indecision app/public/scripts/app.js
@@ -36,7 +36,35 @@ var template = React.createElement(
 );
 // single route element so var template = <h1 id="someid">indecision app </h1><p>This is some info</p> is wrong
 // the right way is put them in a div container wrapper div
+/**
+ * Lecture Notes for challenge time 01
+ * Create your own template
+ * Create a templateTWo var JSX expression 
+ * root div  inside h1 -> Your name Khalid Reda 
+ * p -> Age: your age 23
+ * p -> Location:Mansura
+ * Render templateTwo instead of template 
+ */
 
-
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Khalid Reda'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age:23'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location:Mansura'
+  )
+);
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+// ReactDOM.render(template, appRoot)
+ReactDOM.render(templateTwo, appRoot);
--- 3.Hello React/CAP section 3/lecture 5 Exploring JSX/indecision app/src/app.js
+++ 3.Hello React/CAP section 3/lecture 5 Exploring JSX/indecision app/src/app.js
@@ -16,8 +16,24 @@ var template =(
 );
 // single route element so var template = <h1 id="someid">indecision app </h1><p>This is some info</p> is wrong
 // the right way is put them in a div container wrapper div
+/**
+ * Lecture Notes for challenge time 01
+ * Create your own template
+ * Create a templateTWo var JSX expression 
+ * root div  inside h1 -> Your name Khalid Reda 
+ * p -> Age: your age 23
+ * p -> Location:Mansura
+ * Render templateTwo instead of template 
+ */
 
-
+var templateTwo = (
+  <div>
+    <h1>Khalid Reda</h1>
+    <p>Age:23</p>
+    <p>Location:Mansura</p>
+  </div>
+)
 var appRoot = document.getElementById('app')
-ReactDOM.render(template, appRoot)
+// ReactDOM.render(template, appRoot)
+ReactDOM.render(templateTwo, appRoot)
 
