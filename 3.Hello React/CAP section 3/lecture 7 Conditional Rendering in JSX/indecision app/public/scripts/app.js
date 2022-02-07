9fdf52004ee47dbb6d9db63518078f11cd34f063
khalidreda <khalidaziz2198@gmail.com>
2022-02-07 09:11:25 +0200

    Conditional Rendering in JSX if statements

---
+++ 3.Hello React/CAP section 3/lecture 7 Conditional Rendering in JSX/indecision app/public/scripts/app.js
@@ -0,0 +1,81 @@
+"use strict";
+
+console.log('App.js is running! ');
+
+// JSX - JavaScript XML
+// 7.Conditional Rendering in JSX
+// if statements done
+// ternary operators
+// logical and operator 
+
+var appObject = {
+  title: "Indecision App",
+  subtitle: "Put your life in the hands of a computer"
+};
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    appObject.title,
+    " "
+  ),
+  React.createElement(
+    "p",
+    null,
+    appObject.subtitle
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item two"
+    )
+  )
+);
+
+var user = {
+  name: 'Khalid Reda',
+  age: 23
+  // location:'Cairo'
+};
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      "p",
+      null,
+      "Location: ",
+      location
+    );
+  }
+  // else {
+  //   return 'undefined'
+  // }
+}
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    user.name
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age:",
+    user.age
+  ),
+  getLocation(user.location)
+);
+
+var appRoot = document.getElementById('app');
+ReactDOM.render(templateTwo, appRoot);
---
+++ 3.Hello React/CAP section 3/lecture 7 Conditional Rendering in JSX/indecision app/src/app.js
@@ -0,0 +1,50 @@
+console.log('App.js is running! ')
+
+// JSX - JavaScript XML
+// 7.Conditional Rendering in JSX
+// if statements done
+// ternary operators
+// logical and operator 
+
+var appObject = {
+  title: "Indecision App",
+  subtitle:"Put your life in the hands of a computer"
+}
+var template =(
+<div>
+  <h1>{appObject.title} </h1>
+    <p>{appObject.subtitle }</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+var user = {
+  name: 'Khalid Reda',
+  age: 23,
+  // location:'Cairo'
+}
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location }</p>;
+
+  }
+  // else {
+  //   return 'undefined'
+  // }
+  
+}
+var templateTwo = (
+  <div>
+    <h1>{user.name}</h1>
+    <p>Age:{user.age}</p>
+    {/* <p>Location:{getLocation(user.location)}</p> */}
+    {getLocation(user.location)}
+  </div>
+)
+
+var appRoot = document.getElementById('app')
+ReactDOM.render(templateTwo, appRoot)
+
