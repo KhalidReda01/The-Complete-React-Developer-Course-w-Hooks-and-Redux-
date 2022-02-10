2f275b09c400f9ff3b622f527bc5da1768f0c242
khalidreda <khalidaziz2198@gmail.com>
2022-02-10 14:47:02 +0200

    21/16 Build it Visibility Toggle CFSC2

--- 00/#CSMM/3.Hello React/lecture 16  section-end  Build it Visibility Toggle/indecision app/public/scripts/app.js
+++ 00/#CSMM/3.Hello React/lecture 16  section-end  Build it Visibility Toggle/indecision app/public/scripts/app.js
@@ -1,76 +1,32 @@
-'use strict';
+"use strict";
 
-/**
- * 21/16 Build It Visibilty Toggle
- * create h1 tag visibility Toggle
- * create a show details button
- */
-
-// that's what I'm taking about practice practice practice  is the solution not just watching and code along and practice
-// const showDetails = () => {
-//   let getButton = document.querySelector('.onclicked')
-//   let getDetails = document.querySelector('.btn-details')
-//   console.log(getDetails.innerHTM==="Show details")
-//   if (getDetails.innerHTM==="Show details") {
-//     getButton.innerHTML = "test"
-//   getDetails.innerHTML="Show details"
-//   } else if(getDetails.innerHTM==="Hide details") {
-//     getButton.innerHTML = "<p>Hey. These are some details you can now see!</p>"
-//     getDetails.innerHTML="Hide details"
-//   }
-
-
-//   render()
-// }
-// const hideDetails=() => {
-//   document.querySelector('.onclicked').innerHTML = "<p>Hey. These are some details you can now see!</p>"
-//   document.querySelector('.btn-details').innerHTML="Hide details"
-// }
-// const appRoot = document.getElementById('app')
-
-// const render = () => {
-//   const template = (<div>
-//     <h1>Visibilty Toggle</h1>
-//     <button className='btn-details' onClick={showDetails }>{"Show details"}</button>
-//     {<p className='onclicked'></p>
-// }
-
-// </div>
-
-//   );
-//   ReactDOM.render(template, appRoot)
-// }
-
-
-// render()
-// This idea is the closer to my mind actually
-// Andrea Solution
-var visibility = false;
-var toggleVisibility = function toggleVisibility() {
-  visibility = !visibility;
+var visibilty = false;
+var toggleVisibilty = function toggleVisibilty() {
+  visibilty = !visibilty;
+  console.log(visibilty, !visibilty);
   render();
 };
 var render = function render() {
   var jsx = React.createElement(
-    'div',
+    "div",
     null,
     React.createElement(
-      'h1',
+      "h1",
       null,
-      'Visibilty Toggle'
+      "visibilty Toggle"
     ),
     React.createElement(
-      'button',
-      { onClick: toggleVisibility },
-      visibility ? 'Hide detail' : 'Show details'
+      "button",
+      { onClick: toggleVisibilty },
+      visibilty ? "Hide detail" : "Show details"
     ),
-    visibility && React.createElement(
-      'div',
+    visibilty && React.createElement(
+      "div",
       null,
       React.createElement(
-        'p',
+        "p",
         null,
-        'Hey . These are some details you can now see!'
+        "Hey . These are some details you can now see!"
       )
     )
   );
--- 00/#CSMM/3.Hello React/lecture 16  section-end  Build it Visibility Toggle/indecision app/src/playground/build-it-visible.js
+++ 00/#CSMM/3.Hello React/lecture 16  section-end  Build it Visibility Toggle/indecision app/src/playground/build-it-visible.js
@@ -1,61 +1,17 @@
-/**
- * 21/16 Build It Visibilty Toggle
- * create h1 tag visibility Toggle
- * create a show details button
- */
-
-// that's what I'm taking about practice practice practice  is the solution not just watching and code along and practice
-// const showDetails = () => {
-//   let getButton = document.querySelector('.onclicked')
-//   let getDetails = document.querySelector('.btn-details')
-//   console.log(getDetails.innerHTM==="Show details")
-//   if (getDetails.innerHTM==="Show details") {
-//     getButton.innerHTML = "test"
-//   getDetails.innerHTML="Show details"
-//   } else if(getDetails.innerHTM==="Hide details") {
-//     getButton.innerHTML = "<p>Hey. These are some details you can now see!</p>"
-//     getDetails.innerHTML="Hide details"
-//   }
-  
-
-//   render()
-// }
-// const hideDetails=() => {
-//   document.querySelector('.onclicked').innerHTML = "<p>Hey. These are some details you can now see!</p>"
-//   document.querySelector('.btn-details').innerHTML="Hide details"
-// }
-// const appRoot = document.getElementById('app')
-
-// const render = () => {
-//   const template = (<div>
-//     <h1>Visibilty Toggle</h1>
-//     <button className='btn-details' onClick={showDetails }>{"Show details"}</button>
-//     {<p className='onclicked'></p>
-// }
- 
-// </div>
- 
-//   );
-//   ReactDOM.render(template, appRoot)
-// }
-
-
-// render()
-// This idea is the closer to my mind actually
-// Andrea Solution
-let visibility = false;
-const toggleVisibility = () => {
-  visibility = !visibility;
-  render();
+let visibilty = false;
+const toggleVisibilty = () => {
+  visibilty = !visibilty;
+  console.log(visibilty,!visibilty)
+  render()
 };
 const render = () => {
   const jsx = (
     <div>
-      <h1>Visibilty Toggle</h1>
-      <button onClick={toggleVisibility}>
-        {visibility?'Hide detail':'Show details'}
+      <h1>visibilty Toggle</h1>
+      <button onClick={toggleVisibilty}>
+        {visibilty?"Hide detail":"Show details"}
       </button>
-      {visibility && (
+      {visibilty && (
         <div>
           <p>Hey . These are some details you can now see!</p>
         </div>
@@ -64,4 +20,4 @@ const render = () => {
   )
   ReactDOM.render(jsx,document.getElementById('app'))
 }
-render();
+render()
