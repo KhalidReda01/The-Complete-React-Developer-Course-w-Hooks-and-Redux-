7ca6d25657b873bc68a050949de3d93880ad213f
khalidreda <khalidaziz2198@gmail.com>
2022-02-10 04:54:43 +0200

    Before Challenge Time

--- 3.Hello React/CAP section 3/lecture 13 Forms and Inputs/indecision app/public/scripts/app.js
+++ 3.Hello React/CAP section 3/lecture 13 Forms and Inputs/indecision app/public/scripts/app.js
@@ -2,18 +2,33 @@
 
 console.log('App.js is running! ');
 
-// JSX - JavaScript XML
 /**
  * 18/13 Forms and Inputs
- * 
+ * http://127.0.0.1:8080/?option=hello+
+ * http://127.0.0.1:8080/?option=test
+ * You have to repeat lectuers even if  you undrestand what is going on but for mastery okay so repat again and take notes a
+ * after you code a long and practice with him 
+ *  we don't want to use that old technqie  to do that we are setting up and event handler 
+ * react dom events syntheticEvent React  https://reactjs.org/docs/events.html
+ * working with arrays in jsx is a topic on its own
+ * // Challenge time 
  */
 
 var app = {
   title: "Indecision App",
   subtitle: "Put your life in the hands of a computer",
-  options: ['one', 'two']
+  options: []
 };
-
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  // console.log("Form Submitted!")
+  // console.log(e.target.elements)
+  var option = e.target.elements.option.value;
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+  }
+};
 var template = React.createElement(
   "div",
   null,
@@ -27,11 +42,16 @@ var template = React.createElement(
     "p",
     null,
     app.subtitle
-  ) /**Here you used the logical and operator  */,
+  ),
   React.createElement(
     "p",
     null,
     app.options.length > 0 ? "Here are your options" : "No Options"
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.options.length
   ),
   React.createElement(
     "ol",
@@ -49,7 +69,7 @@ var template = React.createElement(
   ),
   React.createElement(
     "form",
-    null,
+    { onSubmit: onFormSubmit },
     React.createElement("input", { type: "text", name: "option" }),
     React.createElement(
       "button",
--- 3.Hello React/CAP section 3/lecture 13 Forms and Inputs/indecision app/src/app.js
+++ 3.Hello React/CAP section 3/lecture 13 Forms and Inputs/indecision app/src/app.js
@@ -1,27 +1,44 @@
 console.log('App.js is running! ')
 
-// JSX - JavaScript XML
 /**
  * 18/13 Forms and Inputs
- * 
+ * http://127.0.0.1:8080/?option=hello+
+ * http://127.0.0.1:8080/?option=test
+ * You have to repeat lectuers even if  you undrestand what is going on but for mastery okay so repat again and take notes a
+ * after you code a long and practice with him 
+ *  we don't want to use that old technqie  to do that we are setting up and event handler 
+ * react dom events syntheticEvent React  https://reactjs.org/docs/events.html
+ * working with arrays in jsx is a topic on its own
+ * // Challenge time 
  */
 
 const app = {
   title: "Indecision App",
   subtitle: "Put your life in the hands of a computer",
-  options:['one','two']
+  options:[]
 }
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  // console.log("Form Submitted!")
+  // console.log(e.target.elements)
+  const option = e.target.elements.option.value;
+  if (option) {
+    app.options.push(option)
+    e.target.elements.option.value = '';
 
+  }
+}
 const template =(
 <div>
     <h1>{app.title} </h1>    
-    {app.subtitle&& <p>{app.subtitle}</p>/**Here you used the logical and operator  */} 
+    {app.subtitle&& <p>{app.subtitle}</p>} 
     <p>{app.options.length>0?"Here are your options":"No Options"}</p>
+    <p>{app.options.length}</p>
     <ol>
       <li>Item one</li>
       <li>Item two</li>
     </ol>
-    <form>
+    <form onSubmit={onFormSubmit}>
       <input type="text" name="option" />
       <button>Add Option</button>
     </form>
