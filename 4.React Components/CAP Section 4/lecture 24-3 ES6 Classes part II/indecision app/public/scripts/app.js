eefb8c9679571b7aeff4acc644b04ed4a466eef3
khalidreda <khalidaziz2198@gmail.com>
2022-02-12 10:12:59 +0200

    before the challenge time

--- 4.React Components/CAP Section 4/lecture 24-3 ES6 Classes part II/indecision app/public/scripts/app.js
+++ 4.React Components/CAP Section 4/lecture 24-3 ES6 Classes part II/indecision app/public/scripts/app.js
@@ -1,6 +1,12 @@
 'use strict';
 
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -29,9 +35,59 @@ var Person = function () {
 
   return Person;
 }();
+/**
+ * super new keyword how you can use it and why 
+ * super refer to the paret class in this case of the parent of student is person 
+ * super() we are callign the parent constructor funcion 
+ * // '' this is a false value if we use !'' false to true bolean !!' ' true !1'' false I need some explaination but I think I got it 
+ * // Logical not operator to determaie that if it have major or not  
+ * !!undefined false
+ * 
+ */
 
-var me = new Person('Andrew Mead', 26);
+
+var Student = function (_Person) {
+  _inherits(Student, _Person);
+
+  function Student(name, age, major) {
+    _classCallCheck(this, Student);
+
+    var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
+
+    _this.major = major;
+    return _this;
+  }
+
+  _createClass(Student, [{
+    key: 'hasMajor',
+    value: function hasMajor() {
+      return !!this.major;
+    }
+  }, {
+    key: 'getDescription',
+    value: function getDescription() {
+      var description = _get(Student.prototype.__proto__ || Object.getPrototypeOf(Student.prototype), 'getDescription', this).call(this);
+      if (this.hasMajor()) {
+        description += ' Their Major is ' + this.major;
+        console.log('test', this);
+      }
+      return description;
+    }
+  }]);
+
+  return Student;
+}(Person);
+// Challenge time 
+// Traveler -> Person
+// Add support for homeLocation
+// Override getGreeting
+//1.Hi I am Khalid Reda . I'm visiting from Mansura
+//2. Hi I am Khalid Reda 
+
+
+var me = new Student('Khalid Reda', 26, "Computer Sciencce");
+var test = new Student('Khalid Reda', 26, "Computer Sciencce");
 console.log(me.getDescription());
 
-var other = new Person();
+var other = new Student();
 console.log(other.getDescription());
--- 4.React Components/CAP Section 4/lecture 24-3 ES6 Classes part II/indecision app/src/playground/es6-classes-1.js
+++ 4.React Components/CAP Section 4/lecture 24-3 ES6 Classes part II/indecision app/src/playground/es6-classes-1.js
@@ -10,9 +10,43 @@ class Person {
     return `${this.name} is ${this.age} year(s) old.`;
   }
 }
+/**
+ * super new keyword how you can use it and why 
+ * super refer to the paret class in this case of the parent of student is person 
+ * super() we are callign the parent constructor funcion 
+ * // '' this is a false value if we use !'' false to true bolean !!' ' true !1'' false I need some explaination but I think I got it 
+ * // Logical not operator to determaie that if it have major or not  
+ * !!undefined false
+ * 
+ */
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name,age)
+    this.major = major;
+  }
+  hasMajor() {
+  return !! this.major  
+  }
+  getDescription() {
+    let description = super.getDescription();
+    if (this.hasMajor()) {
+      description += ` Their Major is ${this.major}`
+      console.log('test',this)
+    }
+    return description
+  }
+}
+// Challenge time 
+// Traveler -> Person
+// Add support for homeLocation
+// Override getGreeting
+//1.Hi I am Khalid Reda . I'm visiting from Mansura
+//2. Hi I am Khalid Reda 
 
-const me = new Person('Andrew Mead', 26);
+
+const me = new Student('Khalid Reda', 26,"Computer Sciencce");
+const test = new Student('Khalid Reda', 26,"Computer Sciencce");
 console.log(me.getDescription());
 
-const other = new Person();
+const other = new Student();
 console.log(other.getDescription());
