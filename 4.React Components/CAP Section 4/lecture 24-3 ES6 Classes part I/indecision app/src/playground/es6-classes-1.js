066a99cde1a4b47718dbf4ab63156623101a7b40
khalidreda <khalidaziz2198@gmail.com>
2022-02-12 09:17:54 +0200

    CFM lecture 24-3 ES^ classes part 1

--- 4.React Components/CAP Section 4/lecture 24-3 ES6 Classes part I/indecision app/src/playground/es6-classes-1.js
+++ 4.React Components/CAP Section 4/lecture 24-3 ES6 Classes part I/indecision app/src/playground/es6-classes-1.js
@@ -1,23 +1,17 @@
-//  CFSC lecture 24-3 ES6 classes part I
-
+//  CFM lecture 24-3 ES6 classes part I
+ 
 class Person {
-  constructor(name="Anonymous",age= 0) {
+  constructor(name="Anonmyous",age=0) {
     this.name = name;
-    this.age = age
+    this.age = age;
   }
   getGreeting() {
-    return `Hi . I am ${this.name}`
+  return `Hi. ${this.name}`
   }
-  getDescription() {
-    return `${this.name} is ${this.age} years(s) old .`
+  getDescrition() {
+    return `${this.name} is ${this.age} year(s) old `
   }
 }
 const me = new Person("khalid Reda",23)
-const other = new Person();
-// console.log(me.name)
-// console.log(me.age)
-// console.log(other)
 console.log(me.getGreeting())
-console.log(me.getDescription())
-console.log(other.getGreeting())
-console.log(other.getDescription())
+console.log(me.getDescrition())
