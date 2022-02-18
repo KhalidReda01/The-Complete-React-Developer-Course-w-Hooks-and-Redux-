104376c4408b811b6f5d4cc008c1d26a4f8d6659
khalidreda <khalidaziz2198@gmail.com>
2022-02-18 13:30:50 +0200

    OMG Solve the Challenge Now see the lecture see the solution Buid It :Adding state to VisibilityToggle

--- 4.React Components/CAP Section 4/lecture 35-14 Build it Adding State to VisibilityToggle/indecision app/public/scripts/app.js
+++ 4.React Components/CAP Section 4/lecture 35-14 Build it Adding State to VisibilityToggle/indecision app/public/scripts/app.js
@@ -1,14 +1,92 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 //35. Build it:Adding State to visibilityToggle
 /**
  * step 1  is going to create a component since we're trying to render something
  * to the screen and we're going to call this one visibility toggle and on there 
  * we're going to define three methods.
- *  render
- *  constructor 
+ *  render -> render it to the screen
+ *  constructor -> for 2 reason we define constructr we want to bind of of our methods
+ * for the button click and also we want to set up our default state
+ * and finally the method that we're going to have firing on click 
+ * we'll call that something like handleToggle Visibiltity awesome 
+ * Now we have these thre methods but the implementatin odetails arre going to be up to you  
+ * 
+ * *** the last thing I'm gion to give you is the  piece of state we're goin to have 
+ *  a single piece os state visibility and we'll start off at the defalut value false 
+ * and we'll toggle  it betweeen true and false depneding  on how many times the user clicks the button 
+ * when visibilty is false the butoon text will read show details when th visiblity is true will shte the contetn and the button 
+ * and the button text will read hide details 
  */
-// VisibilityToggle -
+// VisibilityToggle -render ,constructor ,handleToggleVisibility
+//visivlity- false 
 
+var VisibilityToggle = function (_React$Component) {
+  _inherits(VisibilityToggle, _React$Component);
 
+  function VisibilityToggle(props) {
+    _classCallCheck(this, VisibilityToggle);
+
+    var _this = _possibleConstructorReturn(this, (VisibilityToggle.__proto__ || Object.getPrototypeOf(VisibilityToggle)).call(this, props));
+
+    _this.handleToggleVisibility = _this.handleToggleVisibility.bind(_this);
+    _this.state = {
+      visibility: true
+    };
+
+    return _this;
+  }
+
+  _createClass(VisibilityToggle, [{
+    key: 'handleToggleVisibility',
+    value: function handleToggleVisibility() {
+      this.setState(function (prevState) {
+        return {
+          visibility: !prevState.visibility
+        };
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'h1',
+          null,
+          'Visibility Toggle'
+        ),
+        React.createElement(
+          'button',
+          { onClick: this.handleToggleVisibility },
+          this.state.visibility ? 'Hide detail' : 'Show details'
+        ),
+        this.state.visibility && React.createElement(
+          'div',
+          null,
+          React.createElement(
+            'p',
+            null,
+            'Hey . These are some details you can now see!'
+          )
+        )
+      );
+    }
+  }]);
+
+  return VisibilityToggle;
+}(React.Component);
+
+ReactDOM.render(React.createElement(VisibilityToggle, null), document.getElementById('app'));
 /*
 
 
@@ -34,4 +112,3 @@ const render = () => {
   ReactDOM.render(jsx,document.getElementById('app'))
 }
 render();*/
-"use strict";
