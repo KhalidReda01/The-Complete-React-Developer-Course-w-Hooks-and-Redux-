f661558e7e575713e5a2c3e98198c2040714030b
khalidreda <khalidaziz2198@gmail.com>
2022-02-19 08:51:42 +0200

    Section 5 Stateless Functional Components Souce Code

---
+++ 5.Stateless Functional Components/Souce Code Section 5/r-06-05-removing-individual-options/src/playground/counter-example.js
@@ -0,0 +1,48 @@
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.state = {
+      count: props.count
+    };
+  }
+  handleAddOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1
+      };
+    });
+  }
+  handleMinusOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count - 1
+      };
+    });
+  }
+  handleReset() {
+    this.setState(() => {
+      return {
+        count: 0
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>reset</button>
+      </div>
+    );
+  }
+}
+
+Counter.defaultProps = {
+  count: 0
+};
+
+ReactDOM.render(<Counter />, document.getElementById('app'));
