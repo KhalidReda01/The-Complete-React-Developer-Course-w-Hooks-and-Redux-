db2f74a4d056f4337ebd4108beb5f20e0edf89f7
khalidreda <khalidaziz2198@gmail.com>
2022-02-20 12:09:55 +0200

    DEES start Confusing part

---
+++ 00/#CSMM/#DEES/lecture 43-5 Removing Individual Options/indecision app/public/scripts/app.js
@@ -0,0 +1,294 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+/**
+ * 43.5 Remving Individual Options 
+ */
+var IndecisionApp = function (_React$Component) {
+  _inherits(IndecisionApp, _React$Component);
+
+  function IndecisionApp(props) {
+    _classCallCheck(this, IndecisionApp);
+
+    var _this = _possibleConstructorReturn(this, (IndecisionApp.__proto__ || Object.getPrototypeOf(IndecisionApp)).call(this, props));
+
+    _this.handleDeleteOptions = _this.handleDeleteOptions.bind(_this);
+    _this.handlePick = _this.handlePick.bind(_this);
+    _this.handleAddOption = _this.handleAddOption.bind(_this);
+    _this.state = {
+
+      // options:[]
+      options: props.options
+    };
+    return _this;
+  }
+
+  _createClass(IndecisionApp, [{
+    key: 'handleDeleteOptions',
+    value: function handleDeleteOptions() {
+      // repalce this with a shortand
+      // this.setState(() => {
+      //   return {
+      //     options:[]`
+      //   }
+      // })
+
+      this.setState(function () {
+        return { options: [] };
+      });
+    }
+    /**
+     * there is an error at this this.state isn't a function
+     *   this.setState((prevState) => ({
+        options: prevState.options.filter((option) => {
+          return true
+        })
+      }))
+     *
+     */
+
+  }, {
+    key: 'handleDeleteOption',
+    value: function handleDeleteOption(option) {
+      // console.log('hdo',option )
+      //Uncaught TypeError: this.setState is not a function       console.log("DEES",option),
+
+      this.setState(function (prevState) {
+        return {
+          options: ['hello what is going on']
+
+        };
+      });
+    }
+    /**
+     * Challenge Time
+     * Your Challenge  is going to be to convert the other to this data set state calls 
+     */
+
+  }, {
+    key: 'handlePick',
+    value: function handlePick() {
+      var _this2 = this;
+
+      this.setState(function () {
+        var randomNum = Math.floor(Math.random() * _this2.state.options.length);
+        var option = _this2.state.options[randomNum];
+        alert(option);
+      });
+    }
+  }, {
+    key: 'handleAddOption',
+    value: function handleAddOption(option) {
+
+      if (!option) {
+        return 'Enter Valid value to add item';
+      } else if (this.state.options.indexOf(option) > -1) {
+        return "This Option already exists";
+      }
+
+      console.log("what is this ", option);
+      // one state to convert
+      // this.setState((prevState) => {
+      //   return {
+
+
+      //     options:prevState.options.concat(option)
+      //   }
+      // })
+      // prevState is not Defined you didn't put it as argument hahah 
+      // Okay the first part of the challenge NOW work so well
+      this.setState(function (prevState) {
+        return { options: prevState.options.concat(option) };
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      // const title = "Indecision ";
+      var subtitle = "Put your Life in the hand of a computer";
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(Header, { subtitle: subtitle }),
+        React.createElement(Action, { hasOptions: this.state.options.length > 0,
+
+          handlePick: this.handlePick
+        }),
+        React.createElement(Options, {
+          options: this.state.options,
+          handleDeleteOptions: this.handleDeleteOptions,
+          handleDeleteOption: this.handleDeleteOption
+        }),
+        React.createElement(AddOptions, {
+          handleAddOption: this.handleAddOption
+        })
+      );
+    }
+  }]);
+
+  return IndecisionApp;
+}(React.Component);
+
+IndecisionApp.defaultProps = {
+  options: ["three"]
+};
+
+var Header = function Header(props) {
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      props.title
+    ),
+    props.subtitle && React.createElement(
+      'h2',
+      null,
+      props.subtitle
+    )
+  );
+};
+Header.defaultProps = {
+  title: 'Indecision'
+
+};
+
+var Action = function Action(props) {
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'button',
+      { onClick: props.handlePick,
+        disabled: !props.hasOptions
+      },
+      'What should I do ?'
+    )
+  );
+};
+
+var Options = function Options(props) {
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'p',
+      null,
+      'Here are your options'
+    ),
+    React.createElement(
+      'button',
+      { onClick: props.handleDeleteOptions },
+      'RemoveAll'
+    ),
+    props.options.map(function (option) {
+      return React.createElement(Option, {
+        key: option,
+        optionText: option,
+        handleDeleteOption: props.handleDeleteOption
+      });
+    })
+  );
+};
+/**
+ * so instead of passing in props.handleDeleteOption just gonna defune an inline arrow function 
+ * {    <button onClick={props.handleDeleteOption}>remove</button>} 
+ *  
+ */
+var Option = function Option(props) {
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'p',
+      null,
+      'Option:',
+      props.optionText
+    ),
+    React.createElement(
+      'button',
+      {
+        onClick: function onClick(e) {
+          props.handleDeleteOption(props.optionText);
+        }
+      },
+      'remove'
+    )
+  );
+};
+
+var AddOptions = function (_React$Component2) {
+  _inherits(AddOptions, _React$Component2);
+
+  function AddOptions(props) {
+    _classCallCheck(this, AddOptions);
+
+    var _this3 = _possibleConstructorReturn(this, (AddOptions.__proto__ || Object.getPrototypeOf(AddOptions)).call(this, props));
+
+    _this3.handleAddOption = _this3.handleAddOption.bind(_this3);
+    _this3.state = {
+      error: undefined
+    };
+    return _this3;
+  }
+
+  _createClass(AddOptions, [{
+    key: 'handleAddOption',
+    value: function handleAddOption(e) {
+      e.preventDefault();
+
+      var option = e.target.elements.option.value.trim();
+      console.log(option);
+      var error = this.props.handleAddOption(option);
+      // another one to convert
+      // this.setState(() => {
+      //   return {
+      //     error: error
+
+      //   }
+      // })
+      // this is the second part of the challenge test it now Okay it working 
+      this.setState(function () {
+        return { error: error };
+      });
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        null,
+        this.state.error && React.createElement(
+          'p',
+          null,
+          this.state.error
+        ),
+        React.createElement(
+          'form',
+          { onSubmit: this.handleAddOption },
+          React.createElement('input', { type: 'text', name: 'option' }),
+          React.createElement(
+            'button',
+            null,
+            'Add Option '
+          )
+        )
+      );
+    }
+  }]);
+
+  return AddOptions;
+}(React.Component);
+
+// ReactDOM.render(<IndecisionApp options={["Devils den","Second District"] }/>,document.getElementById('app'))
+
+
+ReactDOM.render(React.createElement(IndecisionApp, null), document.getElementById('app'));
---
+++ 00/#CSMM/#DEES/lecture 43-5 Removing Individual Options/indecision app/src/app.js
@@ -0,0 +1,216 @@
+
+/**
+ * 43.5 Remving Individual Options 
+ */
+class IndecisionApp extends React.Component{
+  constructor(props) {
+    super(props);
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
+    this.handlePick = this.handlePick.bind(this)
+    this.handleAddOption=this.handleAddOption.bind(this)
+    this.state = {
+    
+      // options:[]
+      options:props.options
+    }
+  }
+  handleDeleteOptions() {
+    // repalce this with a shortand
+    // this.setState(() => {
+    //   return {
+    //     options:[]`
+    //   }
+    // })
+    
+    this.setState(() => ({options:[] } ) )
+  }
+  /**
+   * there is an error at this this.state isn't a function
+   *   this.setState((prevState) => ({
+      options: prevState.options.filter((option) => {
+        return true
+      })
+    }))
+   *
+   */
+  handleDeleteOption(option) {
+    // console.log('hdo',option )
+  //Uncaught TypeError: this.setState is not a function       console.log("DEES",option),
+
+    this.setState((prevState) => ({
+      options:['hello what is going on']
+     
+    }))
+  }
+  /**
+   * Challenge Time
+   * Your Challenge  is going to be to convert the other to this data set state calls 
+   */
+  handlePick() {
+    this.setState(() => {
+      const randomNum = Math.floor(Math.random() * this.state.options.length);
+      const option = this.state.options[randomNum];
+      alert(option);
+    })
+    
+  }
+  handleAddOption(option) {
+   
+    if (!option) {
+      return 'Enter Valid value to add item'
+    } else if (this.state.options.indexOf(option) > -1) {
+      return "This Option already exists"
+    }
+
+    console.log("what is this ", option)
+    // one state to convert
+    // this.setState((prevState) => {
+    //   return {
+        
+       
+    //     options:prevState.options.concat(option)
+    //   }
+    // })
+    // prevState is not Defined you didn't put it as argument hahah 
+    // Okay the first part of the challenge NOW work so well
+    this.setState((prevState)=>({options:prevState.options.concat(option)}))
+  }
+
+  render() {
+    // const title = "Indecision ";
+    const subtitle = "Put your Life in the hand of a computer"
+    return (
+         <div>
+        {/* <Header    /> */}
+        <Header subtitle={subtitle}   />
+        {/* <Header title={title}  subtitle={subtitle} /> */}
+      
+        <Action hasOptions={this.state.options.length > 0}
+        
+        handlePick={this.handlePick}
+        />
+        <Options
+          options={this.state.options}
+          handleDeleteOptions={this.handleDeleteOptions}
+          handleDeleteOption={this.handleDeleteOption}
+        />
+        
+        <AddOptions
+          handleAddOption={this.handleAddOption}
+        />
+    </div>)
+  }
+    
+ 
+}
+IndecisionApp.defaultProps = {
+  options:["three"]
+ }
+
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      {props.subtitle&&<h2>{props.subtitle}</h2>}
+     {/* <h2>{props.subtitle}</h2> */}
+    </div>
+    )
+}
+Header.defaultProps = {
+  title: 'Indecision'
+
+}
+
+const Action = (props) => {
+  return (<div>
+    
+    <button onClick={props.handlePick}
+    disabled={!props.hasOptions}
+    >What should I do ?</button>
+   </div>)
+}
+
+const Options = (props) => {
+  return (
+    <div>
+      <p>Here are your options</p>
+      
+      <button onClick={props.handleDeleteOptions}>RemoveAll</button>
+      {
+        props.options.map((option) => (
+
+          <Option
+            key={option}
+            optionText={option}
+            handleDeleteOption ={props.handleDeleteOption}
+          />)
+  )
+  }
+    </div>
+    
+  )
+}
+/**
+ * so instead of passing in props.handleDeleteOption just gonna defune an inline arrow function 
+ * {    <button onClick={props.handleDeleteOption}>remove</button>} 
+ *  
+ */
+const Option = (props) => {
+  return (
+    <div>
+      <p>Option:{props.optionText}</p> 
+      <button
+        onClick={(e) => {
+          props.handleDeleteOption(props.optionText)
+        }}
+      >
+        remove
+      </button>
+    </div>
+  )
+}
+
+
+class AddOptions extends React.Component{
+  constructor(props) {
+    super(props);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.state = {
+      error: undefined
+    }
+ }
+    handleAddOption(e) {
+    e.preventDefault();
+
+      const option = e.target.elements.option.value.trim();
+      console.log(option)
+      const error = this.props.handleAddOption(option);
+      // another one to convert
+      // this.setState(() => {
+      //   return {
+      //     error: error
+         
+      //   }
+      // })
+      // this is the second part of the challenge test it now Okay it working 
+     this.setState(()=>({ error: error}))
+  
+  }
+  render() {
+    return (
+      <div>
+        {this.state.error && <p>{this.state.error }</p>}  
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option" />
+          <button>Add Option </button>
+        </form>
+        
+      </div>
+    )
+  }
+}
+
+
+// ReactDOM.render(<IndecisionApp options={["Devils den","Second District"] }/>,document.getElementById('app'))
+ReactDOM.render(<IndecisionApp />,document.getElementById('app'))
