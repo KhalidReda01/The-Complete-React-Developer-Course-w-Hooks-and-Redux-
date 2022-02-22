765ee494ca982ab321331d603e2b63b298910e7b
khalidreda <khalidaziz2198@gmail.com>
2022-02-22 22:33:47 +0200

    Came back but not waste your time for this the Error isn't at your code that you test even you tried a simpe component but the problem at babel Now keep study the section then came back after you fully aborve the information

--- 6.Webpack/CAP Section 6/lecture 49-3 Avoid Global Modules/indecision app/src/app.js
+++ 6.Webpack/CAP Section 6/lecture 49-3 Avoid Global Modules/indecision app/src/app.js
@@ -17,227 +17,216 @@ SyntaxError: src/app.js: Unexpected token (100:9)
   103 |
 
  */
+// Okay now I take the same code that works at the source code now gonna see what will happen 
 
 
-
-class IndecisionApp extends React.Component{
-  constructor(props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
-    this.handlePick = this.handlePick.bind(this)
-    this.handleAddOption = this.handleAddOption.bind(this)
-    this.handleDeleteOption=this.handleDeleteOption.bind(this)
-
-    this.state = {
-      options:props.options
-    }
-  }
+  // class IndecisionApp extends React.Component {
+  //   constructor(props) {
+  //     super(props);
+  //     this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+  //     this.handlePick = this.handlePick.bind(this);
+  //     this.handleAddOption = this.handleAddOption.bind(this);
+  //     this.handleDeleteOption = this.handleDeleteOption.bind(this);
+  //     this.state = {
+  //       options: []
+  //     };
+  //   }
+  //   componentDidMount() {
+  //     try {
+  //       const json = localStorage.getItem('options');
+  //       const options = JSON.parse(json);
   
- 
-
-  componentDidMount() {
-    try {
-      const json = localStorage.getItem('options');
-      const options = JSON.parse(json);
-      if (options) {
-           this.setState(() => ({options:options}))
-      }
-   
-
-    } catch (error) {
-      
-    }
-    
-    console.log('fetching data')
-  }
+  //       if (options) {
+  //         this.setState(() => ({ options }));
+  //       }
+  //     } catch (e) {
+  //       // Do nothing at all
+  //     }
+  //   }
+  //   componentDidUpdate(prevProps, prevState) {
+  //     if (prevState.options.length !== this.state.options.length) {
+  //       const json = JSON.stringify(this.state.options);
+  //       localStorage.setItem('options', json);
+  //     }
+  //   }
+  //   componentWillUnmount() {
+  //     console.log('componentWillUnmount');
+  //   }
+  //   handleDeleteOptions() {
+  //     this.setState(() => ({ options: [] }));
+  //   }
+  //   handleDeleteOption(optionToRemove) {
+  //     this.setState((prevState) => ({
+  //       options: prevState.options.filter((option) => optionToRemove !== option)
+  //     }));
+  //   }
+  //   handlePick() {
+  //     const randomNum = Math.floor(Math.random() * this.state.options.length);
+  //     const option = this.state.options[randomNum];
+  //     alert(option);
+  //   }
+  //   handleAddOption(option) {
+  //     if (!option) {
+  //       return 'Enter valid value to add item';
+  //     } else if (this.state.options.indexOf(option) > -1) {
+  //       return 'This option already exists';
+  //     }
   
-  componentDidUpdate(prevProps,prevState) {
-    if (prevState.options.length !== this.state.options.length) {
-      const json = JSON.stringify(this.state.options);
-      localStorage.setItem('options', json)
-      console.log('saving data')
-    }
-    
-  }
- 
-
+  //     this.setState((prevState) => ({
+  //       options: prevState.options.concat(option)
+  //     }));
+  //   }
+  //   render() {
+  //     const subtitle = 'Put your life in the hands of a computer';
   
-  componentWillUnmount() {
-    console.log('ComponentWillUnmount')
-  }
-  handleDeleteOptions() {
-    this.setState(() => ( {
-        options:[]
-      })
-     
-    )
-  }
-  handleDeleteOption(optionToRemove) {
-
-    this.setState((prevState) => ({
-      options:prevState.options.filter((option)=> optionToRemove!==option)
-     
-    }))
-  }
-  handlePick() {
-    this.setState(() => {
-      const randomNum = Math.floor(Math.random() * this.state.options.length);
-      const option = this.state.options[randomNum];
-      alert(option);
-    })
-    
-  }
-  handleAddOption(option) {
-    
-    if (!option) {
-      return 'Enter Valid value to add item'
-    } else if (this.state.options.indexOf(option) > -1) {
-      return "This Option already exists"
-    }
-
-    console.log("what is this ", option)
-    this.setState((prevState) => ({
-        
-       
-        options:prevState.options.concat(option)
-      })
-      
-    )
-  }
-
-  render() {
-    const title = "Indecision ";
-    const subtitle = "Put your Life in the hand of a computer"
-    return (
-         <div>
-       
-        <Header subtitle={subtitle} />
-      
-        <Action hasOptions={this.state.options.length > 0}
-        
-        handlePick={this.handlePick}
-        />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-         handleDeleteOption={this.handleDeleteOption} 
-          
-        />
-        
-        <AddOptions
-          handleAddOption={this.handleAddOption}
-        />
-    </div>)
-  }
-    
- 
-}
-
-IndecisionApp.defaultProps = {
-  options:[]
-}
-
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.title}</h1>
-      {props.subtitle&&<h2>{props.subtitle }</h2> }
-    </div>
-    )
-}
-Header.defaultProps = {
-  title: "Indecision"
- 
-}
-const Action = (props) => {
-  return (<div>
-    
-    <button onClick={props.handlePick}
-    disabled={!props.hasOptions}
-    >What should I do ?</button>
-   </div>)
-}
-
-
-const Options = (props) => {
-  return (
-    <div>
-      <p>Here are your options</p>
-      
-      <button onClick={props.handleDeleteOptions}>RemoveAll</button>
-      {props.options.length===0&& <p>Please add an option  to get Started!</p>}
-      {
-        props.options.map((option) => <Option
-          key={option}
-          optionText={option}
-          handleDeleteOption={props.handleDeleteOption} 
-         
-          
-        />
-  )
-  }
-    </div>
-    
-  )
-}
-
-
-const Option = (props) => {
-  return (
-    <div>
-      <p>Option:{props.optionText}</p> 
-      <button onClick={(e) => {
-        // props.handleDeleteOption(props.optionText)
-        props.handleDeleteOption(props.optionText);
-
-      }}
-      >remove</button>
-    </div>
-  )
-}
-// 
-
-class AddOptions extends React.Component{
-  constructor(props) {
-    super(props);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.state = {
-      error: undefined
-    }
- }
-    handleAddOption(e) {
-    e.preventDefault();
-
-      const option = e.target.elements.option.value.trim();
-      // console.log(option)
-      const error = this.props.handleAddOption(option);
-      //third one 
-      this.setState(() => ({
-          error: error
-         
-        })
-         
-      )
-      if (!error) {
-        // I think if there is not error then made the input value clean 
-        // e.target.elements.option.value = 'OMG I got it '
-        e.target.elements.option.value = 'Add another option '
-        // console.log('what is this part I dont get it '+e.target.elements.option.value)
-   }
-    
-  }
-  render() {
+  //     return (
+  //       <div>
+  //         <Header subtitle={subtitle} />
+  //         <Action
+  //           hasOptions={this.state.options.length > 0}
+  //           handlePick={this.handlePick}
+  //         />
+  //         <Options
+  //           options={this.state.options}
+  //           handleDeleteOptions={this.handleDeleteOptions}
+  //           handleDeleteOption={this.handleDeleteOption}
+  //         />
+  //         <AddOption
+  //           handleAddOption={this.handleAddOption}
+  //         />
+  //       </div>
+  //     );
+  //   }
+  // }
+  
+  // const Header = (props) => {
+  //   return (
+  //     <div>
+  //       <h1>{props.title}</h1>
+  //       {props.subtitle && <h2>{props.subtitle}</h2>}
+  //     </div>
+  //   );
+  // };
+  
+  // Header.defaultProps = {
+  //   title: 'Indecision'
+  // };
+  
+  // const Action = (props) => {
+  //   return (
+  //     <div>
+  //       <button
+  //         onClick={props.handlePick}
+  //         disabled={!props.hasOptions}
+  //       >
+  //         What should I do?
+  //       </button>
+  //     </div>
+  //   );
+  // };
+  
+  // const Options = (props) => {
+  //   return (
+  //     <div>
+  //       <button onClick={props.handleDeleteOptions}>Remove All</button>
+  //       {props.options.length === 0 && <p>Please add an option to get started!</p>}
+  //       {
+  //         props.options.map((option) => (
+  //           <Option
+  //             key={option}
+  //             optionText={option}
+  //             handleDeleteOption={props.handleDeleteOption}
+  //           />
+  //         ))
+  //       }
+  //     </div>
+  //   );
+  // };
+  
+  // const Option = (props) => {
+  //   return (
+  //     <div>
+  //       {props.optionText}
+  //       <button
+  //         onClick={(e) => {
+  //           props.handleDeleteOption(props.optionText);
+  //         }}
+  //       >
+  //         remove
+  //       </button>
+  //     </div>
+  //   );
+  // };
+  
+  // class AddOption extends React.Component {
+  //   constructor(props) {
+  //     super(props);
+  //     this.handleAddOption = this.handleAddOption.bind(this);
+  //     this.state = {
+  //       error: undefined
+  //     };
+  //   }
+  //   handleAddOption(e) {
+  //     e.preventDefault();
+  
+  //     const option = e.target.elements.option.value.trim();
+  //     const error = this.props.handleAddOption(option);
+  
+  //     this.setState(() => ({ error }));
+  
+  //     if (!error) {
+  //       e.target.elements.option.value = '';
+  //     }
+  //   }
+  //   render() {
+  //     return (
+  //       <div>
+  //         {this.state.error && <p>{this.state.error}</p>}
+  //         <form onSubmit={this.handleAddOption}>
+  //           <input type="text" name="option" />
+  //           <button>Add Option </button>
+  //         </form>
+  //       </div>
+  //     );
+  //   }
+  // }
+  
+  const User = (props) => {
     return (
       <div>
-        {this.state.error && <p>{this.state.error }</p>}  
-        <form onSubmit={this.handleAddOption}>
-          <input type="text" name="option" />
-          <button>Add Option </button>
-        </form>
-        
+        <p>Name: {props.name}</p>
+        <p>Age: {props.age}</p>
       </div>
-    )
-  }
-}
+    );
+  };
+  
+  // ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+  /**
+   * gave me the same error even using the same code the works so I think the problem at the node_moduels and package.json
+   * change src/app.js
+SyntaxError: src/app.js: Unexpected token (83:8)
+  81 | 
+  82 |       return (
+> 83 |         <div>
+     |         ^
+  84 |           <Header subtitle={subtitle} />
+  85 |           <Action
+  86 |             hasOptions={this.state.options.length > 0}
 
-ReactDOM.render(<IndecisionApp />,document.getElementById('app'))
+   */
+  ReactDOM.render(<User name="khalid" age="23" />, document.getElementById('app'));
+  
+  /**
+   * I want to laugh now because even using this basic one babel see an error so  there is something that I can't see 
+   * change src/app.js
+SyntaxError: src/app.js: Unexpected token (196:6)
+  194 |   const User = (props) => {
+  195 |     return (
+> 196 |       <div>
+      |       ^
+  197 |         <p>Name: {props.name}</p>
+  198 |         <p>Age: {props.age}</p>
+  199 |       </div>
+
+   * 
+   */
