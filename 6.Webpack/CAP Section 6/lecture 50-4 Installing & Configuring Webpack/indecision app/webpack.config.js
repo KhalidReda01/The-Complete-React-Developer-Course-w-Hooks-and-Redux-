534927607ea01ee39e3b20a0c4df9336df2c65ca
khalidreda <khalidaziz2198@gmail.com>
2022-02-23 22:04:38 +0200

    Now webpack after config file is working

---
+++ 6.Webpack/CAP Section 6/lecture 50-4 Installing & Configuring Webpack/indecision app/webpack.config.js
@@ -0,0 +1,11 @@
+// OMG I missed the output:
+const path = require('path')
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+     path: path.join(__dirname,'public'),
+  filename:'bundle.js'
+  }
+ 
+}
