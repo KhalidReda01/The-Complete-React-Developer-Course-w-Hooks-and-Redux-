9971c9a33d06eeeddbbaa99a1ba40f49b1afa5a2
khalidreda <khalidaziz2198@gmail.com>
2022-02-25 16:54:53 +0200

    Yes finished but not for me you have to CFSC then WVI and keep going for another WTV LOL

--- 6.Webpack/CAP Section 6/lecture 53-7 Importing npm Modules/indecision app/src/app.js
+++ 6.Webpack/CAP Section 6/lecture 53-7 Importing npm Modules/indecision app/src/app.js
@@ -8,3 +8,17 @@
 // import validator from "validator";
 // console.log(validator.isEmail('test@gmail.com'))
 // Why you are distracting yourself Close anything and just Focus for some time the world will be the same don't worry hahahahah Now keep going welcome back
+//npm react
+//https://www.npmjs.com/package/react
+// npm react-dom
+//https://www.npmjs.com/package/react-dom
+
+
+// yarn add react@16.0.0 react-dom@16.0.0
+console.log('app.js is runing')
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+// const template = <p>test1234</p>
+const template = React.createElement('p', {},'testing 123')
+ReactDOM.render(template,document.getElementById('app'))
