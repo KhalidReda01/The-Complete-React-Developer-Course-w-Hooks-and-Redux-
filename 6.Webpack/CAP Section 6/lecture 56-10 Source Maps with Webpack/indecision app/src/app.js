3734ba66bb63b9aab00de59ed74ccb976ab9e5a2
khalidreda <khalidaziz2198@gmail.com>
2022-02-26 16:36:19 +0200

    let's finish this section today Keep going and you will have your Guilt-free-play

--- 6.Webpack/CAP Section 6/lecture 56-10 Source Maps with Webpack/indecision app/src/app.js
+++ 6.Webpack/CAP Section 6/lecture 56-10 Source Maps with Webpack/indecision app/src/app.js
@@ -1,22 +1,12 @@
-// 55 One Component Per File 
+// 56 Source maps with webpack
 
+// Foucs at what you are doing be here
 console.log('app.js is runing')
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp'
 
-// Challenge time
-// convert the header component and the action component
-// creae the file , copy the code , setup import/export
-// see It's so normal you didn't miss to import Options but he did so Its okay Don't be perfectionaist but I think you will be good developer just keep going and time will pass anyway
-// The most important thing time managment and dont' get distracted Finish this amazing Course
-
-
-// Second Challenge Time 
-// Moving the Component code
-// Setup import /export
-// Move the imports to that new files and setup an import here 
 
 
 
