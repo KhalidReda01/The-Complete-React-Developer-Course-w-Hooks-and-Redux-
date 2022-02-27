5af7d6fff5fc9ee06ba686beeabc35d35a915ced
khalidreda <khalidaziz2198@gmail.com>
2022-02-27 18:21:32 +0200

    POC 1-3m

--- 7.Using a Thrid-Party Component/CAP Section 7/60-2 Passing Children to Copmonent/indecision app/src/app.js
+++ 7.Using a Thrid-Party Component/CAP Section 7/60-2 Passing Children to Copmonent/indecision app/src/app.js
@@ -7,7 +7,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp'
 
+const Layout = (props) => {
+  return (
+    <div>
+      <p>Header</p>
+      {props.content}
+      <p>Footer</p>
+    </div>
+  )
+}
 
+const template = (
+  <div>
+    <h1>Page Title</h1>
+    <p>This is my page</p>
+  </div>
+)
 
-
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+// ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<Layout content={template} />, document.getElementById('app'));
