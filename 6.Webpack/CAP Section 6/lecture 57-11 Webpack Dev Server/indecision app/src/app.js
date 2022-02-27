313514f3249d9eddd85a5b6eb6bce929f78a03f8
khalidreda <khalidaziz2198@gmail.com>
2022-02-27 09:27:42 +0200

    GFP Only after CSMM Now enjoy learning

--- 6.Webpack/CAP Section 6/lecture 57-11 Webpack Dev Server/indecision app/src/app.js
+++ 6.Webpack/CAP Section 6/lecture 57-11 Webpack Dev Server/indecision app/src/app.js
@@ -1,5 +1,8 @@
 // 57 Webpack Dev Server
-
+/**
+ * Guilt free play I made a new user at this pc for that distraction from now Distraction is not allowed at study time I switched all the accounts to GFP user Now enjoy Learning
+ * 
+ */
 
 
 import React from 'react';
