786d2d122925afea5e8c26783673ffe2a5d1b299
khalidreda <khalidaziz2198@gmail.com>
2022-02-27 10:50:50 +0200

    Lecture 58 start

---
+++ 6.Webpack/CAP Section 6/lecture 58-12 ES6 Class Properties/indecision app/src/app.js
@@ -0,0 +1,19 @@
+// End the Section 6 Webpack
+// lecture 58/12 ES6 Class Propertie 
+
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp'
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
