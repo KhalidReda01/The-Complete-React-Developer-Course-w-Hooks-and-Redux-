1063b630f86e6d9ca207ec7286593d727055484d
khalidreda <khalidaziz2198@gmail.com>
2022-03-02 08:37:49 +0200

    Ok section end now finish the CFSC and WVI for the previous lecture

--- 7.Using a Thrid-Party Component/CAP Section 7/62-5 Bonus Refactoring Other Statless Functional Components/indecision app/src/components/Header.js
+++ 7.Using a Thrid-Party Component/CAP Section 7/62-5 Bonus Refactoring Other Statless Functional Components/indecision app/src/components/Header.js
@@ -1,10 +1,8 @@
 import React from 'react'
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.title}</h1>
-      {props.subtitle && <h2>{props.subtitle}</h2>}
-    </div>
-  );
-};
+const Header = (props) =>(
+  <div>
+    <h1>{props.title}</h1>
+    {props.subtitle && <h2>{props.subtitle}</h2>}
+  </div>
+);
 export default Header
--- 7.Using a Thrid-Party Component/CAP Section 7/62-5 Bonus Refactoring Other Statless Functional Components/indecision app/src/components/Option.js
+++ 7.Using a Thrid-Party Component/CAP Section 7/62-5 Bonus Refactoring Other Statless Functional Components/indecision app/src/components/Option.js
@@ -1,17 +1,15 @@
 import React from 'react'
 // export default (pros)=>{ } // unknown 
-const Option = (props) => {
-  return (
-    <div>
-      {props.optionText}
-      <button
-        onClick={(e) => {
-          props.handleDeleteOption(props.optionText);
-        }}
-      >
-        remove
-      </button>
-    </div>
-  );
-};
+const Option = (props) => (
+  <div>
+    {props.optionText}
+    <button
+      onClick={(e) => {
+        props.handleDeleteOption(props.optionText);
+      }}
+    >
+      remove
+    </button>
+  </div>
+);
 export default Option;
