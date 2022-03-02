3eb62e362632553a289b0900f371f8a4f5674975
khalidreda <khalidaziz2198@gmail.com>
2022-03-02 08:26:13 +0200

    Last Lecture at section 7 start

---
+++ 7.Using a Thrid-Party Component/CAP Section 7/62-5 Bonus Refactoring Other Statless Functional Components/indecision app/src/app.js
@@ -0,0 +1,13 @@
+
+/**
+ * Section 7 Using a Third-Party Component 
+ * lecture 62/4  Bonus Refactoring Other Statless function components   
+ * 
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp'
+
+
+ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
