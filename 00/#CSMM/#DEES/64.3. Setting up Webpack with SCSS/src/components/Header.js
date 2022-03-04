5462c78f7e79ef7f595f29fe8914d4703464613f
khalidreda <khalidaziz2198@gmail.com>
2022-03-04 11:19:46 +0200

    DEES lecture 64.3 setting up webpack with scss

---
+++ 00/#CSMM/#DEES/64.3. Setting up Webpack with SCSS/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = (props) => (
+  <div>
+    <h1>{props.title}</h1>
+    {props.subtitle && <h2>{props.subtitle}</h2>}
+  </div>
+);
+
+Header.defaultProps = {
+  title: 'Indecision'
+};
+
+export default Header;
---
+++ 00/#CSMM/#DEES/64.3. Setting up Webpack with SCSS/src/components/Option.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Option = (props) => (
+  <div>
+    {props.optionText}
+    <button
+      onClick={(e) => {
+        props.handleDeleteOption(props.optionText);
+      }}
+    >
+      remove
+      </button>
+  </div>
+);
+
+export default Option;
