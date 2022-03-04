520bda63730d3d19a1ac7944764dd10708d400db
khalidreda <khalidaziz2198@gmail.com>
2022-03-04 10:22:59 +0200

    Start lecture Setting up webpack with scss and exclude node_modules from my repo

---
+++ 8.Styling React/CAP Section 8/64.3. Setting up Webpack with SCSS/indecision app/src/app.js
@@ -0,0 +1,15 @@
+
+/**
+ * Section 8 Styling React
+ * lecture 64/4 setting up webpack with scss 
+ * 
+ * first I added a .gitgnore file and add the name of node_modules at it to ignore it from my Githup repo no need to cut and paste it anymore 
+ * 
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp'
+
+
+ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
