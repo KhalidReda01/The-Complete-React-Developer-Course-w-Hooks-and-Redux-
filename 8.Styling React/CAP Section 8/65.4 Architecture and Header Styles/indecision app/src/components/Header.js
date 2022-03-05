eadfa3bd6f2848d8e98aee279210c8b3b536154b
khalidreda <khalidaziz2198@gmail.com>
2022-03-05 11:53:58 +0200

    CAP done for lecture 65/3 Architecture and Header Styles

--- 8.Styling React/CAP Section 8/65.4 Architecture and Header Styles/indecision app/src/components/Header.js
+++ 8.Styling React/CAP Section 8/65.4 Architecture and Header Styles/indecision app/src/components/Header.js
@@ -1,8 +1,8 @@
 import React from 'react'
 const Header = (props) =>(
   <div className="header">
-    <h1>{props.title}</h1>
-    {props.subtitle && <h2>{props.subtitle}</h2>}
+    <h1 className="header__title">{props.title}</h1>
+    {props.subtitle && <h2 className='header__subtitle'>{props.subtitle}</h2>}
   </div>
 );
 export default Header
