4a3a7f68efa25eb1731818b1eae5ce33093e3576
khalidreda <khalidaziz2198@gmail.com>
2022-03-05 13:34:38 +0200

    CAP lecture 66 Reset That 0add . start but after WVI previous lecture

---
+++ 8.Styling React/CAP Section 8/66.5 Reset That $#!/indecision app/src/app.js
@@ -0,0 +1,13 @@
+/**
+ *  66 Reset That  $#!*
+ * 
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp'
+import './styles/styles.scss'
+
+
+
+ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
