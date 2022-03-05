0775d0f94f192aa90955924ebd5b09fd645a8e89
khalidreda <khalidaziz2198@gmail.com>
2022-03-05 19:21:16 +0200

    CAP start lecture 67 but not CFSC&WVI previous lecture

---
+++ 8.Styling React/CAP Section 8/67.5 Theming With Variables/indecision app/src/app.js
@@ -0,0 +1,17 @@
+/**
+ *  DEES the error was because of the name of that folder Reset That $#! this was the Error now I will try again 
+ * 
+ */
+// yarn add normalize.css@7.0.0
+// yarn add normalize.css@7.0.0
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp'
+import 'normalize.css/normalize.css'
+// It's working now 
+import './styles/styles.scss'
+
+
+
+ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
