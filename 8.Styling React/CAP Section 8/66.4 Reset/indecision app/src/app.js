7ade5d11782235e912208d5701603cd85f513889
khalidreda <khalidaziz2198@gmail.com>
2022-03-05 19:08:52 +0200

    Okay now It's working let's see what will happen even I faced something strange too when I tried to install normalize.css@7.0.0 but IT's working now

--- 8.Styling React/CAP Section 8/66.4 Reset/indecision app/src/app.js
+++ 8.Styling React/CAP Section 8/66.4 Reset/indecision app/src/app.js
@@ -2,6 +2,8 @@
  *  DEES the error was because of the name of that folder Reset That $#! this was the Error now I will try again 
  * 
  */
+// yarn add normalize.css@7.0.0
+// yarn add normalize.css@7.0.0
 
 import React from 'react';
 import ReactDOM from 'react-dom';
