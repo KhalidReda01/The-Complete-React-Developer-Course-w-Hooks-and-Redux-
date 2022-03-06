a682ab9d7d156b0eeb215bf2432312f70d8dd3fd
khalidreda <khalidaziz2198@gmail.com>
2022-03-06 14:35:36 +0200

    CAP start lecture 67 Theming with Variables

--- 8.Styling React/CAP Section 8/67.5 Theming With Variables/indecision app/src/app.js
+++ 8.Styling React/CAP Section 8/67.5 Theming With Variables/indecision app/src/app.js
@@ -1,15 +1,12 @@
 /**
- *  DEES the error was because of the name of that folder Reset That $#! this was the Error now I will try again 
- * 
+ * 67/5 Theming with variables 
+ * CAP
  */
-// yarn add normalize.css@7.0.0
-// yarn add normalize.css@7.0.0
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp'
 import 'normalize.css/normalize.css'
-// It's working now 
 import './styles/styles.scss'
 
 
