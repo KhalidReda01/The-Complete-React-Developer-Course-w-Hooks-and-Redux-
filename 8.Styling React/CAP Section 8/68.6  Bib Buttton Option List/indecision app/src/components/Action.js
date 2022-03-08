73498abb8d9a3756b7afb43d8534827b6d31cf3c
khalidreda <khalidaziz2198@gmail.com>
2022-03-08 16:42:32 +0200

    5m Add button style and using the darken sass function

--- 8.Styling React/CAP Section 8/68.6  Bib Buttton Option List/indecision app/src/components/Action.js
+++ 8.Styling React/CAP Section 8/68.6  Bib Buttton Option List/indecision app/src/components/Action.js
@@ -2,7 +2,7 @@ import React from 'react'
 const Action = (props) => {
   return (
     <div>
-      <button
+      <button className="big-button"
         onClick={props.handlePick}
         disabled={!props.hasOptions}
       >
