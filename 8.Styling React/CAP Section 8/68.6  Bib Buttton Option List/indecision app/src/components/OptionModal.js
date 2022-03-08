d1c90d6427e61c6652c31bfb54b8cd1a874ffb61
khalidreda <khalidaziz2198@gmail.com>
2022-03-08 15:21:56 +0200

    CAP lecture 68/6 Big Button and Option list but not  befor CFSC WVI previous lecture

---
+++ 8.Styling React/CAP Section 8/68.6  Bib Buttton Option List/indecision app/src/components/OptionModal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Modal from 'react-modal';
+const OptionModal = (props) => (
+  <Modal
+    isOpen={!!props.selectedOption}
+    onRequestClose={props.handleSelectedOption}
+    contentLabel="Selectted Option"
+  >
+    <h3>Selected Option </h3>
+    {props.selectedOption && <p>{props.selectedOption}</p>}
+    
+    <button onClick={props.handleSelectedOption}>Okay</button>
+
+ </Modal>
+);
+
+export default OptionModal;
