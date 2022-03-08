0550f59ee4dc83513ecd7e3e2d4c99dffaf48b18
khalidreda <khalidaziz2198@gmail.com>
2022-03-08 18:49:51 +0200

    5m

--- 8.Styling React/CAP Section 8/69.7 Styling the Options List/indecision app/src/components/Options.js
+++ 8.Styling React/CAP Section 8/69.7 Styling the Options List/indecision app/src/components/Options.js
@@ -4,7 +4,11 @@ import Option from './Option';
 
 const Options = (props) => (
   <div>
-    <button className="button button--link" onClick={props.handleDeleteOptions}>Remove All</button>
+    <div className="widget-header">
+      <h3>Your Options</h3>
+      <button className="button button--link" onClick={props.handleDeleteOptions}>Remove All</button>
+
+    </div>
     {props.options.length === 0 && <p>Please add an option to get started!</p>}
     {
       props.options.map((option) => (
