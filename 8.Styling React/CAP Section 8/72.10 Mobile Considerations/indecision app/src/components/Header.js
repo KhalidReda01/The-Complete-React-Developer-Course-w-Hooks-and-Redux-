14413565f0d79759308a717f037c0f47f3b6b841
khalidreda <khalidaziz2198@gmail.com>
2022-03-09 21:33:28 +0200

    CAP  start lecture Mobile Considerations

---
+++ 8.Styling React/CAP Section 8/72.10 Mobile Considerations/indecision app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+const Header = (props) =>(
+  <div className="header">
+    <div className="container">
+    <h1 className="header__title">{props.title}</h1>
+    {props.subtitle && <h2 className='header__subtitle'>{props.subtitle}</h2>}
+    </div>
+  
+  </div>
+);
+export default Header
---
+++ 8.Styling React/CAP Section 8/72.10 Mobile Considerations/indecision app/src/components/OptionModal.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Modal from 'react-modal';
+const OptionModal = (props) => (
+  <Modal
+    isOpen={!!props.selectedOption}
+    onRequestClose={props.handleSelectedOption}
+    contentLabel="Selectted Option"
+    closeTimeoutMS={200}
+    className="modal"
+  >
+    <h3 className="modal__title">Selected Option </h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    
+    <button className="button" onClick={props.handleSelectedOption}>Okay</button>
+
+ </Modal>
+);
+
+export default OptionModal;
