aa0b011c42508ca3c7b4db4e019ef8c98d1e6de9
khalidreda <khalidaziz2198@gmail.com>
2022-03-09 21:11:45 +0200

    CFSC lecture Styling React-Modal NFA defination First use

--- 00/#CSMM/8.Styling React/2.CFSC/CFSC-1 Section 8/indecision app/src/components/OptionModal.js
+++ 00/#CSMM/8.Styling React/2.CFSC/CFSC-1 Section 8/indecision app/src/components/OptionModal.js
@@ -5,14 +5,16 @@ const OptionModal = (props) => (
     isOpen={!!props.selectedOption}
     onRequestClose={props.handleClearSelectOption}
     contentLabel="Selected Option"
+    closeTimeoutMS={200}
+    className="modal"
 
   
 
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
 
-    <button onClick={props.handleClearSelectOption}>Okay</button>
+    <button className="button" onClick={props.handleClearSelectOption}>Okay</button>
   </Modal>
 )
 export default OptionModal;
