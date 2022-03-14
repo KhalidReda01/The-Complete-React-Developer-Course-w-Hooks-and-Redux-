ce666875ffbc99fa6db2c370562a3c5c2c48cc6b
khalidreda <khalidaziz2198@gmail.com>
2022-03-14 17:46:41 +0200

    Start lecture 76 setting up Budget App Welcome back to the course Welcome part 2 Budget App after that the course is almost finished but only the hooks left

---
+++ 9.React-Router/CAP Section 9/76.3 Setting Up Budget App/indecision app/src/components/AddOptions.js
@@ -0,0 +1,36 @@
+import React from 'react'
+/**
+ * class properties 
+ * 
+ */
+class AddOption extends React.Component {
+  state = {
+    error: undefined
+  }
+ 
+  handleAddOption =(e)=> {
+    e.preventDefault();
+    // use devtool
+  //  console.log(testing)// you are still here OMG
+    const option = e.target.elements.option.value.trim();
+    const error = this.props.handleAddOption(option);
+
+    this.setState(() => ({ error }));
+
+    if (!error) {
+      e.target.elements.option.value = '';
+    }
+  }
+  render() {
+    return (
+      <div>
+        {this.state.error && <p className="add-option-error">{this.state.error}</p>}
+        <form className="add-option" onSubmit={this.handleAddOption}>
+          <input className="add-option__input" type="text" name="option" />
+          <button class="button">Add Option </button>
+        </form>
+      </div>
+    );
+  }
+}
+export default AddOption
