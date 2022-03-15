552c69f9ef506d58bc681013965fbfed1f8881b7
khalidreda <khalidaziz2198@gmail.com>
2022-03-15 21:39:26 +0200

    1st of this lecture

--- 9.React-Router/CAP Section 9/78.5 Setting up a 404/expensify-app/src/app.js
+++ 9.React-Router/CAP Section 9/78.5 Setting up a 404/expensify-app/src/app.js
@@ -27,6 +27,11 @@ const HelpPage = () => (
     This is from my Help Page component
   </div>
 )
+const NotFoundPage = () => (
+  <div>
+    404!
+  </div>
+)
 
 const routes = (
   <BrowserRouter>
@@ -34,7 +39,8 @@ const routes = (
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpensePage} />
-      <Route path="/help" component={HelpPage}/>
+      <Route path="/help" component={HelpPage} />
+      <Route component={NotFoundPage} />
     </div>      
   </BrowserRouter>
 )
