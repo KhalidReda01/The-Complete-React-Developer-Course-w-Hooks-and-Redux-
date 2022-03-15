a984efec428c45fd4024343b34eb7cd5053ea90c
khalidreda <khalidaziz2198@gmail.com>
2022-03-15 21:50:23 +0200

    CFSC done setting up a 404

--- 00/#CSMM/9.React-Router/2.CFSC/CFSC-1 Section 9/expensify-app/src/app.js
+++ 00/#CSMM/9.React-Router/2.CFSC/CFSC-1 Section 9/expensify-app/src/app.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter,Route } from 'react-router-dom';
+import { BrowserRouter,Route,Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
@@ -26,17 +26,23 @@ const HelpPage = () => (
     This is my Help Page compoent
   </div>
 )
+const NotFoundPage = () => (
+  <div>
+    404!
+  </div>
+)
 
 
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true}/>
       <Route path="/create" component={AddExpensePage}/>
       <Route path="/edit" component={EditExpensePage}/>
-      <Route path="/help" component={HelpPage}/>
+      <Route path="/help" component={HelpPage} />
+      <Route component={NotFoundPage}/>
       
-    </div>
+    </Switch>
   </BrowserRouter>
 )
 ReactDOM.render(routes, document.getElementById('app'));
