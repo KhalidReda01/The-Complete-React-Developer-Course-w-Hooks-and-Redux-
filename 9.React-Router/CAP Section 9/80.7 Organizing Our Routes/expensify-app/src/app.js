ed16cb4a6c73866cc068faa9f0e6b518c26c4c05
khalidreda <khalidaziz2198@gmail.com>
2022-03-16 00:11:08 +0200

    Recap the lecture Now CFSC  to finish it

--- 9.React-Router/CAP Section 9/80.7 Organizing Our Routes/expensify-app/src/app.js
+++ 9.React-Router/CAP Section 9/80.7 Organizing Our Routes/expensify-app/src/app.js
@@ -7,7 +7,12 @@ import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
-
+/**
+ * Recap what I did 
+ * first the point of this lecture is to split our big file to simple small componet as you did with the old project Indecision app
+ * so I created a routers foler indie of it appRouter.js then cut our code to it then import it at app.js and ofcourse render it the AppRotuer copmoent to the screen the dom
+ * second I split every statless function compoent at its own file and thats an easy process that's the lecture so done 
+ */
 
 
 ReactDOM.render(<AppRouter/>, document.getElementById('app'));
