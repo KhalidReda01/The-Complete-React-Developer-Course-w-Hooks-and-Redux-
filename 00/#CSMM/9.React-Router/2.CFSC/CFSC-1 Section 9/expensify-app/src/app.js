09b245b6293251da9d46ccee746b9ea08e8e373c
khalidreda <khalidaziz2198@gmail.com>
2022-03-15 21:12:41 +0200

    CfSC done for lecture 77 React0router 101

--- 00/#CSMM/9.React-Router/2.CFSC/CFSC-1 Section 9/expensify-app/src/app.js
+++ 00/#CSMM/9.React-Router/2.CFSC/CFSC-1 Section 9/expensify-app/src/app.js
@@ -2,9 +2,41 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter,Route } from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
+const ExpenseDashboardPage = () => (
+  <div>
+    This is my dashboard compoent
+  </div>
+)
+const AddExpensePage = () => (
+  <div>
+    This is my Add Expense Page compoent
+  </div>
+)
+const EditExpensePage = () => (
+  <div>
+    This is my Edit Expense Page compoent
+  </div>
+)
+const HelpPage = () => (
+  <div>
+    This is my Help Page compoent
+  </div>
+)
 
 
-ReactDOM.render(<p>This is my boilerplate </p>, document.getElementById('app'));
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+      <Route path="/create" component={AddExpensePage}/>
+      <Route path="/edit" component={EditExpensePage}/>
+      <Route path="/help" component={HelpPage}/>
+      
+    </div>
+  </BrowserRouter>
+)
+ReactDOM.render(routes, document.getElementById('app'));
