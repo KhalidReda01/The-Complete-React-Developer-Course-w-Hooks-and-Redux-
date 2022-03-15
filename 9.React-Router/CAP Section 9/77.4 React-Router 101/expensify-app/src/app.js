382d4463d470797dcc131f793ac8276514c51683
khalidreda <khalidaziz2198@gmail.com>
2022-03-15 20:21:21 +0200

    5m

--- 9.React-Router/CAP Section 9/77.4 React-Router 101/expensify-app/src/app.js
+++ 9.React-Router/CAP Section 9/77.4 React-Router 101/expensify-app/src/app.js
@@ -1,10 +1,19 @@
-
+/**
+ * React-router 101
+ * // install the library
+ * yarn add react-router-dom@4.2.2 there 
+ * https://v5.reactrouter.com/web/guides/philosophy
+ * 
+ */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter,Route} from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-
+const routes = (
+  <BrowserRouter></BrowserRouter>
+)
 
 ReactDOM.render(<p>This is my boilerplate </p>, document.getElementById('app'));
