77af0aa5d8064323d69b6db85eb8adc65748a631
khalidreda <khalidaziz2198@gmail.com>
2022-03-16 00:06:11 +0200

    2nd last part 12m

--- 9.React-Router/CAP Section 9/80.7 Organizing Our Routes/expensify-app/src/components/NotFoundPage.js
+++ 9.React-Router/CAP Section 9/80.7 Organizing Our Routes/expensify-app/src/components/NotFoundPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 const NotFoundPage = () => (
   <div>
     404! -<Link to ="/">Go home</Link>
--- 9.React-Router/CAP Section 9/80.7 Organizing Our Routes/expensify-app/src/routers/AppRouter.js
+++ 9.React-Router/CAP Section 9/80.7 Organizing Our Routes/expensify-app/src/routers/AppRouter.js
@@ -1,33 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import AddExpensePage from '../components/AddExpensePage';
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
+import Header from '../components/Header';
 // Challenge time
 // Create 6 new files for the 6 componets
 // setup imports , component , default export 
 // import into AppRouter so they can be used 
 
-
-
-
-
-const NotFoundPage = () => (
-  <div>
-    404! -<Link to ="/">Go home</Link>
-  </div>
-)
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense </NavLink>
-    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-  </header>
-)
 const AppRouter = () => (
    <BrowserRouter>
     <div>
