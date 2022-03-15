85d01546e701428c4eca041440291ea34894487b
khalidreda <khalidaziz2198@gmail.com>
2022-03-15 21:16:13 +0200

    CAP lecture 78 Setting up a 404 start

---
+++ 9.React-Router/CAP Section 9/78.5 Setting up a 404/expensify-app/src/app.js
@@ -0,0 +1,42 @@
+/**
+ * 78 Setting Up a 404
+ * 
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter,Route} from 'react-router-dom'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+);
+const AddExpensePage = () => (
+  <div>
+    This is from my add expense component
+  </div>
+)
+const EditExpensePage = () => (
+  <div>
+    This is from my Edit Expense component
+  </div>
+)
+const HelpPage = () => (
+  <div>
+    This is from my Help Page component
+  </div>
+)
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage}/>
+    </div>      
+  </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'));
