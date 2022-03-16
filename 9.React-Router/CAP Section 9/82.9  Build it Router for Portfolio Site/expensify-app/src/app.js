ee161a23c7e2352d5a6741ea6ec88e91dca7975d
khalidreda <khalidaziz2198@gmail.com>
2022-03-16 04:16:57 +0200

    Start Build it Router for Portfolio Site

---
+++ 9.React-Router/CAP Section 9/82.9  Build it Router for Portfolio Site/expensify-app/src/app.js
@@ -0,0 +1,12 @@
+/**
+ * 81 Query Strings and URL Parameters
+ * 
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+
+ReactDOM.render(<AppRouter/>, document.getElementById('app'));
