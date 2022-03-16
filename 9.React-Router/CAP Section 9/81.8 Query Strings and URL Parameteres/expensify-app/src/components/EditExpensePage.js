e8f64f8a1134ad52d855dad640e5f7f741132563
khalidreda <khalidaziz2198@gmail.com>
2022-03-16 03:55:24 +0200

    1st part 6m

--- 9.React-Router/CAP Section 9/81.8 Query Strings and URL Parameteres/expensify-app/src/components/EditExpensePage.js
+++ 9.React-Router/CAP Section 9/81.8 Query Strings and URL Parameteres/expensify-app/src/components/EditExpensePage.js
@@ -1,7 +1,59 @@
 import React from "react";
-const EditExpensePage = () => (
+const EditExpensePage = (props) => {
+console.log(props)
+  return (
   <div>
     This is from my Edit Expense component
   </div>
 )
+} 
 export default EditExpensePage;
+/**
+ * http://localhost:8080/edit?query=rent&sort=date
+ * {
+    "match": {
+        "path": "/edit",
+        "url": "/edit",
+        "isExact": true,
+        "params": {}
+    },
+    "location": {
+        "pathname": "/edit",
+        "search": "?query=rent&sort=date",
+        "hash": ""
+    },
+    "history": {
+        "length": 5,
+        "action": "POP",
+        "location": {
+            "pathname": "/edit",
+            "search": "?query=rent&sort=date",
+            "hash": ""
+        }
+    }
+}
+
+// http://localhost:8080/edit#contact-us
+{
+    "match": {
+        "path": "/edit",
+        "url": "/edit",
+        "isExact": true,
+        "params": {}
+    },
+    "location": {
+        "pathname": "/edit",
+        "search": "",
+        "hash": "#contact-us"
+    },
+    "history": {
+        "length": 6,
+        "action": "POP",
+        "location": {
+            "pathname": "/edit",
+            "search": "",
+            "hash": "#contact-us"
+        }
+    }
+}
+ */
