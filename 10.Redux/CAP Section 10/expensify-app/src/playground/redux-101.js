16f5d3a0d35c130dfeef75ac299bdd57c9f73680
khalidreda <khalidaziz2198@gmail.com>
2022-03-18 05:28:07 +0200

    1st par 5m installing Redux but I don't have an interent connection now so keep going

---
+++ 10.Redux/CAP Section 10/expensify-app/src/playground/redux-101.js
@@ -0,0 +1,11 @@
+/**
+ * yarn add redux@3.7.2
+ */
+import { createStore } from 'redux';
+const store = createStore((state={count:0}) => {
+  return state;
+});
+this.setState((prevState) => {
+  return prevState;
+})
+console.log('101')
