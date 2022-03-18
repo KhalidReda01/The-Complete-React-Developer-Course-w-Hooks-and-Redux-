7c072fe5d7ac855b5e1fc888205898aeb1db14e9
khalidreda <khalidaziz2198@gmail.com>
2022-03-18 07:22:56 +0200

    1st part 4m

--- 10.Redux/CAP Section 10/88.6 ES6 Object Destructuring/expensify-app/src/playground/destructuring.js
+++ 10.Redux/CAP Section 10/88.6 ES6 Object Destructuring/expensify-app/src/playground/destructuring.js
@@ -1,1 +1,13 @@
-console.log('destructuring')
+const person = {
+  name: 'Khalid',
+  age: 23,
+  location: {
+    city: 'Mansura',
+    temp:19
+  }
+}
+const name = person.name;
+const age = person.age;
+// so that's why we need es6 destructuring
+// console.log(`${person.name} is ${person.age}.`)
+console.log(`${person.name} is ${person.age}.`)
