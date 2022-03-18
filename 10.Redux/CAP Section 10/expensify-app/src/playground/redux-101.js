20a0470b721d27b72f96d81a7666737c7d75aa63
khalidreda <khalidaziz2198@gmail.com>
2022-03-18 05:34:01 +0200

    Interent at Egypt Sucks Thanks for our Gov

--- 10.Redux/CAP Section 10/expensify-app/src/playground/redux-101.js
+++ 10.Redux/CAP Section 10/expensify-app/src/playground/redux-101.js
@@ -5,7 +5,29 @@ import { createStore } from 'redux';
 const store = createStore((state={count:0}) => {
   return state;
 });
-this.setState((prevState) => {
-  return prevState;
-})
+// this.setState((prevState) => {
+//   return prevState;
+// })
+console.log(store.getState())// Object {count:0}
 console.log('101')
+/**
+ * yarn add redux@3.7.2                                                    s comm
+yarn add v1.22.5
+[1/4] Resolving packages...
+info There appears to be trouble with your network connection. Retrying...ct-Dev
+                                                                          in)   
+info There appears to be trouble with your network connection. Retrying...
+yarn add v1.22.5
+[1/4] Resolving packages...
+info There appears to be trouble with your network connection. Retrying...      
+                                                                                
+info There appears to be trouble with your network connection. Retrying...      
+                                                                                
+info There appears to be trouble with your network connection. Retrying...      
+
+info There appears to be trouble with your network connection. Retrying...
+info There appears to be trouble with your network connection. Retrying...
+info There appears to be trouble with your network connection. Retrying...info There appears to be trouble with your network connection. Retrying...error An unexpected error occurred: "https://registry.yarnpkg.com/redux: ESOCKETTIMEDOUT".
+info If you think this is a bug, please open a bug report with the information provided in "C:\\Users\\khalid\\Desktop\\Current Projects\\The-Complete-React-Developer-Course-w-Hooks-and-Redux-\\10.Redux\\CAP Section 10\\expensify-app\\yarn-error.log".
+info Visit https://yarnpkg.com/en/docs/cli/add for documentation about this command.
+ */
