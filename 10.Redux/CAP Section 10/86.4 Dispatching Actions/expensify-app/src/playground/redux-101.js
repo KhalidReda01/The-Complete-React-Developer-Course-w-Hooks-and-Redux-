2aab8a84a64408634f8fca62e3b7287f628b6daa
khalidreda <khalidaziz2198@gmail.com>
2022-03-18 06:19:40 +0200

    1st part 5m New way of study section Finish WTC RSC CAP WCN DEES then other at the next day I don't think that it's a good idea but I will try ti

--- 10.Redux/CAP Section 10/86.4 Dispatching Actions/expensify-app/src/playground/redux-101.js
+++ 10.Redux/CAP Section 10/86.4 Dispatching Actions/expensify-app/src/playground/redux-101.js
@@ -1,8 +1,29 @@
 import { createStore } from 'redux';
-const store = createStore((state={count:0}) => {
+const store = createStore((state = { count: 0 }, action) => {
+  if (action.type === "INCREMENT") {
+    return {
+      count:state.count+1
+    }
+  } else {
+    return state;
+  }
+  console.log('running')
   return state;
 });
 
 console.log(store.getState())
-console.log('101')
+// Actions - than an object that gets sent to the store
+// walk.stop_walking , sit work, stop_working
+// increment , decrement,reset
+
+
+// I'd like to increment the count
+store.dispatch(
+  {
+    type:'INCREMENT'
+  }
+)
+
+// I'd like to reset the count to zero
+console.log(store.getState())
 
