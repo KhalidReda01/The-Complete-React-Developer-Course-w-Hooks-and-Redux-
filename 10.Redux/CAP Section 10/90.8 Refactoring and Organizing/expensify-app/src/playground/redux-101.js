3bc07b570449dc6afb95d026a1c358b66efa234b
khalidreda <khalidaziz2198@gmail.com>
2022-03-18 08:38:54 +0200

    2nd part 9m

--- 10.Redux/CAP Section 10/90.8 Refactoring and Organizing/expensify-app/src/playground/redux-101.js
+++ 10.Redux/CAP Section 10/90.8 Refactoring and Organizing/expensify-app/src/playground/redux-101.js
@@ -1,6 +1,11 @@
 import { createStore } from 'redux';
 // Action generators
-
+// const add = (data) => {
+const add = ({a,b},c) => {
+  // return data.a + data.b;
+  return a + b+c;
+}
+console.log(add({a:1,b:12}))
 const incrementCount = (payload={}) => ({
   type: 'INCREMENT',
   incrementBy:typeof payload.incrementBy==='number'? payload.incrementBy:1
@@ -9,9 +14,8 @@ const incrementCount = (payload={}) => ({
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + action.incrementBy
       
       };
     case 'DECREMENT':
