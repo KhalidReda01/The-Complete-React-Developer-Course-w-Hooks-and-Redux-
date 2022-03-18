042a4c73364bb16aad925739d0c75bfc09d3fcc6
khalidreda <khalidaziz2198@gmail.com>
2022-03-18 05:48:53 +0200

    Finally I installed it even with this 10kb interent connection speed after cut all other devices connected yes Now I can keep code and learn Redux

--- 10.Redux/CAP Section 10/expensify-app/src/playground/redux-101.js
+++ 10.Redux/CAP Section 10/expensify-app/src/playground/redux-101.js
@@ -10,17 +10,7 @@ const store = createStore((state={count:0}) => {
 // })
 console.log(store.getState())// Object {count:0}
 console.log('101')
-// Can't CAP now because of the data is zero Really I need to install redux
-// for 1 hour now can't install it It's friday Okay keep going and code with him as long as you want to do that even if you didn't install redux I will come back to it but I will not waste my time I want to code now
-// that's it keep going and for the challenge time comes and without Redux installed It's impossible to solve it so There is not choice to wait to Interent come back I hate this what I'm saying no one gonna see what I'm writing and if see it I don't care Who care ahhahaahhhha
-/**
- * $ yarn add redux@3.7.2
-yarn add v1.22.5
-[1/4] Resolving packages...
-info There appears to be trouble with your network connection. Retrying...
-info There appears to be trouble with your network connection. Retrying...
-info There appears to be trouble with your network connection. Retrying...
-info There appe
-OMG
 
- */
+// Actions
+// I'd like to increment the count 
+// I'd like to reset the cout to zero 
