cf88f0ed57d7c4a053605a06688aaf8cc43bfff9
khalidreda <khalidaziz2198@gmail.com>
2022-03-18 09:56:36 +0200

    2nd part 10m

---
+++ 10.Redux/CAP Section 10/91.9 Reducers/expensify-app/src/playground/redux-expensify.js
@@ -0,0 +1,13 @@
+import { createStore ,combineReducers } from "redux";
+const demoState = {
+  expenses: [{
+    id: 'pdfdfasdf',
+    description: 'January Rent',
+    note: "This was the final payment for that address OKay Loda",
+    amount: 54500,
+    createdAt:0
+  }],
+  filters: {
+    text:'rent'
+  }
+}
