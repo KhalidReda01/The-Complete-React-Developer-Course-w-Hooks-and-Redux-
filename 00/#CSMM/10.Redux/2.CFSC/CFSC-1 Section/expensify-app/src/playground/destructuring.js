ea4234968eaee6ed576a4e59d53d424a1d58c44b
khalidreda <khalidaziz2198@gmail.com>
2022-03-19 14:07:09 +0200

    CFSC-CFM ES6 Array Destructuring

--- 00/#CSMM/10.Redux/2.CFSC/CFSC-1 Section/expensify-app/src/playground/destructuring.js
+++ 00/#CSMM/10.Redux/2.CFSC/CFSC-1 Section/expensify-app/src/playground/destructuring.js
@@ -22,5 +22,14 @@
 // const {name:publisherName="Self-published"}=book.publisher
 // console.log(publisherName);// Penguin / Self-Published
 
+const address = ['15 Imam Stree', 'Mark Minia Elnasr ', 'Mansura', '1235434']
+// console.log(`you are in ${address[1]} ${address[2]}`)
+const [,city,state]=address
+console.log(`you are in ${city} ${state}`)
 
+// Now the challene time 
+const item = ['Coffee(hot)', '$2.00', '$2.55', '$2.75'];
+const [itemName,, meduimPrice] = item
 
+console.log(`A ${itemName} costs ${meduimPrice}`)
+// console.log(`A meduim coffee (hot) costs $2.50`)
