30d2cea0fa63114c71f0e8c01ee1df8ada86f670
khalidreda <khalidaziz2198@gmail.com>
2022-03-19 10:19:37 +0200

    CAP2 done lecture recap too Subscribing and Dynamic Actions

--- 00/#CSMM/10.Redux/1.CAP/CAP-2 Section 10/expensify-app/src/playground/redux-101.js
+++ 00/#CSMM/10.Redux/1.CAP/CAP-2 Section 10/expensify-app/src/playground/redux-101.js
@@ -2,7 +2,6 @@
 import { createStore } from 'redux';
 
 const store = createStore((state = { count: 0 }, action) => {
-  console.log(action)
   switch (action.type) {
     case 'INCREMENT':
       const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
@@ -10,8 +9,13 @@ const store = createStore((state = { count: 0 }, action) => {
         count: state.count + incrementBy
       };
     case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count:state.count -1
+        count: state.count - decrementBy
+      };
+    case 'SET':
+      return {
+        count:action.count
       }
     case 'RESET':
       return {
@@ -38,18 +42,32 @@ store.dispatch({
   type: 'INCREMENT',
   
 })
+store.dispatch({
+  type: 'INCREMENT',
+  
+})
+store.dispatch({
+  type:'RESET'
+})
 
 store.dispatch({
   type:'DECREMENT'
 })
-// Challenge Time 
-
-
 store.dispatch({
-  type:'DECREMENT'
+  type: 'DECREMENT',
+  decrementBy:10
+})
+store.dispatch({
+  type: 'SET',
+  count:101
 })
 
-// store.dispatch({
-//   type:'RESET'
-// })
+
+/**
+ * Recap what I did lecture subscribing and Dynamic Actions
+ * first up I leant how to  watch for changes to the store by using the subsribe method and pass the functinon that will call every time the store changed I got it  
+ * even the return function from this store is unsubscribe function  you can call it 
+ * the other thing is learnt about pass dynamic information along inside the action object the store Any way keep going now I fully absorved this lecture but Hey never miss CFSC-CFM 
+ * 
+ */
 
