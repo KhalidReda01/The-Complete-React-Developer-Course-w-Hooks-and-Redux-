09ec3bd7c6fb229e3f6fec7d29ced7570f1b66d7
khalidreda <khalidaziz2198@gmail.com>
2022-03-19 08:48:05 +0200

    DEES Dispaching actions but I found it and this only record

--- 00/#CSMM/10.Redux/2.CFSC/CFSC-1 Section/expensify-app/src/playground/redux-101.js
+++ 00/#CSMM/10.Redux/2.CFSC/CFSC-1 Section/expensify-app/src/playground/redux-101.js
@@ -1,5 +1,28 @@
 import { createStore } from "redux";
-const store = createStore((state={count:10}) => {
-  return state
+const store = createStore((state = { count: 0 }, actions) => {
+  switch (actions.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    
+    default:
+      return { state }
+      
+  }
+
 })
+console.log(store.getState())
+store.dispatch({
+  type:'INCREMENT'
+})
+store.dispatch({
+  type:'DECREMENT'
+})
+
+
+
+// store.dispatch({
+//   type:'RESET'
+// })
 console.log(store.getState())
