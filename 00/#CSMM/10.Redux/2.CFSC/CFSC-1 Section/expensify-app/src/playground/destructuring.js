28d92a262dacb2a56fbd84f5253d2809569d3fa3
khalidreda <khalidaziz2198@gmail.com>
2022-03-19 13:57:45 +0200

    ES6 Array Destructuring CFSC-CFM start

--- 00/#CSMM/10.Redux/2.CFSC/CFSC-1 Section/expensify-app/src/playground/destructuring.js
+++ 00/#CSMM/10.Redux/2.CFSC/CFSC-1 Section/expensify-app/src/playground/destructuring.js
@@ -1,23 +1,26 @@
-const Person = {
-  name: 'khalid',
-  age: 23,
-  location: {
-    city: 'Mansura',
-    temp:29
-  }
-}
-const {name:firstName="Anonmous",age }=Person
-// const name = Person.name;
-// const age = Person.age;
-// console.log(`${Person.name} is ${Person.age}`)
-console.log(`${firstName} is ${age}`)
-// the Challenge 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    // name:'Penguin'
-  }
-}
-const {name:publisherName="Self-published"}=book.publisher
-console.log(publisherName);// Penguin / Self-Published 
+// const Person = {
+//   name: 'khalid',
+//   age: 23,
+//   location: {
+//     city: 'Mansura',
+//     temp:29
+//   }
+// }
+// const {name:firstName="Anonmous",age }=Person
+// // const name = Person.name;
+// // const age = Person.age;
+// // console.log(`${Person.name} is ${Person.age}`)
+// console.log(`${firstName} is ${age}`)
+// // the Challenge
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     // name:'Penguin'
+//   }
+// }
+// const {name:publisherName="Self-published"}=book.publisher
+// console.log(publisherName);// Penguin / Self-Published
+
+
+
