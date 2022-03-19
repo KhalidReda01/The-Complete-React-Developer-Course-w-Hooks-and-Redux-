61d3f05ccbec5a856db26b29964263090cc69784
khalidreda <khalidaziz2198@gmail.com>
2022-03-19 05:23:36 +0200

    First CAP done for this section Conrats and thats in less than one day you finish the enitire sectin but you understand nothing hahahha why because you didn't study it you just WTV CAP now you have to repeat it today and tomrrow again Redux is a fundamental topic so Give it the time and effort that needs Okay keep going

--- 10.Redux/CAP Section 10/97.15  Sorting Redux Data/expensify-app/src/playground/redux-expensify.js
+++ 10.Redux/CAP Section 10/97.15  Sorting Redux Data/expensify-app/src/playground/redux-expensify.js
@@ -135,6 +135,8 @@ const getVisibleExpenses = (expenses,{text,sortBy,startDate,endDate}) => {
   }).sort((a,b) => {
     if (sortBy === 'date') {
       return a.createdAt < b.createdAt? 1:-1
+    } else if (sortBy === 'amount') {
+      return a.amount <b.amount ? 1:-1
     }
   })
 }
