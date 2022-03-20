96a3056411c64ff7e7d3414c6b2cfb0b23e57270
khalidreda <khalidaziz2198@gmail.com>
2022-03-20 09:02:08 +0200

    CFSC-CfM lecture done 92/10 Working with multiple Reducers

--- 00/#CSMM/10.Redux/2.CFSC/CFSC-1 Section/expensify-app/src/playground/redux-expensify.js
+++ 00/#CSMM/10.Redux/2.CFSC/CFSC-1 Section/expensify-app/src/playground/redux-expensify.js
@@ -1,4 +1,42 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
+//ADD_EXPENSE
+//REMOVE_EXPENSE
+//EDIT_EXPENSE
+//SET_TEXT_FILTER
+//SORT_BY_AMOUNT
+//SET_START_DATE
+//SET_END_DATE
+
+//Expense Reducer
+const expenseReducerDefaultState=[]
+const expenseReducer = (state = expenseReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+//Filter Reducer
+const filterReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate:undefined
+}
+const filterReducer=(state = filterReducerDefaultState,action)=> {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+// store creation
+const store = createStore(
+  combineReducers({
+    expenses: expenseReducer,
+    filters:filterReducer
+      }
+  
+))
+console.log(store.getState())
 const demoState = {
   expenses: [{
     id: 'adfadfd',
