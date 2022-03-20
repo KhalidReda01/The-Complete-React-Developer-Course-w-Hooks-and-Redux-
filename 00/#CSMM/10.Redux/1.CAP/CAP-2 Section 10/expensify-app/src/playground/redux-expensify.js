5cd92b98f48197d2ddd51bef69a6e6137abf72ab
khalidreda <khalidaziz2198@gmail.com>
2022-03-20 07:38:15 +0200

    1st part 5m CAP2-WVI Working with multiple Reducers

--- 00/#CSMM/10.Redux/1.CAP/CAP-2 Section 10/expensify-app/src/playground/redux-expensify.js
+++ 00/#CSMM/10.Redux/1.CAP/CAP-2 Section 10/expensify-app/src/playground/redux-expensify.js
@@ -1,4 +1,25 @@
 import { createStore, combineReducers } from "redux";
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT-FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+// Expenses Reducer
+const expensesReducerDefaultState=[]
+const expensesReducer=(state = expensesReducerDefaultState, action)=>{
+  switch (action.type) {
+    default:
+      return state;
+  }
+  
+}
+// Store Creation
+const store = createStore(expensesReducer)
+console.log(store.getState())
 const demoState = {
   expenses: [{
     id: 'afdafd',
