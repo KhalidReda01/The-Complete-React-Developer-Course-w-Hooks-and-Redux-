4ff65dae35e6cda5e3c874a216be3121b1ee9ed2
khalidreda <khalidaziz2198@gmail.com>
2022-03-20 05:25:07 +0200

    Great this section need that CfSC-CFM

--- 00/#CSMM/10.Redux/2.CFSC/CFSC-1 Section/expensify-app/src/playground/redux-101.js
+++ 00/#CSMM/10.Redux/2.CFSC/CFSC-1 Section/expensify-app/src/playground/redux-101.js
@@ -1,10 +1,23 @@
 import { createStore } from "redux";
+
+// Action generators are function that return action object 
+const incrementCount = (payload={}) => {
+  console.log(payload.incrementBy)
+  return {
+    type: 'INCREMENT',
+    incrementBy: typeof payload.incrementBy === 'number' ? payload.incrementBy : 1
+  }
+  
+}
+
 const store = createStore((state = { count: 0 }, actions) => {
   switch (actions.type) {
     case 'INCREMENT':
-      const incrementBy = typeof actions.incrementBy === 'number' ? actions.incrementBy : 1;
+      console.log(actions)
+
+      // const incrementBy = typeof actions.incrementBy === 'number' ? actions.incrementBy : 1;
       return {
-        count: state.count + incrementBy
+        count: state.count + actions.incrementBy
       };
     case 'DECREMENT':
       const decrementBy= typeof actions.decrementBy==='number'?actions.decrementBy:1
@@ -32,45 +45,47 @@ const store = createStore((state = { count: 0 }, actions) => {
   console.log(store.getState())
 })
 
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy:5
-})
-// unsubsribe()
-store.dispatch({
-  type:'DECREMENT'
-})
+store.dispatch(incrementCount())
+store.dispatch(incrementCount({incrementBy:20}))
+// store.dispatch({
+//   type: 'INCREMENT',
+//   incrementBy:5
+// })
+// // unsubsribe()
+// store.dispatch({
+//   type:'DECREMENT'
+// })
 
-store.dispatch({
-  type:'DECREMENT'
-})
+// store.dispatch({
+//   type:'DECREMENT'
+// })
 
-store.dispatch({
-  type:'DECREMENT'
-})
+// store.dispatch({
+//   type:'DECREMENT'
+// })
 
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy:10
-})
+// store.dispatch({
+//   type: 'DECREMENT',
+//   decrementBy:10
+// })
 
-store.dispatch({
-  type:'RESET'
-})
+// store.dispatch({
+//   type:'RESET'
+// })
 
-store.dispatch({
-  type:'DECREMENT'
-})
+// store.dispatch({
+//   type:'DECREMENT'
+// })
 
 
 
-store.dispatch({
-  type:'RESET'
-})
-store.dispatch({
-  type: 'SET',
-  count:101
-})
+// store.dispatch({
+//   type:'RESET'
+// })
+// store.dispatch({
+//   type: 'SET',
+//   count:101
+// })
 store.subscribe(() => {
   console.log(store.getState())
 })
