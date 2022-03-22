9644689946b299cc09c1b8441859c6549d01a058
khalidreda <khalidaziz2198@gmail.com>
2022-03-22 11:05:09 +0200

    Challenge Time STC 2nd part 11:55 m

--- 11.React With Redux/CAP Section 11/103.6  Controlled Inputs for Filters/expensify-app/src/components/ExpenseListFilters.js
+++ 11.React With Redux/CAP Section 11/103.6  Controlled Inputs for Filters/expensify-app/src/components/ExpenseListFilters.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { setTextFilter } from '../actions/filters';
 const ExpenseListFilters = (props) => (
   <div>
     <input type="text" value={props.filters.text} onChange={(e) => {
+      props.dispatch(setTextFilter(e.target.value))
       console.log(e.target.value)
     } }/>
 
--- 11.React With Redux/CAP Section 11/103.6  Controlled Inputs for Filters/expensify-app/src/components/ExpenseListItem.js
+++ 11.React With Redux/CAP Section 11/103.6  Controlled Inputs for Filters/expensify-app/src/components/ExpenseListItem.js
@@ -4,6 +4,8 @@ const ExpenseListItem = ({description,amount,createdAt}) => (
   <div>
     <h3>{description}</h3>
     <p>{amount} - {createdAt}</p>
+    <button>Remove</button>
   </div>
 )
 export default ExpenseListItem;
+// Challenge Time
