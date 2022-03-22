2b77803d4fa09aa6d675951a0434166d6160781d
khalidreda <khalidaziz2198@gmail.com>
2022-03-22 11:10:53 +0200

    Lecture CAP done Controlled Inputs for Filters U<10%_A_1stCAP_ESS

--- 11.React With Redux/CAP Section 11/103.6  Controlled Inputs for Filters/expensify-app/src/components/ExpenseListItem.js
+++ 11.React With Redux/CAP Section 11/103.6  Controlled Inputs for Filters/expensify-app/src/components/ExpenseListItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({dispatch,description,amount,createdAt}) => (
+const ExpenseListItem = ({dispatch,id,description,amount,createdAt}) => (
   <div>
     <h3>{description}</h3>
     <p>{amount} - {createdAt}</p>
