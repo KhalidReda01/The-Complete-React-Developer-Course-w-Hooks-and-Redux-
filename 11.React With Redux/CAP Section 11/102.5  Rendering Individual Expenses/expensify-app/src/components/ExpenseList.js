ad7f24dfae80c2c4d6a0fa716e7eeb26486290c2
khalidreda <khalidaziz2198@gmail.com>
2022-03-22 09:20:33 +0200

    find the error and don't worry this is the first CAP you gonna aborb this but today you have to finish the section then study it you will not go to section 12 never between master Redux sectin Forget the time Now focus on the problem and find it

--- 11.React With Redux/CAP Section 11/102.5  Rendering Individual Expenses/expensify-app/src/components/ExpenseList.js
+++ 11.React With Redux/CAP Section 11/102.5  Rendering Individual Expenses/expensify-app/src/components/ExpenseList.js
@@ -4,10 +4,11 @@ import ExpenseListItem from './ExpenseListItem';
 const ExpenseList = (props) => (
   <div>
     <h1>Expense List</h1>
-    
-    {/* {props.expenses.map((expense) => {
-      return <ExpenseListItem key ={expense.id} expense={...expense }/>
-    })} */}
+    <ExpenseListItem/>
+    {props.expenses.map((expense) => {
+    return <ExpenseListItem/>
+      {/* return <ExpenseListItem key ={expense.id} expense={...expense }/> */}
+    })}
   </div>
 )
 const mapStateToProps=(state) => {
--- 11.React With Redux/CAP Section 11/102.5  Rendering Individual Expenses/expensify-app/src/components/ExpenseListItem.js
+++ 11.React With Redux/CAP Section 11/102.5  Rendering Individual Expenses/expensify-app/src/components/ExpenseListItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ExpenseListItem = ({description,amount,createdAt}) => (
   <div>
     <h3>{description}</h3>
-    <p>{amount} -{createdAt}</p>
+    <p>{amount} - {createdAt}</p>
   </div>
 )
 export default ExpenseListItem;
