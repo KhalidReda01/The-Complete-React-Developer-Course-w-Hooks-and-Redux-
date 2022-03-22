1c52ddc9fed697096844ab04f229342bce7a27e5
khalidreda <khalidaziz2198@gmail.com>
2022-03-22 08:01:39 +0200

    1st 4m

--- 11.React With Redux/CAP Section 11/100.3  The Higher Order Component/expensify-app/src/app.js
+++ 11.React With Redux/CAP Section 11/100.3  The Higher Order Component/expensify-app/src/app.js
@@ -24,4 +24,5 @@ const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses)
 console.log(store.getState());
 
-ReactDOM.render(<AppRouter/>, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
+
--- 11.React With Redux/CAP Section 11/100.3  The Higher Order Component/expensify-app/webpack.config.js
+++ 11.React With Redux/CAP Section 11/100.3  The Higher Order Component/expensify-app/webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/playground/hoc.js',
   output: {
      path: path.join(__dirname,'public'),
   filename:'bundle.js'
