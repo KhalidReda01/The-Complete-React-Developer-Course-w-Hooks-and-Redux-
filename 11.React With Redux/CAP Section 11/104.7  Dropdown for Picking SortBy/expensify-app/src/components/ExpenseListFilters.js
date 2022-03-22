ba631cad58bfe69b2887c385b45036042c618210
khalidreda <khalidaziz2198@gmail.com>
2022-03-22 11:46:50 +0200

    Lecture CAP done Dropdown for Picking sortBy hidden bug start clean SC U_<5%_A1stCAP_ES-session

--- 11.React With Redux/CAP Section 11/104.7  Dropdown for Picking SortBy/expensify-app/src/components/ExpenseListFilters.js
+++ 11.React With Redux/CAP Section 11/104.7  Dropdown for Picking SortBy/expensify-app/src/components/ExpenseListFilters.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { setTextFilter } from '../actions/filters';
+import { setTextFilter,sortByDate,sortByAmount } from '../actions/filters';
 const ExpenseListFilters = (props) => (
   <div>
-    <input type="text" value={props.filters.text} onChange={(e) => {
+    <input type="text"
+      value={props.filters.text}
+      onChange={(e) => {
       props.dispatch(setTextFilter(e.target.value))
-      // console.log(e.target.value)
-    } }/>
-    <select>
+     
+      }}
+      
+    />
+    <select
+      value={props.filters.sortBy}
+      onChange={(e) => {
+        if (e.target.value === 'date') {
+          props.dispatch(sortByDate())
+        } else if (e.target === 'amount') {
+          props.dispatch(sortByAmount())
+       }
+      }}
+    >
       <option value="date">Date</option>
       <option value="amount">Amount</option>
     </select>
   </div>
 )
-//Challenge Time
-//Setup value and onChange for select
+
 
 const mapStateToProps = (state) => {
   return {
