a6e31071fa045d16d80e21a7ecbb4723dca9d908
khalidreda <khalidaziz2198@gmail.com>
2022-03-22 08:24:13 +0200

    CAP start lecture 101/4 Connecting Store and component With React-Redux

---
+++ 11.React With Redux/CAP Section 11/101.4  Connecting Store and Component With React-Redux/expensify-app/src/app.js
@@ -0,0 +1,28 @@
+
+/**
+ * CAP 1st Section 11 React With Redux
+ * 101/4 Connecting Store and Component With React-Redux 
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+const store = configureStore();
+store.dispatch(addExpense({description:'Water bill '}))
+store.dispatch(addExpense({ description: 'Gas bill ' }))
+
+
+store.dispatch(setTextFilter('water'));
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(visibleExpenses)
+console.log(store.getState());
+
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
+
