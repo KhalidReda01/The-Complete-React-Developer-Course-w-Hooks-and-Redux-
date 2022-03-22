fb4111b8bd08fe29eaffe1c5bf107ad9755b87a4
khalidreda <khalidaziz2198@gmail.com>
2022-03-22 11:38:09 +0200

    Before Challenge time STC-4m

--- 11.React With Redux/CAP Section 11/104.7  Dropdown for Picking SortBy/expensify-app/src/app.js
+++ 11.React With Redux/CAP Section 11/104.7  Dropdown for Picking SortBy/expensify-app/src/app.js
@@ -15,14 +15,14 @@ import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 const store = configureStore();
-store.dispatch(addExpense({description:'Water bill ',amount:4500}))
-store.dispatch(addExpense({ description: 'Gas bill ' }))
+store.dispatch(addExpense({description:'Water bill',amount:4500}))
+store.dispatch(addExpense({ description: 'Gas bill',createdAt:1000 }))
+store.dispatch(addExpense({description:'Rent',amount:109500}))
+
 
 
 store.dispatch(setTextFilter('water'));
-setTimeout((() => {
-  store.dispatch(setTextFilter('bill'))
-}),3000)
+
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses)
--- 11.React With Redux/CAP Section 11/104.7  Dropdown for Picking SortBy/expensify-app/src/components/ExpenseListFilters.js
+++ 11.React With Redux/CAP Section 11/104.7  Dropdown for Picking SortBy/expensify-app/src/components/ExpenseListFilters.js
@@ -5,11 +5,17 @@ const ExpenseListFilters = (props) => (
   <div>
     <input type="text" value={props.filters.text} onChange={(e) => {
       props.dispatch(setTextFilter(e.target.value))
-      console.log(e.target.value)
+      // console.log(e.target.value)
     } }/>
-
+    <select>
+      <option value="date">Date</option>
+      <option value="amount">Amount</option>
+    </select>
   </div>
 )
+//Challenge Time
+//Setup value and onChange for select
+
 const mapStateToProps = (state) => {
   return {
     filters:state.filters
