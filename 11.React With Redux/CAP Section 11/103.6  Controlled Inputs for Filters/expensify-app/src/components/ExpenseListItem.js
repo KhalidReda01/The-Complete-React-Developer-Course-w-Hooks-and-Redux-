c74d15986b8bda3fe1fc8e3d0f50baef94f0b9e7
khalidreda <khalidaziz2198@gmail.com>
2022-03-22 11:08:41 +0200

    DEES id  is not deinfed but I catch it I didn't pass it

--- 11.React With Redux/CAP Section 11/103.6  Controlled Inputs for Filters/expensify-app/src/components/ExpenseListItem.js
+++ 11.React With Redux/CAP Section 11/103.6  Controlled Inputs for Filters/expensify-app/src/components/ExpenseListItem.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({description,amount,createdAt}) => (
+const ExpenseListItem = ({dispatch,description,amount,createdAt}) => (
   <div>
     <h3>{description}</h3>
     <p>{amount} - {createdAt}</p>
-    <button>Remove</button>
+    <button onClick={() => {
+      dispatch(removeExpense({id}))
+    }}>Remove</button>
   </div>
 )
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
 // Challenge Time
