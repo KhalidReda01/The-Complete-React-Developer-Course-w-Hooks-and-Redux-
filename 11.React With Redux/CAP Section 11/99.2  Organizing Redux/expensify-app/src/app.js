a7b721be127be8ec189d016fbd265be427164c06
khalidreda <khalidaziz2198@gmail.com>
2022-03-22 07:07:50 +0200

    CAP start lecture 99/2 Organizing Redux Data

---
+++ 11.React With Redux/CAP Section 11/99.2  Organizing Redux/expensify-app/src/app.js
@@ -0,0 +1,13 @@
+
+/**
+ * CAP 1st Section 11 React With Redux
+ * 99/2 Organizing Redux Data 
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+
+ReactDOM.render(<AppRouter/>, document.getElementById('app'));
