b12cf127be8a70591d9b7e3855ef134b92161723
khalidreda <khalidaziz2198@gmail.com>
2022-03-22 09:07:44 +0200

    DEES find it

--- 11.React With Redux/CAP Section 11/102.5  Rendering Individual Expenses/expensify-app/src/components/ExpenseList.js
+++ 11.React With Redux/CAP Section 11/102.5  Rendering Individual Expenses/expensify-app/src/components/ExpenseList.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
 const ExpenseList = (props) => (
   <div>
     <h1>Expense List</h1>
-    {props.expenses.length}
+    {props.expenses.map(() => {
+      {/* return <ExpenseListItem expense={expense }/> */}
+      return <ExpenseListItem expense={...expense }/>
+    })}
   </div>
 )
 const mapStateToProps=(state) => {
--- 11.React With Redux/CAP Section 11/102.5  Rendering Individual Expenses/expensify-app/src/components/ExpenseListItem.js
+++ 11.React With Redux/CAP Section 11/102.5  Rendering Individual Expenses/expensify-app/src/components/ExpenseListItem.js
@@ -1,3 +1,12 @@
 //Challenge Time
 //Export a statless functional component
 //description,amount,createdAt
+import React from 'react';
+
+const ExpenseListItem = ({description,amount,createdAt}) => (
+  <div>
+    <h3>{description}</h3>
+    <p>{amount}-{createdAt}</p>
+  </div>
+)
+export default ExpenseListItem;
