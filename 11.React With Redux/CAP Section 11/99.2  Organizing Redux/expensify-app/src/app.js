ef9617e0424b45071c274bd0fa29856742ac5b8d
khalidreda <khalidaziz2198@gmail.com>
2022-03-22 07:33:16 +0200

    Challenge Time  STC

--- 11.React With Redux/CAP Section 11/99.2  Organizing Redux/expensify-app/src/app.js
+++ 11.React With Redux/CAP Section 11/99.2  Organizing Redux/expensify-app/src/app.js
@@ -6,8 +6,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
+const store = configureStore();
+// Challenge Time
+// addExpense -> Water bill
+// addExpense -> Gas bill
+// setTextFilter -> bill (2 items)-> water (1 item )
+// getVisibleExpenses -> print visible one to the screen 
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter/>, document.getElementById('app'));
--- 11.React With Redux/CAP Section 11/99.2  Organizing Redux/expensify-app/src/store/configureStore.js
+++ 11.React With Redux/CAP Section 11/99.2  Organizing Redux/expensify-app/src/store/configureStore.js
@@ -1,10 +1,13 @@
 import { createStore,combineReducers } from "redux";
 // Store Creation
 import expensesReducer from '../reducers/expenses';
-import filterReducer from '../reducers/filters'
-const store = createStore(
+import filterReducer from '../reducers/filters';
+export default () => {
+  const store = createStore(
   combineReducers({
     expenses: expensesReducer,
     filters:filterReducer
   })
-);
+  );
+  return store
+}
