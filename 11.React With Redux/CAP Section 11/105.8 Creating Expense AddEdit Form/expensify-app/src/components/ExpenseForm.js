4d97c483940fdbb0608e84f00d4a0f11eb7f3a70
khalidreda <khalidaziz2198@gmail.com>
2022-03-22 14:50:03 +0200

    3rd part 16m HOCC but not judge before CAP2

--- 11.React With Redux/CAP Section 11/105.8 Creating Expense AddEdit Form/expensify-app/src/components/ExpenseForm.js
+++ 11.React With Redux/CAP Section 11/105.8 Creating Expense AddEdit Form/expensify-app/src/components/ExpenseForm.js
@@ -4,11 +4,25 @@ import React from 'react';
 // setup onChange and value for textarea
 export default class Expense extends React.Component{
   state = {
-    description:''
+    description: '',
+    note: '',
+    amount:''
   }
   onDescriptionChange = (e) => {
     const description = e.target.value;
     this.setState(()=>({description}))
+  }
+  onNoteChange = (e) => {
+    // const note = e.target.value;
+    e.persist()
+    this.setState(()=>({note:e.target.value}))
+  }
+  
+  onAmountChange = (e) => {
+    const amount = e.target.value;
+    if () {
+      this.setState(()=>({amount}))
+    }
   }
   render() {
     return (
@@ -18,14 +32,20 @@ export default class Expense extends React.Component{
             type="text"
             placeholder="Description"
             autoFocus
+            value={this.state.description}
+            onChange={this.onDescriptionChange}
+            
           />
           <input
             type="number"
             placeholder="Amount"
-            p
+            value={this.state.amount}
+            onChange={this.onAmountChange}
           />
           <textarea
             placeholder="Add a note for your expense (optional)"
+            value={this.state.note}
+            onChange={this.onNoteChange}
           >
 
           </textarea>
