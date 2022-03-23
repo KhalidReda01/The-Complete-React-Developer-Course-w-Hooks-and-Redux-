4178431aae9a1dbe34fa604bdec3e24656ba44d2
khalidreda <khalidaziz2198@gmail.com>
2022-03-23 12:58:07 +0200

    12m DEES who care

--- 11.React With Redux/CAP Section 11/108.11  Wiring Up Edit Expense/expensify-app/src/components/ExpenseForm.js
+++ 11.React With Redux/CAP Section 11/108.11  Wiring Up Edit Expense/expensify-app/src/components/ExpenseForm.js
@@ -8,14 +8,18 @@ const now = moment();
 console.log(now.format('MMM Do, YYYY'));
 
 export default class ExpenseForm extends React.Component {
-  state = {
-    description: '',
-    note: '',
-    amount: '',
-    createdAt: moment(),
+  constructor(props) {
+    super(props);
+    state = {
+    description:props.expense?props.expense.description:'',
+    note: props.expense?props.expense.note:'',
+    amount: props.expense?(props.expense.amount/100).toString():'',
+    createdAt:props.expense?moment(props.expense.createdAt): moment(),
     calendarFocused: false,
     error:''
   };
+  }
+  
   onDescriptionChange = (e) => {
     const description = e.target.value;
     this.setState(() => ({ description }));
