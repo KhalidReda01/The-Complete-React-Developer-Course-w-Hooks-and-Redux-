c76f4e455228fb856e56ea7a871fbcdd2384b57d
khalidreda <khalidaziz2198@gmail.com>
2022-03-23 12:39:07 +0200

    CAP start Lecture 108/11 Wiring Up Edit Expense

---
+++ 11.React With Redux/CAP Section 11/108.11  Wiring Up Edit Expense/expensify-app/src/components/AddExpensePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
+const AddExpensePage = (props) => (
+  <div>
+    <h1>Add Expense</h1>
+    <ExpenseForm
+      onSubmit={(expense) => {
+        props.dispatch(addExpense(expense));
+        props.history.push('/');
+        console.log(expense)
+    }}
+
+    />
+  </div>
+);
+
+export default connect()(AddExpensePage);
---
+++ 11.React With Redux/CAP Section 11/108.11  Wiring Up Edit Expense/expensify-app/src/components/ExpenseForm.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+// const date = new Date();
+const now = moment();
+console.log(now.format('MMM Do, YYYY'));
+
+export default class ExpenseForm extends React.Component {
+  state = {
+    description: '',
+    note: '',
+    amount: '',
+    createdAt: moment(),
+    calendarFocused: false,
+    error:''
+  };
+  onDescriptionChange = (e) => {
+    const description = e.target.value;
+    this.setState(() => ({ description }));
+  };
+  onNoteChange = (e) => {
+    const note = e.target.value;
+    this.setState(() => ({ note }));
+  };
+  onAmountChange = (e) => {
+    const amount = e.target.value;
+
+    if (!amount|| amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
+      this.setState(() => ({ amount }));
+    }
+  };
+  onDateChange = (createdAt) => {
+    if (createdAt) {
+      this.setState(() => ({ createdAt }));
+    }
+    
+  };
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocused: focused }));
+  };
+  onSubmit = (e) => {
+    e.preventDefault()
+    if (!this.state.description || !this.state.amount) {
+            // Set Error State equal to "Please Provide description and amount"
+
+      this.setState(()=>({error:'Please Provide description and amount'}))
+    } else {
+      // Clear the error
+      this.setState(()=>({error:''}))
+      // console.log('Submitted!')
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note:this.state.note
+      })
+    }
+  }
+  render() {
+    return (
+      <div>
+        {this.state.error && <p>{this.state.error }</p>}
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            placeholder="Description"
+            autoFocus
+            value={this.state.description}
+            onChange={this.onDescriptionChange}
+          />
+          <input
+            type="text"
+            placeholder="Amount"
+            value={this.state.amount}
+            onChange={this.onAmountChange}
+          />
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
+          />
+          <textarea
+            placeholder="Add a note for your expense (optional)"
+            value={this.state.note}
+            onChange={this.onNoteChange}
+          >
+          </textarea>
+          <button>Add Expense</button>
+        </form>
+      </div>
+    )
+  }
+}
