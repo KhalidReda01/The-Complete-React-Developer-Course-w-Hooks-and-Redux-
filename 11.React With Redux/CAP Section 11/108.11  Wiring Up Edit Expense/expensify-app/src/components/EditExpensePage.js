b475a602ada223bda436536fd8c4371c22bf725a
khalidreda <khalidaziz2198@gmail.com>
2022-03-23 12:50:32 +0200

    DEES Go Home page instead you know what I mean Keep goin

--- 11.React With Redux/CAP Section 11/108.11  Wiring Up Edit Expense/expensify-app/src/components/EditExpensePage.js
+++ 11.React With Redux/CAP Section 11/108.11  Wiring Up Edit Expense/expensify-app/src/components/EditExpensePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 const EditExpensePage = (props) => {
   console.log(props);
@@ -8,5 +9,11 @@ const EditExpensePage = (props) => {
     </div>
   );
 };
-
-export default EditExpensePage;
+const mapStateToProps = (state,props) => {
+  return {
+    expense: state.expenses.find(() => {
+      return expense.id===props.match.params.id;
+    })
+  }
+}
+export default connect(mapStateToProps)(EditExpensePage) ;
