ec2a59d78257d2959c6773686d1362bafbec9259
khalidreda <khalidaziz2198@gmail.com>
2022-03-25 14:47:55 +0200

    Okay I was right

--- 00/#CSMM/11.React With Redux/2.CFSC/CFSC-1 Section 11/expensify-app/src/app.js
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-1 Section 11/expensify-app/src/app.js
@@ -9,19 +9,27 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water bill' }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('water'));
-
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
-
 store.subscribe(() => {
     const state = store.getState();
     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
     console.log(visibleExpenses);
   });
+  
+store.dispatch(addExpense({ description: 'Water bill' }));
+store.dispatch(addExpense({ description: 'Gas bill' }));
+store.dispatch(setTextFilter('water'));
+store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
+console.log(store.getState());
+
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
+
+// store.subscribe(() => {
+//     const state = store.getState();
+//     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+//     console.log(visibleExpenses);
+//   });
   
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
