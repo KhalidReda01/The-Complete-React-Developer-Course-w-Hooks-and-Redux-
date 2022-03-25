de6631e5137ac53232ec9e5bc7459abd829cd1f2
khalidreda <khalidaziz2198@gmail.com>
2022-03-25 18:02:54 +0200

    1st part

---
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-1 Section 11/expensify-app/src/playground/hoc.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const Info = (props) => (
+  <div>
+    <h1>Info</h1>
+    <p>The info is :{props.info }</p>
+  </div>
+)
+ReactDOM.render(<Info info="These are the details" />,document.getElementById('app'))
