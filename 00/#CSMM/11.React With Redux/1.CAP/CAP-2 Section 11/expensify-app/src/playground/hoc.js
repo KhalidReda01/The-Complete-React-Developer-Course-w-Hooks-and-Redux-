8c5c444d30138ddaf6ac5ccc3e944888fde0e004
khalidreda <khalidaziz2198@gmail.com>
2022-03-25 16:48:08 +0200

    half done and I think this lecture is little easy but Keep going

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/playground/hoc.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/playground/hoc.js
@@ -1,3 +1,8 @@
+//Higher Order (HOC) - A component (HOC) that renders another component
+//Reuse Code
+//Render Hijacking
+//Prop Manpulation 
+//Abtract state 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -5,6 +10,25 @@ const Info = (props) => (
   <div>
     <h1>Info</h1>
     <p>The info is :{props.info}</p>
+    
   </div>
 )
-ReactDOM.render(<Info info="These are the deaitls"/>,document.getElementById('app'))
+// step 1 create a regular function not a creact component
+// this is the high order comonent 
+/**
+ ** In this div we have a few gools 
+ ** the first goal to add admin warning in 
+ ** the second still render the wrapped component 
+
+ */
+const withAdminWarning = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      <p> This is Private info. Please don't share !</p>
+      <WrappedComponent/>
+    </div>
+  )
+}
+const AdminInfo=withAdminWarning(Info)
+
+ReactDOM.render(<AdminInfo info="These are the deaitls"/>,document.getElementById('app'))
