7b69590d55aaafdb6693b79a1d6d0e80096b7a5f
khalidreda <khalidaziz2198@gmail.com>
2022-03-25 18:12:18 +0200

    CFSC-CfM 100/3 The Higer Order Componnt U-99%

--- 00/#CSMM/11.React With Redux/2.CFSC/CFSC-1 Section 11/expensify-app/src/playground/hoc.js
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-1 Section 11/expensify-app/src/playground/hoc.js
@@ -9,11 +9,20 @@ const Info = (props) => (
 const withAdminWarning = (WrappedComponent) => {
   return (props)=> (
     <div>
-      <p>This is private info. Pleae don't share !</p>
+     {props.isAdmin&&<p>This is private info. Pleae don't share !</p>} 
       <WrappedComponent {...props }/>
     </div>
   )
 }
-const AdminInfo=withAdminWarning(Info)
+const requireAuthentication = (WrappedComponent) => {
+  return (props) => (
+    <div>
+      {props.isAuthenticated ? <WrappedComponent {...props}/>:<p>Please login in to view Info</p>}
+    </div>
+  )
+}
+const AdminInfo = withAdminWarning(Info)
+const AuthInfo=requireAuthentication(Info)
 // ReactDOM.render(<Info info="These are the details" />,document.getElementById('app'))
-ReactDOM.render(<AdminInfo info="These are the details" />,document.getElementById('app'))
+// ReactDOM.render(<AdminInfo isAdmin={false} info="These are the details" />,document.getElementById('app'))
+ReactDOM.render(<AuthInfo isAuthenticated={true} info="These are the details" />,document.getElementById('app'))
