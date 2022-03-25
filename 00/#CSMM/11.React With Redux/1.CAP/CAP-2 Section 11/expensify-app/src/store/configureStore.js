42e440668dcd226924ef549823b575058297115c
khalidreda <khalidaziz2198@gmail.com>
2022-03-25 13:19:46 +0200

    import done but now gonna make a few small chagnes to this one go ahead and export by default a function you gonna see what I'm talking about Keep goin

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/store/configureStore.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/store/configureStore.js
@@ -1,3 +1,6 @@
+import { createStore, combineReducers } from "redux";
+import expensesReducer from '../reducers/expenses'
+import filterReducer from '../reducers/filters'
 // Store.creation  
 const store = createStore(
   combineReducers({
