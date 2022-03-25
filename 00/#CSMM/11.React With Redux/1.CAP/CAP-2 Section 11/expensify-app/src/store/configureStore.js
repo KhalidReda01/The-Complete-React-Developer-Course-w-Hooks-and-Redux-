3e391498a620c2e905d84ca3660cca676809e032
khalidreda <khalidaziz2198@gmail.com>
2022-03-25 13:24:14 +0200

    8m Configure store Now done and It's actually ready for use in order to actually use this fiel we have to first make sure we can run webpack correctly but I'm gonna do that Keep going

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/store/configureStore.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/store/configureStore.js
@@ -1,10 +1,19 @@
 import { createStore, combineReducers } from "redux";
 import expensesReducer from '../reducers/expenses'
-import filterReducer from '../reducers/filters'
-// Store.creation  
-const store = createStore(
+import filterReducer from '../reducers/filters';
+
+
+export default () => {
+  const store = createStore(
   combineReducers({
     expenses: expensesReducer,
     filters:filterReducer
   })
-);
+  );
+  return store
+}
+// Store.creation  
+/**
+ * * Now this  means that when we import the function the default export from configure store we just 
+ * * call it we get the store back we can go ahead and actually use it 
+ */
