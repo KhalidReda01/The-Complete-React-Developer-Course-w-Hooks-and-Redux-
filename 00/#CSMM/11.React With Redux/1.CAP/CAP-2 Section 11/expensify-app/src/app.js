c109503113b8f0e7683d903c92fb7fc9027383c0
khalidreda <khalidaziz2198@gmail.com>
2022-03-25 13:29:59 +0200

    Basically proven that the reducer the storel fiel they are all been set up correctly congrats

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/app.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/app.js
@@ -33,14 +33,17 @@
  */
 /**
  * 99/2 Organizing Redux 
- *  
+ *  Write all my notes at paper untill now so no need to write anything here Okay keep goina and you can make mix but I think I fully understand what I'm doing but just notes to remember later and actually master Redux 
+ * 
  * 
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
-
-
+ const store=configureStore()
+console.log(store.getState())
 ReactDOM.render(<AppRouter/>, document.getElementById('app'));
