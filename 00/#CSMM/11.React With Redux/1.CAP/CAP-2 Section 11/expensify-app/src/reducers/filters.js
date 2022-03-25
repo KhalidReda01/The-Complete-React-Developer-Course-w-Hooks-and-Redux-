283eefd18e0dd5781c5b8607b64d44e065258fd7
khalidreda <khalidaziz2198@gmail.com>
2022-03-25 13:04:30 +0200

    2nd that's it for the reducer created two reducer one for expenses and other for filters

---
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/reducers/filters.js
@@ -0,0 +1,41 @@
+//Filter Reducer
+const filterReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate:undefined
+}
+export default (state=filterReducerDefaultState,action) => {
+  switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      // console.log(action.text)
+      return {
+        ...state,
+        text:action.text
+      }
+    case 'SORT_BY_AMOUNT':
+      console.log(action.sortBy)
+      return {
+        ...state,
+        sortBy:action.sortBy
+      }
+    case 'SORT_BY_DATE':
+      return {
+        ...state,
+        sortBy:action.sortBy
+      }
+    case 'SET_START_DATE':
+      return {
+        ...state,
+        startDate:action.startDate,
+        
+      }
+    case 'SET_END_DATE':
+      return {
+        ...state,
+        endDate:action.endDate
+      }
+    default:
+      return state;
+  }
+}
