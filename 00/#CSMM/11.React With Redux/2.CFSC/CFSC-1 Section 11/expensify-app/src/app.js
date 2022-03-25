a10c98c3b294a304f248353d5c3047ec496f4fcf
khalidreda <khalidaziz2198@gmail.com>
2022-03-25 14:33:44 +0200

    I will not let it go why subsribe He said I could but when I implement it It didn't work so now I will use his work to see

---
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-1 Section 11/expensify-app/src/app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water bill' }));
+store.dispatch(addExpense({ description: 'Gas bill' }));
+store.dispatch(setTextFilter('water'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
