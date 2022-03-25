3abe7401a28e694584e8d4728bf8f0f8ab52167d
khalidreda <khalidaziz2198@gmail.com>
2022-03-25 14:24:40 +0200

    Okay this should work but the subsribe isn't reactive to any values to the console as if it's not exist

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/app.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/app.js
@@ -52,15 +52,17 @@ const store = configureStore()
  store.dispatch(addExpense({description:'Gas bill'}))
  store.dispatch(setTextFilter('water'))
  store.dispatch(setTextFilter('water'))
- store.dispatch(setTextFilter('water'))
-// const state=store.getState()
+ store.dispatch(setTextFilter('bill'))
+const state=store.getState()
 // const visibleExpense = getVisibleExpenses(state.expenses,state.filters)
 // console.log(visibleExpense)
-store.subscribe(() => {
-  const state = store.getState();
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-  console.log(visibleExpenses)
+store.subscribe(() => 
+{
+  const state=store.getState()
+  const visibleExpense = getVisibleExpenses(state.expenses,state.filters)
+
+  console.log(visibleExpense)
 
 })
- 
+
 ReactDOM.render(<AppRouter/>, document.getElementById('app'));
