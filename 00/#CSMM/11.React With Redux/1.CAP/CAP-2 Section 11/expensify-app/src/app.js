42658e864478eea35bc38833e34e204edc95ccc4
khalidreda <khalidaziz2198@gmail.com>
2022-03-25 14:53:20 +0200

    DEES solved OMG nice feeling my code wasn't the souce of the problem It wsa just the position and I did something the instructor said it as an option it wasn't part of the challnege so really give the course time and you will get impressive results just be patient Now have some rest

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/app.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/app.js
@@ -49,13 +49,18 @@ import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 const store = configureStore()
  store.dispatch(addExpense({description:'Water bill '}))
- store.dispatch(addExpense({description:'Gas bill'}))
+store.dispatch(addExpense({ description: 'Gas bill' }))
+store.subscribe(() => {
+  const state=store.getState()
+  const visibleExpense = getVisibleExpenses(state.expenses,state.filters)
+  console.log(visibleExpense)
+  
+   })
  store.dispatch(setTextFilter('water'))
  store.dispatch(setTextFilter('water'))
- store.dispatch(setTextFilter('bill'))
-const state=store.getState()
-const visibleExpense = getVisibleExpenses(state.expenses,state.filters)
-console.log(visibleExpense)
+store.dispatch(setTextFilter('bill'))
+store.dispatch(setTextFilter('water'))
+
 
 
 ReactDOM.render(<AppRouter/>, document.getElementById('app'));
