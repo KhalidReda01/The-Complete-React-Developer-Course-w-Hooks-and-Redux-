90e7cb75e218e02661ac5a2e06e66710bcc6e179
khalidreda <khalidaziz2198@gmail.com>
2022-03-26 12:00:54 +0200

    10m WVI done and use Code Snaps for the first time to help  me review this part later CSMM

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseList.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseList.js
@@ -3,13 +3,13 @@ import {connect} from "react-redux"
 const ExpenseList = (props) => (
   <div>
     <h1>Expense List </h1>
-    {props.name}
+    {props.expenses.length}
   </div>
 )
 // HOC
 const ConnectedExpenseList = connect((state) => {
   return {
-    name:'khalid'
+    expenses:state.expenses
   }
 })(ExpenseList);
 export default ConnectedExpenseList;
