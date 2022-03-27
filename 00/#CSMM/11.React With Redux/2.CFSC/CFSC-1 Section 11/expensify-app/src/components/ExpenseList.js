2ee64f35977a660a28c57d36c90a7c817e800bf5
khalidreda <khalidaziz2198@gmail.com>
2022-03-27 09:49:51 +0200

    R-this is the first connected component to the redux store RR

--- 00/#CSMM/11.React With Redux/2.CFSC/CFSC-1 Section 11/expensify-app/src/components/ExpenseList.js
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-1 Section 11/expensify-app/src/components/ExpenseList.js
@@ -3,12 +3,12 @@ import { connect } from 'react-redux';
 const ExpenseList = (props) => (
   <div>
     <h1>Expense List</h1>
-    {props.name}
+    {props.expenses.length}
   </div>
 )
 const ConnectedExpenseList = connect((state) => {
   return {
-    name:'khalid'
+   expenses:state.expenses
   }
 })(ExpenseList)
 export default ConnectedExpenseList;
