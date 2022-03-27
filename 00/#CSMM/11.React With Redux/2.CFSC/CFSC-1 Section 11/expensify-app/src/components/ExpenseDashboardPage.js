330782c3c7cd66c025a6b45ddf66c8ab73add038
khalidreda <khalidaziz2198@gmail.com>
2022-03-27 09:45:46 +0200

    You see with you eyes how that is so import to break your code or split it into commits

--- 00/#CSMM/11.React With Redux/2.CFSC/CFSC-1 Section 11/expensify-app/src/components/ExpenseDashboardPage.js
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-1 Section 11/expensify-app/src/components/ExpenseDashboardPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ExpenseList from './ExpenseList';
 
 const ExpenseDashboardPage = () => (
   <div>
-    This is from my dashboard component!
+    <ExpenseList/>
+
   </div>
 );
 
