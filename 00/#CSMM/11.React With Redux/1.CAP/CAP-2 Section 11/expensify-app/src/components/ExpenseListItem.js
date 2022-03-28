ce578af296381e30f6d9e23597442425b1d53295
khalidreda <khalidaziz2198@gmail.com>
2022-03-28 14:19:16 +0200

    It's working with SC so seeems that I have typos Okay I will came back to see it Now I have to go

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseListItem.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseListItem.js
@@ -1,8 +1,18 @@
-import React from "react";
-const ExpenseListItem = (props) => (
+// import React from "react";
+// const ExpenseListItem = (props) => (
+//   <div>
+//     <h3>{props.description}</h3>
+//     <p>{props.amount}-{props.createAt}</p>
+//   </div>
+// )
+// export default ExpenseListItem
+import React from 'react';
+
+const ExpenseListItem = ({ description, amount, createdAt }) => (
   <div>
-    <h3>{props.description}</h3>
-    <p>{props.amount}-{props.createAt}</p>
+    <h3>{description}</h3>
+    <p>{amount} - {createdAt}</p>
   </div>
-)
-export default ExpenseListItem
+);
+
+export default ExpenseListItem;
