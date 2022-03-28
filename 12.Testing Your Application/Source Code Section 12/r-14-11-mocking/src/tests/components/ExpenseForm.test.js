7ca1c2bb5c598c0d8d367806d0b3ddd638716451
khalidreda <khalidaziz2198@gmail.com>
2022-03-28 13:12:36 +0200

    Section 12 Testing Your Application Source Code

---
+++ 12.Testing Your Application/Source Code Section 12/r-14-11-mocking/src/tests/components/ExpenseForm.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExpenseForm from '../../components/ExpenseForm';
+import expenses from '../fixtures/expenses';
+
+test('should render ExpenseForm correctly', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should render ExpenseForm correctly with expense data', () => {
+  const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
+  expect(wrapper).toMatchSnapshot();
+});
+
+
---
+++ 12.Testing Your Application/Source Code Section 12/r-14-16-expense-list/src/tests/components/ExpenseListFilters.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { ExpenseListFilters } from '../../components/ExpenseListFilters';
+import { filters, altFilters } from '../fixtures/filters';
+
+let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
+
+beforeEach(() => {
+  setTextFilter = jest.fn();
+  sortByDate = jest.fn();
+  sortByAmount = jest.fn();
+  setStartDate = jest.fn();
+  setEndDate = jest.fn();
+  wrapper = shallow(
+    <ExpenseListFilters
+      filters={filters}
+      setTextFilter={setTextFilter}
+      sortByDate={sortByDate}
+      sortByAmount={sortByAmount}
+      setStartDate={setStartDate}
+      setEndDate={setEndDate}
+    />
+  );
+});
+
+test('should render ExpenseListFilters correctly', () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should render ExpenseListFilters with alt data correctly', () => {
+  wrapper.setProps({
+    filters: altFilters
+  });
+  expect(wrapper).toMatchSnapshot();
+});
