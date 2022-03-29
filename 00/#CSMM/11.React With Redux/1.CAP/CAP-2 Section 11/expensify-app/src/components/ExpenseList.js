8275ee77139657364898a568793e1c9f06dfafae
khalidreda <khalidaziz2198@gmail.com>
2022-03-29 13:34:46 +0200

    Seems that I had a hidden bug Okay let's check what I have and that's will make me understand the code better

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseList.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseList.js
@@ -1,44 +1,44 @@
-// import React from "react";
-// import { connect } from "react-redux"
-// import ExpenseListItem from "./ExpenseListItem";
+import React from "react";
+import { connect } from "react-redux"
+import ExpenseListItem from "./ExpenseListItem";
 
-// const ExpenseList = (props) => (
-//   <div>
-//     <h1>Expense List </h1>
-//     {props.expenses.map((expense) => {
-//       return <ExpenseListItem key={expense.id} expense={expense}  />
+const ExpenseList = (props) => (
+  <div>
+    <h1>Expense List </h1>
+    {props.expenses.map((expense) => {
+      return <ExpenseListItem key={expense.id} expense={expense}  />
      
-//     })}
-//   </div>
-// )
-// const mapStateToProps=(state) => {
-//   return {
-//     expenses: state.expenses,
-//     filters:state.filters
-//   }
-// }
-// export default connect(mapStateToProps)(ExpenseList);
+    })}
+  </div>
+)
+const mapStateToProps=(state) => {
+  return {
+    expenses: state.expenses,
+    filters:state.filters
+  }
+}
+export default connect(mapStateToProps)(ExpenseList);
 /**
  * Try the source code
  */
- import React from 'react';
- import { connect } from 'react-redux';
- import ExpenseListItem from './ExpenseListItem';
- import selectExpenses from '../selectors/expenses';
+//  import React from 'react';
+//  import { connect } from 'react-redux';
+//  import ExpenseListItem from './ExpenseListItem';
+//  import selectExpenses from '../selectors/expenses';
  
- const ExpenseList = (props) => (
-   <div>
-     <h1>Expense List</h1>
-     {props.expenses.map((expense) => {
-       return <ExpenseListItem key={expense.id} {...expense} />;
-     })}
-   </div>
- );
+//  const ExpenseList = (props) => (
+//    <div>
+//      <h1>Expense List</h1>
+//      {props.expenses.map((expense) => {
+//        return <ExpenseListItem key={expense.id} {...expense} />;
+//      })}
+//    </div>
+//  );
  
- const mapStateToProps = (state) => {
-   return {
-     expenses: selectExpenses(state.expenses, state.filters)
-   };
- };
+//  const mapStateToProps = (state) => {
+//    return {
+//      expenses: selectExpenses(state.expenses, state.filters)
+//    };
+//  };
  
- export default connect(mapStateToProps)(ExpenseList);
+//  export default connect(mapStateToProps)(ExpenseList);
--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseListItem.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseListItem.js
@@ -1,18 +1,18 @@
-// import React from "react";
-// const ExpenseListItem = (props) => (
-//   <div>
-//     <h3>{props.description}</h3>
-//     <p>{props.amount}-{props.createAt}</p>
-//   </div>
-// )
-// export default ExpenseListItem
-import React from 'react';
-
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+import React from "react";
+const ExpenseListItem = (props) => (
   <div>
-    <h3>{description}</h3>
-    <p>{amount} - {createdAt}</p>
+    <h3>{props.description}</h3>
+    <p>{props.amount}-{props.createAt}</p>
   </div>
-);
+)
+export default ExpenseListItem
+// import React from 'react';
 
-export default ExpenseListItem;
+// const ExpenseListItem = ({ description, amount, createdAt }) => (
+//   <div>
+//     <h3>{description}</h3>
+//     <p>{amount} - {createdAt}</p>
+//   </div>
+// );
+
+// export default ExpenseListItem;
