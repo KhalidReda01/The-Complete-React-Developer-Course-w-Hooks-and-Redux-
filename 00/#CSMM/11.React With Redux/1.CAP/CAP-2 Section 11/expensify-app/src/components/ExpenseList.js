53da4aec6121914169155c017ba09d05b5bf6dd0
khalidreda <khalidaziz2198@gmail.com>
2022-03-29 13:49:19 +0200

    DEES solved I found the but it was the passed props spread operator you used a wrong fomula Okay NOW keep going at the lecture then afer that CFSC-CFM then go to the next lecture

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseList.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseList.js
@@ -1,24 +1,24 @@
-// import React from "react";
-// import { connect } from "react-redux"
-// import ExpenseListItem from "./ExpenseListItem";
+import React from "react";
+import { connect } from "react-redux"
+import ExpenseListItem from "./ExpenseListItem";
 
 
-// const ExpenseList = (props) => (
-//   <div>
-//     <h1>Expense List </h1>
-//     {props.expenses.map((expense) => {
-//       return <ExpenseListItem key={expense.id} expense={expense}  />
+const ExpenseList = (props) => (
+  <div>
+    <h1>Expense List </h1>
+    {props.expenses.map((expense) => {
+      return <ExpenseListItem key={expense.id} {...expense}  />
      
-//     })}
-//   </div>
-// )
-// const mapStateToProps=(state) => {
-//   return {
-//     expenses: state.expenses,
-//     filters:state.filters
-//   }
-// }
-// export default connect(mapStateToProps)(ExpenseList);
+    })}
+  </div>
+)
+const mapStateToProps=(state) => {
+  return {
+    expenses: state.expenses,
+    filters:state.filters
+  }
+}
+export default connect(mapStateToProps)(ExpenseList);
 /**
  * Try the source code
  */
