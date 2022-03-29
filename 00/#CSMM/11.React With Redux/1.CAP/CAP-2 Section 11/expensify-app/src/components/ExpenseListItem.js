490bc090a2c99cd47efde51f6f5e2edc112109b2
khalidreda <khalidaziz2198@gmail.com>
2022-03-29 13:41:24 +0200

    Okay at the ExpenseListItem component I only found that I missed the d letter at createdAt so keep going

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseListItem.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseListItem.js
@@ -1,13 +1,12 @@
-import React from "react";
-const ExpenseListItem = (props) => (
+import React from 'react';
+const ExpenseListItem = ({ description, amount, createdAt }) => (
   <div>
-    <h3>{props.description}</h3>
-    <p>{props.amount}-{props.createAt}</p>
+    <h3>{description}</h3>
+    <p>{amount} - {createdAt}</p>
   </div>
-)
+);
 export default ExpenseListItem
 // import React from 'react';
-
 // const ExpenseListItem = ({ description, amount, createdAt }) => (
 //   <div>
 //     <h3>{description}</h3>
