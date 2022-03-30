71aa3d56a1d035f76d73cd99492231e7b5f30ced
khalidreda <khalidaziz2198@gmail.com>
2022-03-30 18:23:10 +0200

    Challenge STC easy one

--- 12.Testing Your Application/CAP Section 12/112.2 Setting up Jest/expensify-app/src/tests/add.test.js
+++ 12.Testing Your Application/CAP Section 12/112.2 Setting up Jest/expensify-app/src/tests/add.test.js
@@ -6,3 +6,8 @@ test('Should add two numbers', () => {
   const result = add(3, 4);
   expect(result).toBe(7);
 }) 
+
+test('Should generate greeting from name', () => {
+  const result = generateGreeting(`Khalid`);
+  expect(result).toBe('Hello Khalid!')
+})
