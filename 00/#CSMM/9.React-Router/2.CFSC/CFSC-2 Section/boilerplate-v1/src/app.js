8909d20f11ccd08cdc6f2ab4f1d766b3fd1728c9
khalidreda <khalidaziz2198@gmail.com>
2022-04-03 22:22:44 +0200

    DEES doen

--- 00/#CSMM/9.React-Router/2.CFSC/CFSC-2 Section/boilerplate-v1/src/app.js
+++ 00/#CSMM/9.React-Router/2.CFSC/CFSC-2 Section/boilerplate-v1/src/app.js
@@ -1,10 +1,11 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-ReactDOM.render(AppRouter, document.getElementById('app'));
+ReactDOM.render(<AppRouter/>, document.getElementById('app'));
 /**
  * Using the HTML5 History API
  * https://css-tricks.com/using-the-html5-history-api/
