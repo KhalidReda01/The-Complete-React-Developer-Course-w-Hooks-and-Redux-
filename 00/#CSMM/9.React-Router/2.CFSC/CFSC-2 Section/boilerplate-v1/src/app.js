699b61e8449a9f39590fb3de011d47a62b95e5e0
khalidreda <khalidaziz2198@gmail.com>
2022-04-03 22:22:22 +0200

    DEES done AppRouter is a component unlike the routes variable

--- 00/#CSMM/9.React-Router/2.CFSC/CFSC-2 Section/boilerplate-v1/src/app.js
+++ 00/#CSMM/9.React-Router/2.CFSC/CFSC-2 Section/boilerplate-v1/src/app.js
@@ -1,69 +1,10 @@
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter,Route,Switch,Link,NavLink } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-const ExpenseDashboardPage = () => (
-  <div>
-    This is from my dashboard component
-  </div>
-)
-const AddExpensePage = () => (
-  <div>
-    This is from my add expense component
-  </div>
-)
-const EditExpensePage = () => (
-  <div>
-    This is my Edit Expense Page
-  </div>
-)
-const HelpPage = () => (
-  <div>
-    This is my Help Page
-  </div>
-)
-const NotFoundPage = () => (
-  <div>
-    404!
-    <Link to="/">Go Home</Link>
-  </div>
-)
-/**
- * Challenge Time 
- * Link to home page 
- * Link to the create Expense Page 
- * Link to the edit page 
- * Link to the help page  * 
- */
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName='is-active' exact={true}>Dashboard</NavLink>
-    <NavLink to="/create" activeClassName='is-active'>Expense</NavLink>
-    <NavLink to="/edit" activeClassName='is-active'>Edit</NavLink>
-    <NavLink to="/help" activeClassName='is-active'>Help</NavLink>
-  </header>
-)
-const routes = (
-  <BrowserRouter>
-    <div>
-       <Header/>
-    <Switch>
-      <Route path="/" component={ExpenseDashboardPage} exact={true} />
-      <Route path="/create" component={AddExpensePage} />
-      <Route path="/edit" component={EditExpensePage} />
-      <Route path="/help" component={HelpPage} />
-      <Route component={NotFoundPage}/>
-    </Switch>
-    </div>
-   
-  </BrowserRouter>
-)
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(AppRouter, document.getElementById('app'));
 /**
  * Using the HTML5 History API
  * https://css-tricks.com/using-the-html5-history-api/
---
+++ 00/#CSMM/9.React-Router/2.CFSC/CFSC-2 Section/boilerplate-v1/src/routers/AppRouter.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { BrowserRouter,Route,Switch,Link,NavLink } from 'react-router-dom';
+
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+)
+const AddExpensePage = () => (
+  <div>
+    This is from my add expense component
+  </div>
+)
+const EditExpensePage = () => (
+  <div>
+    This is my Edit Expense Page
+  </div>
+)
+const HelpPage = () => (
+  <div>
+    This is my Help Page
+  </div>
+)
+const NotFoundPage = () => (
+  <div>
+    404!
+    <Link to="/">Go Home</Link>
+  </div>
+)
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName='is-active' exact={true}>Dashboard</NavLink>
+    <NavLink to="/create" activeClassName='is-active'>Expense</NavLink>
+    <NavLink to="/edit" activeClassName='is-active'>Edit</NavLink>
+    <NavLink to="/help" activeClassName='is-active'>Help</NavLink>
+  </header>
+)
+const AppRouter =()=> (
+  <BrowserRouter>
+    <div>
+       <Header/>
+    <Switch>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage} />
+      <Route path="/edit" component={EditExpensePage} />
+      <Route path="/help" component={HelpPage} />
+      <Route component={NotFoundPage}/>
+    </Switch>
+    </div>
+   
+  </BrowserRouter>
+)
+export default AppRouter
