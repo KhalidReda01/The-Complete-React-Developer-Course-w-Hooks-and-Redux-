8b6effb8f35bd398da11e1d4a30cec6ff9493c0a
khalidreda <khalidaziz2198@gmail.com>
2022-04-03 22:12:17 +0200

    lecture done CfSC Linking between Routes

--- 00/#CSMM/9.React-Router/2.CFSC/CFSC-2 Section/boilerplate-v1/src/app.js
+++ 00/#CSMM/9.React-Router/2.CFSC/CFSC-2 Section/boilerplate-v1/src/app.js
@@ -42,7 +42,7 @@ const NotFoundPage = () => (
 const Header = () => (
   <header>
     <h1>Expensify</h1>
-    <NavLink to="/" activeClassName='is-active'>Dashboard</NavLink>
+    <NavLink to="/" activeClassName='is-active' exact={true}>Dashboard</NavLink>
     <NavLink to="/create" activeClassName='is-active'>Expense</NavLink>
     <NavLink to="/edit" activeClassName='is-active'>Edit</NavLink>
     <NavLink to="/help" activeClassName='is-active'>Help</NavLink>
