cef033ee970c394bf395f5170ee5cb4d131e7459
khalidreda <khalidaziz2198@gmail.com>
2022-04-03 19:40:41 +0200

    Now HTG

--- 00/#CSMM/9.React-Router/2.CFSC/CFSC-2 Section/boilerplate-v1/src/app.js
+++ 00/#CSMM/9.React-Router/2.CFSC/CFSC-2 Section/boilerplate-v1/src/app.js
@@ -1,10 +1,21 @@
 
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {ReactDOM} from 'react-dom';
+import { BrowserRouter,Route } from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-
-
-ReactDOM.render(<p>This is my boilerplate </p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+)
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage}/>
+    </div>
+  </BrowserRouter>
+)
+ReactDOM.render(routes, document.getElementById('app'));
