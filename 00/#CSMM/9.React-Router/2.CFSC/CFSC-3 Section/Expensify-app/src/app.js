45a63d142ddead38f7c987b2faf58430d445be9d
khalidreda <khalidaziz2198@gmail.com>
2022-04-05 17:44:09 +0200

    Linkding between Routes

--- 00/#CSMM/9.React-Router/2.CFSC/CFSC-3 Section/Expensify-app/src/app.js
+++ 00/#CSMM/9.React-Router/2.CFSC/CFSC-3 Section/Expensify-app/src/app.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter,Link,Route,Switch } from 'react-router-dom';
+import { BrowserRouter,Link,Route,Switch,NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
@@ -35,6 +35,10 @@ const NotFoundPage = () => (
 const Header = () => (
   <div>
     <h1>Expensify</h1>
+    <NavLink to ="/"  activeClassName='is-active' exact={true}>Dashboard</NavLink>
+    <NavLink to ="/create" activeClassName='is-active'>Create</NavLink>
+    <NavLink to ="/edit" activeClassName='is-active'>Edit Expense</NavLink>
+    <NavLink to ="/help" activeClassName='is-active'>Help</NavLink>
   </div>
 )
 const routes = (
