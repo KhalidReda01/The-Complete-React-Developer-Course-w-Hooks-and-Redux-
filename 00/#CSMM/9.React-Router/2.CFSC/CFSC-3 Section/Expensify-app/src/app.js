6d323ab225aa57b0cb2f8421e54bdc463394146e
khalidreda <khalidaziz2198@gmail.com>
2022-04-05 17:32:47 +0200

    Setting up a 404 page done and use Switch compoennt

--- 00/#CSMM/9.React-Router/2.CFSC/CFSC-3 Section/Expensify-app/src/app.js
+++ 00/#CSMM/9.React-Router/2.CFSC/CFSC-3 Section/Expensify-app/src/app.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter,Route } from 'react-router-dom';
+import { BrowserRouter,Route,Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
@@ -16,12 +16,30 @@ const AddExpensePage = () => (
     This is from my add Expense Page 
   </div>
 )
+const EditExpensePage = () => (
+  <div>
+    This is from my Edit Expense Page 
+  </div>
+)
+const HelpPage = () => (
+  <div>
+    This is frommy help Page 
+  </div>
+)
+const NotFoundPage = () => (
+  <div>
+  404!
+  </div>
+)
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true}/>
       <Route path="/create" component={AddExpensePage }/>
-    </div>
+      <Route path="/edit" component={EditExpensePage }/>
+      <Route path="/help" component={HelpPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 )
 
