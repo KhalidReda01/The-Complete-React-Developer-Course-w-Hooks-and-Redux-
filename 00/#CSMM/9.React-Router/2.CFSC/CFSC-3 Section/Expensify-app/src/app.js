ccd1e4cc03ec0f8b425e40c073135d9bb30cd0b9
khalidreda <khalidaziz2198@gmail.com>
2022-04-05 17:26:01 +0200

    Cannot Get Create and you know why now gonna solve it

--- 00/#CSMM/9.React-Router/2.CFSC/CFSC-3 Section/Expensify-app/src/app.js
+++ 00/#CSMM/9.React-Router/2.CFSC/CFSC-3 Section/Expensify-app/src/app.js
@@ -2,9 +2,27 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter,Route } from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard component
+  </div>
+)
+const AddExpensePage = () => (
+  <div>
+    This is from my add Expense Page 
+  </div>
+)
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage }/>
+      <Route path="/create" component={AddExpensePage }/>
+    </div>
+  </BrowserRouter>
+)
 
-
-ReactDOM.render(<p>This is my Expensify </p>, document.getElementById('app'));
+ReactDOM.render(routes, document.getElementById('app'));
