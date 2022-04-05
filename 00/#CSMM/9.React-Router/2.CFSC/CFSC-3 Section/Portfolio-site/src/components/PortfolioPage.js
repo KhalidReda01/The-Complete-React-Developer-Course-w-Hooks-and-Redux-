ca327555ad7b0de669027424bcbd277e8d58d2e0
khalidreda <khalidaziz2198@gmail.com>
2022-04-05 18:30:17 +0200

    Challenge Done Section 9 React-Router Done keep practice more more more more and more

--- 00/#CSMM/9.React-Router/2.CFSC/CFSC-3 Section/Portfolio-site/src/components/PortfolioPage.js
+++ 00/#CSMM/9.React-Router/2.CFSC/CFSC-3 Section/Portfolio-site/src/components/PortfolioPage.js
@@ -4,8 +4,8 @@ const PortfolioPage = () => (
   <div>
     <h1>My Work</h1>
     <p>Checkout the following things I've done</p>
-    <Link to='/PortfolioItemPage'>ItemOne</Link>
-    <Link to='/PortfolioItemPage/2'>ItemTwo</Link>
+    <Link to='/portfolio/1'>ItemOne</Link>
+    <Link to='/portfolio/2'>ItemTwo</Link>
   </div>
 )
 export default PortfolioPage
