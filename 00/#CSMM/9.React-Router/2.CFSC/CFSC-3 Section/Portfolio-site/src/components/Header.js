df79203d0a8e3fb456ca2b128ee4449febafa1fe
khalidreda <khalidaziz2198@gmail.com>
2022-04-05 18:28:43 +0200

    Okay testing it IT's wokring with the path

--- 00/#CSMM/9.React-Router/2.CFSC/CFSC-3 Section/Portfolio-site/src/components/Header.js
+++ 00/#CSMM/9.React-Router/2.CFSC/CFSC-3 Section/Portfolio-site/src/components/Header.js
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
   <div>
     <h1>Portfolio</h1>
-     <NavLink to='/'>Home</NavLink>
-     <NavLink to='/portfolio'>Portfolio</NavLink>
-     <NavLink to='/contact'>Contact</NavLink>
+     <NavLink to='/' activeClassName='is-active' exact={true}>Home</NavLink>
+     <NavLink to='/portfolio' activeClassName='is-active' exact={true}>Portfolio</NavLink>
+     <NavLink to='/contact' activeClassName='is-active'>Contact</NavLink>
   </div>
 )
 export default Header
