8c114ce9b991f0d79f5638c8c87dbf00ba9d1a96
khalidreda <khalidaziz2198@gmail.com>
2022-04-06 22:22:34 +0200

    CFSC U-100% lecture 86 Dispatching Actions

--- 00/#CSMM/10.Redux/2.CFSC/CFSC-2 Section 10 Redux/Expensify-app/src/playground/redux-101.js
+++ 00/#CSMM/10.Redux/2.CFSC/CFSC-2 Section 10 Redux/Expensify-app/src/playground/redux-101.js
@@ -3,19 +3,43 @@
  */
 import { createStore } from "redux";
 const store = createStore((state = { count: 0 }, action) => {
-  console.log(action)
-  console.log(state.count)
-  if (action.type === 'INCREMENT') {
-    return {
-      count:state.count+1
-    }
-  } else {
-    return state;
-  }
  
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count:state.count+1
+      }
+    case 'DECREMENT':
+      return {
+        count:state.count-1
+      }
+    case 'RESET':
+      return {
+        count:0
+      }
+     default :
+     return state;
+ 
+  }
+
   
 })
+//Increment the count by 1
 store.dispatch({
   type:'INCREMENT'
 })
+console.log(store.getState())
+store.dispatch({
+  type:'INCREMENT'
+})
+console.log(store.getState())
+//Decrement the count by 1 
+store.dispatch({
+  type:'DECREMENT'
+})
+console.log(store.getState())
+//Reset the cout to zero
+store.dispatch({
+  type:'RESET'
+}) 
 console.log(store.getState())
