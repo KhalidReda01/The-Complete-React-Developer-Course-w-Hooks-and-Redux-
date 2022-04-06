65dc7e25450a0015883eaee5d40a0743c15ebcd5
khalidreda <khalidaziz2198@gmail.com>
2022-04-06 22:13:04 +0200

    Actions and dispatching method

--- 00/#CSMM/10.Redux/2.CFSC/CFSC-2 Section 10 Redux/Expensify-app/src/playground/redux-101.js
+++ 00/#CSMM/10.Redux/2.CFSC/CFSC-2 Section 10 Redux/Expensify-app/src/playground/redux-101.js
@@ -2,9 +2,20 @@
  * 86 Dispatching Actions 
  */
 import { createStore } from "redux";
-const store = createStore((state = { count:0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+  console.log(action)
+  console.log(state.count)
+  if (action.type === 'INCREMENT') {
+    return {
+      count:state.count+1
+    }
+  } else {
+    return state;
+  }
+ 
   
 })
-
+store.dispatch({
+  type:'INCREMENT'
+})
 console.log(store.getState())
