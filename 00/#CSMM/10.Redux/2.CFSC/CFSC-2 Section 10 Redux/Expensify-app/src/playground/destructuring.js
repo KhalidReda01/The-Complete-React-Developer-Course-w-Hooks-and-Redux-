91de0d566b9f4e49285357f30fb794ae9194a81e
khalidreda <khalidaziz2198@gmail.com>
2022-04-06 23:31:38 +0200

    1st part Done

--- 00/#CSMM/10.Redux/2.CFSC/CFSC-2 Section 10 Redux/Expensify-app/src/playground/destructuring.js
+++ 00/#CSMM/10.Redux/2.CFSC/CFSC-2 Section 10 Redux/Expensify-app/src/playground/destructuring.js
@@ -1,1 +1,12 @@
-console.log('destructuring')
+const Person = {
+  name: 'khalid',
+  age: 23,
+  location: {
+    city: 'Mansura',
+    temp:15
+  }
+}
+const name = Person.name;
+const age = Person.age;
+console.log(`${Person.name} is ${Person.age}`)
+console.log(`${name} is ${age}`)
