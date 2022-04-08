4ed58b53d2758b66d7ba19cd43d6e9a90da483eb
khalidreda <khalidaziz2198@gmail.com>
2022-04-08 23:09:41 +0200

    with es6 Array destructuing Now gonna implement this on the payload

--- 00/#CSMM/10.Redux/2.CFSC/CFSC-2 Section 10 Redux/Expensify-app/src/playground/redux-101.js
+++ 00/#CSMM/10.Redux/2.CFSC/CFSC-2 Section 10 Redux/Expensify-app/src/playground/redux-101.js
@@ -1,8 +1,8 @@
 
 import { createStore } from "redux";
 //Action generators
-const add =(data)=>{
-  return data.a+data.b
+const add =({a,b})=>{
+  return a+b
 }
 console.log(add({a:1,b:12}))
 const incrementCount = (payload = {}) => ({
