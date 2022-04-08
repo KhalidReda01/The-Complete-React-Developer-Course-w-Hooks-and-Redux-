20973e0c60ae5d48eef223f72c2b6b52b2691000
khalidreda <khalidaziz2198@gmail.com>
2022-04-08 23:08:56 +0200

    Without es6 Array Destructuring

--- 00/#CSMM/10.Redux/2.CFSC/CFSC-2 Section 10 Redux/Expensify-app/src/playground/redux-101.js
+++ 00/#CSMM/10.Redux/2.CFSC/CFSC-2 Section 10 Redux/Expensify-app/src/playground/redux-101.js
@@ -1,6 +1,10 @@
 
 import { createStore } from "redux";
-//Action generators 
+//Action generators
+const add =(data)=>{
+  return data.a+data.b
+}
+console.log(add({a:1,b:12}))
 const incrementCount = (payload = {}) => ({
   type: 'INCREMENT',
   incrementBy:typeof payload.incrementBy==='number'?payload.incrementBy:1
