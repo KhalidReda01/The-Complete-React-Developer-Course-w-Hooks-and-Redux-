b1816b03617b9d1b17319cbbbc2616be5522d46e
khalidreda <khalidaziz2198@gmail.com>
2022-04-09 00:19:26 +0200

    Bug sloved Not bad at all

--- 00/#CSMM/10.Redux/2.CFSC/CFSC-2 Section 10 Redux/Expensify-app/src/playground/redux-expensify.js
+++ 00/#CSMM/10.Redux/2.CFSC/CFSC-2 Section 10 Redux/Expensify-app/src/playground/redux-expensify.js
@@ -4,11 +4,13 @@ import uuid from 'uuid'
 // Not bad
 const addExpense = ({description='',note='',amount=0,createdAt=0}={})=> ({
   type: 'ADD_EXPENSE',
+  expense:{
   id: uuid(),
-  description,
-  note,
-  amount,
-  createdAt
+    description,
+    note,
+    amount,
+    createdAt
+}
 })
 //REMOVE_EXPENSE
 //EDIT_EXPENSE
