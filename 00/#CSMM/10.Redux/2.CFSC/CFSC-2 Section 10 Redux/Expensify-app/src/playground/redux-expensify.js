be89aa39469db92c3f00f870603def1cf651e48e
khalidreda <khalidaziz2198@gmail.com>
2022-04-09 05:38:27 +0200

    HOCC Sorting Redux Data Back again is a must This part is the logic part of the project

--- 00/#CSMM/10.Redux/2.CFSC/CFSC-2 Section 10 Redux/Expensify-app/src/playground/redux-expensify.js
+++ 00/#CSMM/10.Redux/2.CFSC/CFSC-2 Section 10 Redux/Expensify-app/src/playground/redux-expensify.js
@@ -126,6 +126,12 @@ const getVisibleExpenses = (expenses, {text,sortBy,startDate,endDate}) => {
     const endDateMatch =  typeof startDate !=='number'||expense.createdAt>=startDate;
     const textMatch = true;
     return startDateMatch && endDateMatch && textMatch;
+  }).sort((a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt <b.createdAt?1:-1
+    } else if (sortBy === 'amount') {
+      return a.amount<b.amount?1:-1
+    }
   })
 }
 //Store Creation
