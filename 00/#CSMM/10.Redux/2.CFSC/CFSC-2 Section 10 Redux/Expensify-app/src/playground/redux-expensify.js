0ccff88c89ca3d8ea0036e163d7e68acf7599d03
khalidreda <khalidaziz2198@gmail.com>
2022-04-10 04:59:05 +0200

    Review Section 10 CFSC Redux Done U 80%

--- 00/#CSMM/10.Redux/2.CFSC/CFSC-2 Section 10 Redux/Expensify-app/src/playground/redux-expensify.js
+++ 00/#CSMM/10.Redux/2.CFSC/CFSC-2 Section 10 Redux/Expensify-app/src/playground/redux-expensify.js
@@ -134,15 +134,17 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
 // Get visible expenses 
 const getVisibleExpenses = (expenses, {text,sortBy,startDate,endDate}) => {
   return expenses.filter((expense) => {
-    const startDateMatch=typeof startDate !=='number' ||expense.createdAt>=startDate;
-    const endDateMatch=typeof endDate !=='number'||expense.createdAt<=endDate;
-    const textMatch=expense.description.toLowerCase().includes(text.toLowerCase());
+    const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+    const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
     return startDateMatch && endDateMatch && textMatch;
   }).sort((a, b) => {
     if (sortBy === 'date') {
       console.log(a.createdAt)
-    console.log(b)
-      return a.createdAt < b.createdAt ? 1:-1
+      console.log(b)
+      return a.createdAt < b.createdAt ? 1 : -1
+    } else if (sortBy === 'amount') {
+      return a.amount<b.amount ?1:-1
     }
     
   })
@@ -164,8 +166,8 @@ store.subscribe(() => {
   console.log(visibleExpenses)
 });
 
-const expenseOne = store.dispatch(addExpense({ description: 'rent', amount: 100,createdAt:-21000 }));
-const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300,createdAt:-1000 }));
+const expenseOne = store.dispatch(addExpense({ description: 'rent', amount: 500,createdAt:-21000 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 600,createdAt:-1000 }));
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
