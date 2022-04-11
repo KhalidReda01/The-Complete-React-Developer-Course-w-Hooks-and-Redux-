47e19f033cc0d7b8960dd862801122a14d466294
khalidreda <khalidaziz2198@gmail.com>
2022-04-11 16:27:43 +0200

    Got it but why not WTV But Adjust VC first

--- 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/app.js
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/app.js
@@ -15,8 +15,11 @@ import './styles/styles.scss';
 const store = configureStore()
 store.dispatch(addExpense({description:'Water bill'}))
 store.dispatch(addExpense({ description: 'Gas bill' }))
-// store.dispatch(setTextFilter('bill'))
+store.dispatch(setTextFilter('bill'))
 // console.log(store.getState())
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'))
+},3000)
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses)
--- 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/ExpenseList.js
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/ExpenseList.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import expenses from '../reducers/expenses';
 
 const ExpenseList = (props) => (
   <div>
     <h1>Expense List</h1>
-    {props.name}
+    {props.expenses.length}
+    {props.filters.text}
   </div>
 )
 const ConnectedExpenseList = connect((state) => {
+  console.log(state)
   return {
-    name :'Ramdan'
+    expenses: state.expenses,
+    filters:state.filters
   }
 })(ExpenseList)
 export default ConnectedExpenseList;
