340596b30c34a4f3658d43f0e1d37c05537e362f
khalidreda <khalidaziz2198@gmail.com>
2022-04-11 14:28:48 +0200

    That's the normal component how is it different from the HOC

--- 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/playground/hoc.js
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/playground/hoc.js
@@ -1,1 +1,10 @@
-console.log('hoc')
+// Higher Order Component (HOC) - A component (HOC) that renders another component 
+import React from 'react';
+import ReactDOM from 'react-dom';
+const Info = (props) => (
+  <div>
+    <h1>Info</h1>
+    <p>The info is :{props.info}</p>
+  </div>
+)
+ReactDOM.render(<Info info='There are the details'/> ,document.getElementById('app'))
