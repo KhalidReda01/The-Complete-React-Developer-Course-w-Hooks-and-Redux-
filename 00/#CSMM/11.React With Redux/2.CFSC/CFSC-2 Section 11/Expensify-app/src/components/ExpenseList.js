41e104419377d81149840ea0e05e10e7c90a1b6c
khalidreda <khalidaziz2198@gmail.com>
2022-04-12 11:11:47 +0200

    DEES

--- 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/ExpenseList.js
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/ExpenseList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import ExpenseListItem from './ExpenseListItem';
 const ExpenseList = (props) => (
   <div>
     <h1>Expense List</h1>
-    {props.expenses.length}
-    {props.filters.text}
+    {props.expenses.map(() => {
+      return <ExpenseListItem {...expense}/>
+   })}
   </div>
 )
 const mapStateToProps = (state) => {
