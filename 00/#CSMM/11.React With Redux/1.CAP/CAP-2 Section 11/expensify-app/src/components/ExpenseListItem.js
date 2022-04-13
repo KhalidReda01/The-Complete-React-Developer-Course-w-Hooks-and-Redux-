f2b3e31c2e112910d7d222974833518fc91de1b7
khalidreda <khalidaziz2198@gmail.com>
2022-04-14 00:33:01 +0200

    dispatch destructuring HOCC part

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseListItem.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseListItem.js
@@ -6,7 +6,7 @@ const ExpenseListItem = ({dispatch,id, description, amount, createdAt }) => (
     <h3>{description}</h3>
     <p>{amount} - {createdAt}</p>
     <button onClick={() => {
-      
+      dispatch(removeExpense({id}))
     }}>Remove</button>
   </div>
 );
