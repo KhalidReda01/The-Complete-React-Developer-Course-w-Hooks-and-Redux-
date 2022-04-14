253f14b689847221ee46220f8a6a6f1010d19c86
khalidreda <khalidaziz2198@gmail.com>
2022-04-14 22:59:36 +0200

    Discover why the other way of export didn't work

--- 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/AddExpensePage.js
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/AddExpensePage.js
@@ -1,10 +1,8 @@
 import React from 'react';
-// import ExpenseForm from './ExpenseForm';
 import Expense from './ExpenseFrom';
 const AddExpensePage = () => (
   <div>
     <h1>Add Expense</h1>
-    {/* <ExpenseForm/> */}
     <Expense/>
   </div>
 );
