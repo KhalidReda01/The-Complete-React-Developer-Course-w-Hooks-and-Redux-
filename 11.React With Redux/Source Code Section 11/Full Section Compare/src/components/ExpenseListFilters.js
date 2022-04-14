71e757ff297f5be08fdada80573be4081ce63e83
khalidreda <khalidaziz2198@gmail.com>
2022-04-15 00:30:19 +0200

    Lecture 6 Controlled Inputs for Filters

---
+++ 11.React With Redux/Source Code Section 11/Full Section Compare/src/components/ExpenseListFilters.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { setTextFilter } from '../actions/filters';
+
+const ExpenseListFilters = (props) => (
+  <div>
+    <input type="text" value={props.filters.text} onChange={(e) => {
+      props.dispatch(setTextFilter(e.target.value));
+    }} />
+  </div>
+);
+
+const mapStateToProps = (state) => {
+  return {
+    filters: state.filters
+  };
+};
+
+export default connect(mapStateToProps)(ExpenseListFilters);
