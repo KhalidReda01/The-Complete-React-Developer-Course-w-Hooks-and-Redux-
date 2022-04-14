a5a13705c3e61851be4cd7daa5ddfca0b0159887
khalidreda <khalidaziz2198@gmail.com>
2022-04-14 23:25:25 +0200

    DEES Regular Expressin WTV WVI is a must

--- 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/ExpenseFrom.js
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/ExpenseFrom.js
@@ -1,11 +1,30 @@
 import React from 'react'; 
- export default class Expense extends React.Component {
+export default class Expense extends React.Component {
+  state = {
+    description: '',
+    notes: '',
+    amount:''
+  }
+  onDescriptionChange = (e) => {
+    const description = e.target.value 
+    this.setState(()=>({description}))
+  }
+  onNoteChange = (e) => {
+    const note = e.target.value
+    this.setState(()=>({note}))
+  }
+  onAmountChange = (e) => {
+    const amount = e.target.value;
+    // if (amount.match(/^\d*(\.\d{0,2}?$/)){
+    //   this.setState(()=>({amount}))
+    // }
+  }
   render() {
     return (
       <div>
         <form>
-          <input type='text' placeholder='Description' autoFocus />
-          <input type='number' placeholder='Amount' />
+          <input type='text' placeholder='Description' autoFocus value={this.state.description} onChange={this.onDescriptionChange} />
+          <input type='number' placeholder='Amount' value={this.state.amount} onChange={this.onAmountChange }/>
           <textarea placeholder='Add a note for your expense (otional)'>
           
           </textarea>
