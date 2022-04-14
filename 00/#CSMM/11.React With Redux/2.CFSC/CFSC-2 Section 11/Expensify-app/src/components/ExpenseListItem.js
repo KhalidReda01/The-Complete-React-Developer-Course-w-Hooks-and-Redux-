484c3d04c887de74557dee6d65a9fcb57421d8b3
khalidreda <khalidaziz2198@gmail.com>
2022-04-14 21:44:18 +0200

    I can't deny that this part is little confusing

--- 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/ExpenseListItem.js
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/ExpenseListItem.js
@@ -2,15 +2,24 @@
 // Export a statelss functional componant
 // Description,amount,CreatedAt
 import React from 'react';
-import { connect } from 'react-redux';
-
-const ExpenseListItem = ({dispatch,id,description,amount,createdAt}) => (
+// import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
+const ExpenseListItem = (props) => (
   <div>
-    <h3>{description}</h3>
-    <p>{amount} - {createdAt}</p>
+    {/* {dispatch,id,description,amount,createdAt} */}
+    {/* <h3>{description}</h3>
+    <p>{amount} - {createdAt}</p> */}
     <button onClick={() => {
-      dispatch(removeExpense({id}))
+      console.log(props)
+      // dispatch(removeExpense({id}))
     }} >Remove</button>
   </div>
 )
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
+/*{
+  "id": "cbe88d69-c072-4e2c-a63c-8e1d697fae33",
+  "description": "Water bill",
+  "note": "",
+  "amount": 0,
+  "createdAt": 0
+}*/
