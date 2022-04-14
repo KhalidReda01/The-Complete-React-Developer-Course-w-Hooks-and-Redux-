0dd79241894ac80d4e9b30bea8dd047abfc8bb4c
khalidreda <khalidaziz2198@gmail.com>
2022-04-14 22:00:31 +0200

    I was right dispatch comes from react-redux library connect I told  you the read the react-redux docs is a must

--- 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/ExpenseListItem.js
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/ExpenseListItem.js
@@ -13,7 +13,8 @@ const ExpenseListItem = ({dispatch,id,description,amount,createdAt}) => (
     }} >Remove</button>
   </div>
 )
-export default connect()(ExpenseListItem);
+// export default connect()(ExpenseListItem);
+export default ExpenseListItem;
 /**
  * Okay after I seey the pros I didn't find the dispatch like the other pross so where did it come from HOCC 
  * 
