0a4c9af6811a8a91a9ee2107ea176db803734935
khalidreda <khalidaziz2198@gmail.com>
2022-04-14 21:36:22 +0200

    DEES done

--- 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/ExpenseListFilters.js
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/ExpenseListFilters.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { setTextFilter } from '../actions/filters';
 const ExpenseListFilters = (props) => (
   <div>
     <input type='text' value={props.filters.text} onChange={(e) => {
-      // console.log(props)
+      console.log(props)
       props.dispatch(setTextFilter(e.target.value))
       console.log(e.target.value)
     }} />
