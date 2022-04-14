070173093dbe8308e1de95bcb485f31312c54851
khalidreda <khalidaziz2198@gmail.com>
2022-04-14 21:47:18 +0200

    Keep going and to fully understand it you have to CAP All section at one session to fully aborb it

--- 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/ExpenseListItem.js
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/ExpenseListItem.js
@@ -2,24 +2,15 @@
 // Export a statelss functional componant
 // Description,amount,CreatedAt
 import React from 'react';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 import { removeExpense } from '../actions/expenses';
-const ExpenseListItem = (props) => (
+const ExpenseListItem = ({dispatch,id,description,amount,createdAt}) => (
   <div>
-    {/* {dispatch,id,description,amount,createdAt} */}
-    {/* <h3>{description}</h3>
-    <p>{amount} - {createdAt}</p> */}
+    <h3>{description}</h3>
+    <p>{amount} - {createdAt}</p>
     <button onClick={() => {
-      console.log(props)
-      // dispatch(removeExpense({id}))
+      dispatch(removeExpense({id}))
     }} >Remove</button>
   </div>
 )
-export default ExpenseListItem;
-/*{
-  "id": "cbe88d69-c072-4e2c-a63c-8e1d697fae33",
-  "description": "Water bill",
-  "note": "",
-  "amount": 0,
-  "createdAt": 0
-}*/
+export default connect()(ExpenseListItem);
