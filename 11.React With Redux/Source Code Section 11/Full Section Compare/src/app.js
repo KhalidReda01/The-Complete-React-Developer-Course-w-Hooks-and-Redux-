1d7d13f4467531d6b047010b552f4e2cd6f7b737
khalidreda <khalidaziz2198@gmail.com>
2022-04-15 00:28:56 +0200

    Lecture 5 Rendering Individual Expenses

--- 11.React With Redux/Source Code Section 11/Full Section Compare/src/app.js
+++ 11.React With Redux/Source Code Section 11/Full Section Compare/src/app.js
@@ -11,12 +11,12 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' }));
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
 store.dispatch(addExpense({ description: 'Gas bill' }));
 store.dispatch(setTextFilter('water'));
 
 setTimeout(() => {
-  store.dispatch(setTextFilter('rent'));
+  store.dispatch(setTextFilter('bill'));
 }, 3000)
 
 const state = store.getState();
