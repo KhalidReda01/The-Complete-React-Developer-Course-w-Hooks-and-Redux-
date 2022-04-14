6c8edcf7067263d670498adaa6bddf66e4717144
khalidreda <khalidaziz2198@gmail.com>
2022-04-15 00:31:49 +0200

    Lecture 7 Dropdown for Picking SortBy

--- 11.React With Redux/Source Code Section 11/Full Section Compare/src/app.js
+++ 11.React With Redux/Source Code Section 11/Full Section Compare/src/app.js
@@ -12,12 +12,8 @@ import './styles/styles.scss';
 const store = configureStore();
 
 store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(setTextFilter('water'));
-
-setTimeout(() => {
-  store.dispatch(setTextFilter('bill'));
-}, 3000)
+store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
