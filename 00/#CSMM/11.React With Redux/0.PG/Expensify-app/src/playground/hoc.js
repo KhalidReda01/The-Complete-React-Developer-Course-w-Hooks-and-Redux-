df47828e7d2dca50df06c2a1d9050cb694072e41
khalidreda <khalidaziz2198@gmail.com>
2022-04-15 17:59:05 +0200

    Okay this part Full U

--- 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/playground/hoc.js
+++ 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/playground/hoc.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 const Info = (props) => (
   <div>
+    {console.log(props)}
     <h1>Info</h1>
     <p>The info is :{props.info}</p>
   </div>
 )
-ReactDOM.render(<Info info="There are the datails"/>,document.getElementById('app'))
+const withAdminWarning = (WrappedComponent) => {
+  return () => (
+    <div>
+    <p>This is private info . Please don't share !</p>
+      <WrappedComponent />
+    </div>
+  )
+}
+const AdminInfo=withAdminWarning(Info)
+ReactDOM.render(<AdminInfo info="There are the datails"/>,document.getElementById('app'))
