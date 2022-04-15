84df8aa77a7d58b93ede4164d41c0b0d047e16f0
khalidreda <khalidaziz2198@gmail.com>
2022-04-15 14:31:53 +0200

    DEES 107/10 Wiring up ADD Expense CFSC done but the app as you see I told you before just CFSC without WVI and WTV  will not be an effective way of study specially with the hard parts at this course

--- 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/AddExpensePage.js
+++ 00/#CSMM/11.React With Redux/2.CFSC/CFSC-2 Section 11/Expensify-app/src/components/AddExpensePage.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Expense from './ExpenseFrom';
 // import Expense from './ExpenseFrom';
-import ExpenseForm from './ExpenseFrom'
-const AddExpensePage = () => (
+import ExpenseForm from './ExpenseFrom';
+import { addExpense } from '../actions/expenses';
+const AddExpensePage = (props) => (
   <div>
     <h1>Add Expense</h1>
     <ExpenseForm 
       onSubmit={(expense) => {
+        props.dispatch(addExpense(expense))
+        props.history.push('./')
         console.log(expense)
       }}
     />
   </div>
 );
 
-export default AddExpensePage;
+export default connect()(AddExpensePage) ;
