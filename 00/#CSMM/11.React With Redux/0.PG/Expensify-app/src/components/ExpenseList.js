54957efab8cc23f55228d236c0ae4d04196e0042
khalidreda <khalidaziz2198@gmail.com>
2022-04-16 08:46:23 +0200

    Deleted what I did ant start again Lecture 102.5 Rendering Indvidual Expenses

--- 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseList.js
+++ 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseList.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
-import ExpenseListItem from "./ExpenseListItem";
-import selectExpenses from '../selectors/expenses';
 const ExpenseList = (props) => (
   <div>
     <h1>Expense List</h1>
-    {props.expenses.map((expense) => {
-      return <ExpenseListItem key={expense.id} {...expense}/>
-   })}
+    {props.expenses.length}
+    {props.filters.text}
   </div>
 )
 const mapStateToProps=(state) => {
   return {
-    expenses: selectExpenses(state.expenses,state.filters)
+    expenses: state.expenses,
+    filters:state.filters
 } 
 }
 export default  connect(mapStateToProps)(ExpenseList);
--- 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseListItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const ExpenseListItem = (props) => (
-  <div>
-    {console.log(props)}
-    <h3>{props.description}</h3>
-    <p>{props.amount} - {props.createdAt}</p>
-  </div>
-)
-export default ExpenseListItem
