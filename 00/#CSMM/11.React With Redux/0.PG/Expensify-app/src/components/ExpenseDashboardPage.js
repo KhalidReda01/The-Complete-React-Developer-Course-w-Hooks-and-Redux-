cbb5c2f42418858df086f99c564530311ce5e183
khalidreda <khalidaziz2198@gmail.com>
2022-04-16 10:06:58 +0200

    Deleted what I did and start again

--- 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseDashboardPage.js
+++ 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseDashboardPage.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
-import ExpenseListFilters from './ExpenseListFilters';
 const ExpenseDashboardPage = () => (
   <div>
-    <ExpenseListFilters/>
   <ExpenseList/>
   </div>
 );
--- 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseListItem.js
+++ 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseListItem.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { removeExpense } from '../actions/expenses';
-const ExpenseListItem = ({dispatch,id,description,amount,createdAt}) => (
+const ExpenseListItem = ({description,amount,createdAt}) => (
   <div>
     <h3>{description}</h3>
     <p>{amount} - {createdAt}</p>
-    <button onClick={() => {
-      console.log({ id })
-      dispatch(removeExpense({id}))
-    }} >Remove</button>
   </div>
 )
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
