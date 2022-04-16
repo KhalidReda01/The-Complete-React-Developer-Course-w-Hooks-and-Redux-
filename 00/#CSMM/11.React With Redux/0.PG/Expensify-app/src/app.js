026974fb6d3732a55bd1a4a281a4b9c2bc532a3e
khalidreda <khalidaziz2198@gmail.com>
2022-04-16 07:41:04 +0200

    cfsc review done for lecture 104/4 Connecting store and component with React-Redux

--- 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/app.js
+++ 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/app.js
@@ -18,7 +18,9 @@ store.dispatch(setTextFilter('water'));
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
-
+setTimeout((() => {
+  store.dispatch(setTextFilter('rent'))
+}),3000)
 const jsx = (
   <Provider store={store}>
     <AppRouter/>
