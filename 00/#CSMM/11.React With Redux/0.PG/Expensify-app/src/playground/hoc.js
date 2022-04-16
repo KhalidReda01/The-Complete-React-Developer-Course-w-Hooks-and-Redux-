83b6bdfc3ed87e4d7a6ee30fc58c12b1fc4589ef
khalidreda <khalidaziz2198@gmail.com>
2022-04-16 03:47:05 +0200

    1st Ct

---
+++ 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/playground/hoc.js
@@ -0,0 +1,10 @@
+// Higher Order Component (HOC) - A component (HOC ) that renders a nother component
+import React from 'react';
+import ReactDOM from 'react-dom';
+const Info = (props) => (
+  <div>
+    <h1>Info</h1>
+    <p>The info is :{props.info}</p>
+  </div>
+)
+ReactDOM.render(<Info info="There are the deatils"/>,document.getElementById('app'))
