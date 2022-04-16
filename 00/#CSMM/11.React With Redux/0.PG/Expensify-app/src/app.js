8842a9047b8ae447c44fd3b5db3fb77f0b5b671a
khalidreda <khalidaziz2198@gmail.com>
2022-04-16 07:16:57 +0200

    3rd

--- 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/app.js
+++ 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/app.js
@@ -14,7 +14,9 @@ const store = configureStore();
 store.dispatch(addExpense({ description: 'Water bill' }));
 store.dispatch(addExpense({ description: 'Gas bill' }));
 store.dispatch(setTextFilter('water'));
-
+setTimeout((() => {
+  store.dispatch(setTextFilter('rent'))
+}),3000)
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
--- 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseList.js
+++ 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseList.js
@@ -4,13 +4,15 @@ const ExpenseList = (props) => (
   <div>
     <h1>Expense List</h1>
     {props.expenses.length}
+    {props.filters.text}
     {console.log(props)}
   </div>
 )
 const ConnectedExpenseList = connect((state) => {
   console.log(state)
   return {
-    expenses:state.expenses
+    expenses: state.expenses,
+    filters:state.filters
   }
 })(ExpenseList)
 export default ConnectedExpenseList
