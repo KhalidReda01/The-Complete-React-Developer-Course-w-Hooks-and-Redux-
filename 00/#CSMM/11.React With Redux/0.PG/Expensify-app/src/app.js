c4151467f57ec989784e19172f716fb4c0518f58
khalidreda <khalidaziz2198@gmail.com>
2022-04-16 08:32:44 +0200

    Lecture 102 CFSC done review

--- 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/app.js
+++ 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/app.js
@@ -15,7 +15,7 @@ store.dispatch(addExpense({ description: 'Water bill',amount:4500,createdAt:1 })
 store.dispatch(addExpense({ description: 'Gas bill' }));
 store.dispatch(setTextFilter('water'));
 setTimeout((() => {
-  store.dispatch(setTextFilter('rent'))
+  store.dispatch(setTextFilter('bill'))
 }),3000)
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
--- 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseList.js
+++ 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseList.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
 import ExpenseListItem from "./ExpenseListItem";
+import selectedExpenses from '../selectors/expenses';
 const ExpenseList = (props) => (
   <div>
     <h1>Expense List</h1>
+    {console.log('test')}
+    {console.log(props)}
     {props.expenses.map((expense) => {
       {/* console.log(expense) */}
       return <ExpenseListItem key={expense.id }{...expense}/>
@@ -12,8 +15,7 @@ const ExpenseList = (props) => (
 )
 const mapStateToProps=(state) => {
   return {
-    expenses: state.expenses,
-    filters:state.filters
+    expenses:selectedExpenses(state.expenses,state.filters)
 } 
 }
 export default  connect(mapStateToProps)(ExpenseList);
