16fa068c82ec35a02e63de9116f466d5276be326
khalidreda <khalidaziz2198@gmail.com>
2022-04-16 07:52:43 +0200

    DEES

--- 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseList.js
+++ 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseList.js
@@ -1,12 +1,14 @@
-import React from "react";
-import { connect } from "react-redux";
-const ExpenseList = (props) => (
-  <div>
-    <h1>Expense List</h1>
-    {props.name}
-  </div>
-)
-const ConnectedExpenseList = connect((state) => {
- name:'kahlid'
-})(ExpenseList)
-export default ConnectedExpenseList;
+// import React from "react";
+// import { connect } from "react-redux";
+// const ExpenseList = (props) => (
+//   <div>
+//     <h1>Expense List</h1>
+//     {/* {props.name} */}
+//   </div>
+// )
+// const ConnectedExpenseList = connect((state) => {
+//   return {
+//   name:'khalid'
+// } 
+// })(ExpenseList)
+// export default ConnectedExpenseList;
