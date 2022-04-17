5978a2e172bbb005c9f28cba91ee773c96cb8b82
khalidreda <khalidaziz2198@gmail.com>
2022-04-17 11:14:10 +0200

    Hidden bug before I started this repo means that the pg one is the right and working fine as I did yesterday so Understand the concept at this repo and start clean

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseListFilters.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseListFilters.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter } from '../actions/filters';
+import { setTextFilter,sortByDate,sortByAmount } from '../actions/filters';
 const ExpenseListFilters = (props) => (
   <div>
     {console.log('hellio')}
@@ -9,13 +9,24 @@ const ExpenseListFilters = (props) => (
       props.dispatch(setTextFilter(e.target.value))
       console.log(e.target.value)
     }} />
-    <select>
+    <select
+      value={props.filters.sortBy}
+      onChange={(e) => {
+        if (e.target.value === 'date') {
+          props.dispatch(sortByDate())
+        } else if (e.target.value === 'amount') {
+          props.dispatch(sortByAmount())
+        }
+      }}
+    
+    >
       <option value='date'>Date</option>
       <option value='amount'>Amount</option>
     </select>
     
   </div>
 )
+//setup value and onChange for select
 const mapStateToProps = (state) => {
   console.log('this is HOCC')
   console.log(state)
