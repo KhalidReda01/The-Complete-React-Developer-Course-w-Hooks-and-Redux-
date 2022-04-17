bbaf05de778ffeb7270c710a22b30a0a390f55ae
khalidreda <khalidaziz2198@gmail.com>
2022-04-17 13:17:02 +0200

    Challenge Solved U100%

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseForm.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseForm.js
@@ -4,12 +4,19 @@ import React from 'react';
 // setup onChange and value for textArea
 export default class ExpenseForm extends React.Component{
   state = {
-    description: ''
+    description: '',
+    note: ''
 
   };
   onDescriptionChange = (e) => {
     const description = e.target.value;
     this.setState(()=>({description}))
+  }
+  onNoteChange = (e) => {
+    const note = e.target.value;
+    // e.persist()
+    // this.setState(()=>({note:e.target.value}))
+    this.setState(()=>({note}))
   }
   render() {
     return (
@@ -28,7 +35,9 @@ export default class ExpenseForm extends React.Component{
            placeholder='Amount' 
 
           />
-          <textarea placeholder='Add a not for your expense(Optional)'
+          <textarea placeholder='Add a note for your expense(Optional)'
+            value={this.state.note}
+            onChange={this.onNoteChange}
           > 
            
           </textarea>
