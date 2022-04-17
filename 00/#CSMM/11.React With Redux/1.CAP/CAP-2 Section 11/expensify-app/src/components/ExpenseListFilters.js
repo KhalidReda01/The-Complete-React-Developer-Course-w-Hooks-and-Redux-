eebc7bdadadb878942de9758966991697dad45e5
khalidreda <khalidaziz2198@gmail.com>
2022-04-17 11:08:37 +0200

    Challenge STC

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseListFilters.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseListFilters.js
@@ -8,7 +8,11 @@ const ExpenseListFilters = (props) => (
     <input type='text' value={props.filters.text} onChange={(e) => {
       props.dispatch(setTextFilter(e.target.value))
       console.log(e.target.value)
-    } }/>
+    }} />
+    <select>
+      <option value='date'>Date</option>
+      <option value='amount'>Amount</option>
+    </select>
     
   </div>
 )
