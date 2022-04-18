693744543281b9ba38757220069725cd41b219fd
khalidreda <khalidaziz2198@gmail.com>
2022-04-18 14:20:13 +0200

    No problem before the reg now WTV again why not

---
+++ 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseForm.js
@@ -0,0 +1,43 @@
+import React from 'react';
+export default class ExpenseForm extends React.Component{
+  state = {
+    description: 'test',
+    amount: '',
+    note: '',
+    
+  }
+  onDescriptionChange = (e) => {
+    const description = e.target.value
+    this.setState(()=>({description}))
+  }
+  onNoteChange = (e) => {
+    const note = e.target.value
+    this.setState(()=>({note}))
+  }
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            type='text'
+            placeholder='Description'
+            autoFocus
+            value={this.state.description}
+            onChange={this.onDescriptionChange}
+
+          />
+          <input
+            type='number'
+            placeholder='amount'
+          />
+          <textarea
+            placeholder='Add a note for your expense (optional)'
+            value={this.state.note}
+            onChange={this.onNoteChange}
+          ></textarea>
+          <button>Add Expense</button>
+        </form>
+      </div>
+    )
+  }
+}
