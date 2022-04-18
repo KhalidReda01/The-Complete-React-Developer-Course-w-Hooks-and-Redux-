ab40997f57a873360f75f9d0df136a9336d0bbdc
khalidreda <khalidaziz2198@gmail.com>
2022-04-18 12:58:54 +0200

    deleted what I did and recode again as practice

--- 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseListFilters.js
+++ 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseListFilters.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter, sortByAmount, sortByDate } from '../actions/filters';
+import { setTextFilter} from '../actions/filters';
 
 const ExpenseListFilters = (props) => (
   <div>
@@ -8,20 +8,7 @@ const ExpenseListFilters = (props) => (
       props.dispatch(setTextFilter(e.target.value))
       console.log(e.target.value)
     }} />
-    <select
-      value={props.filters.sortBy}
-      onChange={(e) => {
-        if (e.target.value === 'date') {
-          props.dispatch(sortByDate())
-        } else if (e.target.value === 'amount') {
-          props.dispatch(sortByAmount())
-        }
-      }}
-    
-    >
-      <option value='date'>Date</option>
-      <option value='amount'>Amount</option>
-    </select>
+  
   </div>
 )
 const mapStateToProps = (state) => {
