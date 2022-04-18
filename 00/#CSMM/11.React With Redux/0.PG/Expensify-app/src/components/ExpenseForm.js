78c1d246a4ec7645e23fe66a8399b8b6a3bb0b05
khalidreda <khalidaziz2198@gmail.com>
2022-04-18 14:05:11 +0200

    Lecture done with regrex101 105/8 Creating Expense Add/Edit Form

--- 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseForm.js
+++ 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseForm.js
@@ -13,6 +13,12 @@ export default class ExpenseForm extends React.Component{
     const note = e.target.value;
     this.setState(()=>({note}))
   }
+  onAmountchange = (e) => {
+    const amount = e.target.value;
+    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+      this.setState(()=>({amount}))
+    }
+  }
   render() {
     return (
       <div>
@@ -24,7 +30,11 @@ export default class ExpenseForm extends React.Component{
             value={this.state.description}
             onChange={this.onDescriptionChange}
             />
-          <input type='number' placeholder='Amount' />
+          <input type='text'
+            placeholder='Amount'
+            value={this.state.amount}
+            onChange={this.onAmountchange}
+          />
           <textarea
             placeholder='Add a note for your expense(optional)'
             value={this.state.note}
