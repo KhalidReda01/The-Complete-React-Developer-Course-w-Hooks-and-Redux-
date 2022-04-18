1e2da96719bf9db18e5736693ba8ffe56bd18757
khalidreda <khalidaziz2198@gmail.com>
2022-04-18 13:06:24 +0200

    104/7 Dropdown for picking sortBy CFSC

--- 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseListFilters.js
+++ 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseListFilters.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFilter} from '../actions/filters';
+import { setTextFilter, sortByAmount, sortByDate} from '../actions/filters';
 
 const ExpenseListFilters = (props) => (
   <div>
@@ -8,6 +8,19 @@ const ExpenseListFilters = (props) => (
       props.dispatch(setTextFilter(e.target.value))
       console.log(e.target.value)
     }} />
+    <select value={props.filters.sortBy}
+      onChange={(e) => {
+        if (e.target.value === 'date') {
+        props.dispatch(sortByDate())
+        } else if (e.target.value === 'amount') {
+          props.dispatch(sortByAmount())
+      }
+    }}
+    
+    >
+      <option value='date'> Date</option>
+      <option value='amount'>Amount</option>
+    </select>
   
   </div>
 )
