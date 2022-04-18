b5536982ff78492da148f5b6c844637ad4bd2866
khalidreda <khalidaziz2198@gmail.com>
2022-04-18 07:03:01 +0200

    Challenge Done  I didn't STC Cool

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseListItem.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseListItem.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { removeExpense } from '../actions/expenses';
 
 
 const ExpenseListItem = ({dispatch,id,description,amount,createdAt}) => (
   <div>
-    <h3>{description}</h3>
+    <Link to={`edit/${id}`}>
+       <h3>{description}</h3>
+    </Link>
+   
     <p>{amount} - {createdAt}</p>
     <button onClick={() => {
       dispatch(removeExpense({id}))
