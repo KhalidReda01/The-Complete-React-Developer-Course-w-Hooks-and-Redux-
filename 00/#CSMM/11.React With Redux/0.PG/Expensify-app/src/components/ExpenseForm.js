de07d44b327543c9387ba33a71aaf91ee322ad0a
khalidreda <khalidaziz2198@gmail.com>
2022-04-18 13:53:44 +0200

    Challenge Done not HOCC this time at all It's  good

--- 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseForm.js
+++ 00/#CSMM/11.React With Redux/0.PG/Expensify-app/src/components/ExpenseForm.js
@@ -1,12 +1,35 @@
 import React from 'react';
 export default class ExpenseForm extends React.Component{
+  state = {
+    description: '',
+    note: '',
+    amount:''
+  }
+  onDescriptionChange = (e) => {
+    const description = e.target.value;
+    this.setState(()=>({description}))
+  }
+  onNoteChange = (e) => {
+    const note = e.target.value;
+    this.setState(()=>({note}))
+  }
   render() {
     return (
       <div>
         <form>
-          <input type='text' placeholder='Description' autoFocus />
+          <input
+            type='text'
+            placeholder='Description'
+            autoFocus 
+            value={this.state.description}
+            onChange={this.onDescriptionChange}
+            />
           <input type='number' placeholder='Amount' />
-          <textarea placeholder='Add a note for your expense(optional)'></textarea>
+          <textarea
+            placeholder='Add a note for your expense(optional)'
+            value={this.state.note}
+            onChange={this.onNoteChange}
+          ></textarea>
           <button>Add Expense</button>
         </form>
       </div>
