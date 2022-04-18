3ece81c6aa8f7e750574053c9f91d57b7144d7b7
khalidreda <khalidaziz2198@gmail.com>
2022-04-18 07:30:20 +0200

    challenge Solved Done

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/EditExpensePage.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/EditExpensePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { editExpense } from '../actions/expenses';
 import ExpenseForm from './ExpenseForm';
 const EditExpensePage = (props) => {
   console.log(props);
@@ -9,9 +10,11 @@ const EditExpensePage = (props) => {
         expense={props.expense}
         onSubmit={(expense) => {
           // Challenge Time
-          // Dispatch the action to edit the expense 
+          // Dispatch the action to edit the expense
           //Redirect to the dashboard
-          console.log('updated',expense)
+          props.dispatch(editExpense(props.expense.id,expense))
+          console.log('updated', expense)
+          props.history.push('/')
         }}
 
       />
