d42fe9f78b78026bbc4a888325ee7933d02f6578
khalidreda <khalidaziz2198@gmail.com>
2022-04-18 07:27:30 +0200

    Challenge Time 13m

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseForm.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseForm.js
@@ -9,10 +9,10 @@ export default class ExpenseForm extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-    description:  '',
-    note: '',
-    amount: '',
-    createdAt: moment(),
+    description:props.expense?props.expense.description:  '',
+    note:props.expense?props.expense.note: '',
+    amount:props.expense?(props.expense.amount/100).toString(): '',
+    createdAt:props.expense?moment(props.expense.createdAt): moment(),
     calenderFocused: false,
     error:''
 
