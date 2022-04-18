7efe4f69c6dca2124b2e4d363d707113e7089fdb
khalidreda <khalidaziz2198@gmail.com>
2022-04-18 10:23:30 +0200

    7m

--- 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseListFilters.js
+++ 00/#CSMM/11.React With Redux/1.CAP/CAP-2 Section 11/expensify-app/src/components/ExpenseListFilters.js
@@ -1,28 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { DateRangePicker } from 'react-dates';
 import { setTextFilter,sortByDate,sortByAmount } from '../actions/filters';
 
-const ExpenseListFilters = (props) => (
-  <div>
-    <input type='text' value={props.filters.text} onChange={(e) => {
-      props.dispatch(setTextFilter(e.target.value))
-      console.log(e.target.value)
-    }} />
-    <select
-      value={props.filters.sortBy}
-      onChange={(e) => {
-        if (e.target.value === 'date') {
-          props.dispatch(sortByDate())
-        } else if (e.target.value === 'amount') {
-          props.dispatch(sortByAmount())
-        }
-      }}
-    >
-      <option value='date'>Date</option>
-      <option value='amount'>Amount</option>
-    </select>
-  </div>
-)
+class ExpenseListFilters extends React.Component{
+  state = {
+    calenderFocused:null
+  }
+  render() {
+    return (
+      <div>
+        <input type='text' value={this.props.filters.text} onChange={(e) => {
+          this.props.dispatch(setTextFilter(e.target.value))
+          console.log(e.target.value)
+        }} />
+        <select
+          value={this.props.filters.sortBy}
+          onChange={(e) => {
+            if (e.target.value === 'date') {
+              this.props.dispatch(sortByDate())
+            } else if (e.target.value === 'amount') {
+              this.props.dispatch(sortByAmount())
+            }
+          }}
+        >
+          <option value='date'>Date</option>
+          <option value='amount'>Amount</option>
+        </select>
+        <DateRangePicker 
+          startDate={this.props.filters.startDate}
+      />
+      </div>
+    )
+  }
+
+}
+
 const mapStateToProps = (state) => {
   return {
     filters:state.filters
