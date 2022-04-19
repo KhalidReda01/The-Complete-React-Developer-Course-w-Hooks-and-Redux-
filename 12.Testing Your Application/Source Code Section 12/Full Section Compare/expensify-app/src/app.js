1654867710f59805bb8cecf4f3a72e488aeaee4e
khalidreda <khalidaziz2198@gmail.com>
2022-04-19 06:11:01 +0200

    Lecture 121/11 Mocking Libraries with Jest

--- 12.Testing Your Application/Source Code Section 12/Full Section Compare/expensify-app/src/app.js
+++ 12.Testing Your Application/Source Code Section 12/Full Section Compare/expensify-app/src/app.js
@@ -8,6 +8,7 @@ import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
--- 12.Testing Your Application/Source Code Section 12/Full Section Compare/expensify-app/src/components/ExpenseForm.js
+++ 12.Testing Your Application/Source Code Section 12/Full Section Compare/expensify-app/src/components/ExpenseForm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import moment from 'moment';
 import { SingleDatePicker } from 'react-dates';
-import 'react-dates/lib/css/_datepicker.css';
 
 export default class ExpenseForm extends React.Component {
   constructor(props) {
