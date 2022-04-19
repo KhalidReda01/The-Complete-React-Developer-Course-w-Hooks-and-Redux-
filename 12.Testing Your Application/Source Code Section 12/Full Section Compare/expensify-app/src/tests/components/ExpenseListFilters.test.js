6da8d381d92e420da1166429ec35b8dfd438c862
khalidreda <khalidaziz2198@gmail.com>
2022-04-19 06:17:15 +0200

    Lecture 127/17 last lecture at section 12 Testing YOur Application :Testing ExpenseListFilters II

--- 12.Testing Your Application/Source Code Section 12/Full Section Compare/expensify-app/src/tests/components/ExpenseListFilters.test.js
+++ 12.Testing Your Application/Source Code Section 12/Full Section Compare/expensify-app/src/tests/components/ExpenseListFilters.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import moment from 'moment';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
 import { filters, altFilters } from '../fixtures/filters';
 
@@ -32,4 +33,45 @@ test('should render ExpenseListFilters with alt data correctly', () => {
     filters: altFilters
   });
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should handle text change', () => {
+  const value = 'rent';
+  wrapper.find('input').simulate('change', {
+    target: { value }
+  });
+  expect(setTextFilter).toHaveBeenLastCalledWith(value);
+});
+
+test('should sort by date', () => {
+  const value = 'date';
+  wrapper.setProps({
+    filters: altFilters
+  });
+  wrapper.find('select').simulate('change', {
+    target: { value }
+  });
+  expect(sortByDate).toHaveBeenCalled();
+});
+
+test('should sort by amount', () => {
+  const value = 'amount';
+  wrapper.find('select').simulate('change', {
+    target: { value }
+  });
+  expect(sortByAmount).toHaveBeenCalled();
+});
+
+test('should handle date changes', () => {
+  const startDate = moment(0).add(4, 'years');
+  const endDate = moment(0).add(8, 'years');
+  wrapper.find('DateRangePicker').prop('onDatesChange')({ startDate, endDate });
+  expect(setStartDate).toHaveBeenLastCalledWith(startDate);
+  expect(setEndDate).toHaveBeenLastCalledWith(endDate);
+});
+
+test('hould handle date focus changes', () => {
+  const calendarFocused = 'endDate';
+  wrapper.find('DateRangePicker').prop('onFocusChange')(calendarFocused);
+  expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
 });
