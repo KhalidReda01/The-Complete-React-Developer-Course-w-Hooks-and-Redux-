adb69d7df3a656b2a470a401278728d488413048
khalidreda <khalidaziz2198@gmail.com>
2022-04-19 06:09:18 +0200

    Lecture 119/9 Enzyme

--- 12.Testing Your Application/Source Code Section 12/Full Section Compare/expensify-app/src/tests/components/Header.test.js
+++ 12.Testing Your Application/Source Code Section 12/Full Section Compare/expensify-app/src/tests/components/Header.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
 });
