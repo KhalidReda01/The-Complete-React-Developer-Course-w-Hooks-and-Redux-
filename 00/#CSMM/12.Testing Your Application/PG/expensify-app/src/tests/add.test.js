ded18190fb75a71db9f7f521548ac8184040f7f0
khalidreda <khalidaziz2198@gmail.com>
2022-04-19 10:07:16 +0200

    Before use assertions library

--- 00/#CSMM/12.Testing Your Application/PG/expensify-app/src/tests/add.test.js
+++ 00/#CSMM/12.Testing Your Application/PG/expensify-app/src/tests/add.test.js
@@ -1,4 +1,7 @@
-const add = (a, b) => a + b;
+const add = (a, b) => a + b+1;
 test('should add two numbers', () => {
-  
+  const result = add(3, 4);
+  if (result !== 7) {
+    throw new Error(`You added 4 and 3. The result was ${result}. Expect 7`)
+  }
 });
