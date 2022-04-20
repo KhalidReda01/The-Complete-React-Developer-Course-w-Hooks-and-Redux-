43f41336785571466ba6d63e48e55acd05651116
khalidreda <khalidaziz2198@gmail.com>
2022-04-20 10:53:51 +0200

    sort by amout suit case now the little hard one but I think it easy too let's see what will happen

--- 12.Testing Your Application/CAP Section 12/114.4 Testing Filters Action Generators/expensify-app/src/tests/actions.js/filters.test.js
+++ 12.Testing Your Application/CAP Section 12/114.4 Testing Filters Action Generators/expensify-app/src/tests/actions.js/filters.test.js
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import { setStartDate, setEndDate, sortByDate } from "../../actions/filters";
+import { setStartDate, setEndDate, sortByDate, sortByAmount } from "../../actions/filters";
 
 test('should generate set start date action object', () => {
   const action = setStartDate(moment(0));
@@ -20,5 +20,11 @@ test('should  generate sort by date action object', () => {
   const action = sortByDate();
   expect(action).toEqual({
     type:'SORT_BY_DATE'
+  })
+})
+test('shoud generate sort by amount action object', () => {
+  const action = sortByAmount();
+  expect(action).toEqual({
+    type:'SORT_BY_AMOUNT'
   })
 })
