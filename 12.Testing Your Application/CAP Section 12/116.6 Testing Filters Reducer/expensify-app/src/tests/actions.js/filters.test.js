cc4c45240d2ee5d878f15870ab028e3c8f01609c
khalidreda <khalidaziz2198@gmail.com>
2022-04-20 11:54:53 +0200

    Not clean code I mean the start already have hidden bug

---
+++ 12.Testing Your Application/CAP Section 12/116.6 Testing Filters Reducer/expensify-app/src/tests/actions.js/filters.test.js
@@ -0,0 +1,48 @@
+import moment from 'moment';
+import { setStartDate, setEndDate, sortByDate, sortByAmount, setTextFilter } from "../../actions/filters";
+
+test('should generate set start date action object', () => {
+  const action = setStartDate(moment(0));
+  expect(action).toEqual({
+    type: 'SET_START_DATE',
+    startDate:moment(0)
+  })
+});
+test('should generate set end date action object', () => {
+  const action = setEndDate(moment(0));
+  expect(action).toEqual({
+    type: 'SET_END_DATE',
+    endDate:moment(0)
+  })
+})
+// Challenge Time
+test('should generate set text filter action object with provided text value', () => {
+  const text = 'bill';
+  const action = setTextFilter(text);
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text:'bill'
+  })
+  
+})
+
+test('should generate set text filter action object with default value', () => {
+  const action = setTextFilter();
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text:''
+  })
+})
+
+test('should  generate sort by date action object', () => {
+  const action = sortByDate();
+  expect(action).toEqual({
+    type:'SORT_BY_DATE'
+  })
+})
+test('shoud generate sort by amount action object', () => {
+  const action = sortByAmount();
+  expect(action).toEqual({
+    type:'SORT_BY_AMOUNT'
+  })
+})
