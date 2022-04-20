0352dde1e6cef671691c640e5e57f1de7a963516
khalidreda <khalidaziz2198@gmail.com>
2022-04-20 11:38:30 +0200

    2nd part 11m but It's harder than the previous lecture so CFSC later will be very important

--- 12.Testing Your Application/CAP Section 12/115.5 Testing Expenses Selectors/expensify-app/src/tests/selectors/expenses.text.js
+++ 12.Testing Your Application/CAP Section 12/115.5 Testing Expenses Selectors/expensify-app/src/tests/selectors/expenses.text.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import selectExpenses from '../../selectors/expenses';
 
 const expenses = [{
@@ -11,13 +12,13 @@ const expenses = [{
   description: 'Rent',
   note: '',
   amount: 109500,
-  createdAt:-1000
+  createdAt:moment(0).subtract(4,'days').valueOf()
 },{
   id: '3',
   description: 'Credit Card',
   note: '',
   amount: 4500,
-  createdAt:1000
+  createdAt:moment(0).add(4,'days').valueOf()
 }]
 
 test('should filter by text value', () => {
@@ -29,4 +30,14 @@ test('should filter by text value', () => {
   }
   const result = selectExpenses(expenses, filters);
   expect(result).toEqual([expenses[2],expenses[1]])
+})
+test('should filter by startDAte', () => {
+  const filters = {
+    text: '',
+    sortBy: 'date',
+    startDate: moment(0),
+    endDate:undefined
+  }
+  const result = selectExpenses(expenses, filters);
+  expect(result).toEqual([expenses[2],expenses[0]])
 })
