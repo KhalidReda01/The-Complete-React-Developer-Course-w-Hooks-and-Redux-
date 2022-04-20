07978c371d2ce66e4f5a34ea4126780100479785
khalidreda <khalidaziz2198@gmail.com>
2022-04-20 08:41:45 +0200

    First challenge at section 12

--- 00/#CSMM/12.Testing Your Application/PG/expensify-app/src/tests/add.test.js
+++ 00/#CSMM/12.Testing Your Application/PG/expensify-app/src/tests/add.test.js
@@ -1,4 +1,6 @@
 const add = (a, b) => a + b;
+// Challenge Time
+const generateGreeting =(name)=>`Hello ${name}!`
 test('should add two numbers',()=> {
   const result = add(3, 4);
   expect(result).toBe(7)
