89162e9e772d889cf8b964e14442c9f8474c11ab
khalidreda <khalidaziz2198@gmail.com>
2022-04-20 08:46:39 +0200

    Lecture 112.2 Setting up Jest U-100% CFSC

--- 00/#CSMM/12.Testing Your Application/PG/expensify-app/src/tests/add.test.js
+++ 00/#CSMM/12.Testing Your Application/PG/expensify-app/src/tests/add.test.js
@@ -1,13 +1,17 @@
 const add = (a, b) => a + b;
 // Challenge Time
-const generateGreeting =(name)=>`Hello ${name}!`
+const generateGreeting =(name='Anonymous')=>`Hello ${name}!`
 test('should add two numbers',()=> {
   const result = add(3, 4);
   expect(result).toBe(7)
 
 })
 
-test('Shoud generate Greeting ', () => {
+test('Shoud generate Greeting for name ', () => {
   const result = generateGreeting('Khalid')
   expect(result).toBe("Hello Khalid!")
 })
+test('should generate Greeting for no name', () => {
+  const result = generateGreeting();
+  expect(result).toBe("Hello Anonymous!")
+}) 
