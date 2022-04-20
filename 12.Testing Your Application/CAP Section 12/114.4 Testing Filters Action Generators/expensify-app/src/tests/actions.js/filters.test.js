15e5c934ed5c6ee2e6bd30f2b1a6598a12c06b60
khalidreda <khalidaziz2198@gmail.com>
2022-04-20 10:51:15 +0200

    first test case sort by date easy one and sorty by amount will be the same

--- 12.Testing Your Application/CAP Section 12/114.4 Testing Filters Action Generators/expensify-app/src/tests/actions.js/filters.test.js
+++ 12.Testing Your Application/CAP Section 12/114.4 Testing Filters Action Generators/expensify-app/src/tests/actions.js/filters.test.js
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import { setStartDate, setEndDate } from "../../actions/filters";
+import { setStartDate, setEndDate, sortByDate } from "../../actions/filters";
 
 test('should generate set start date action object', () => {
   const action = setStartDate(moment(0));
@@ -16,3 +16,9 @@ test('should generate set end date action object', () => {
   })
 })
 // Challenge Time
+test('should  generate sort by date action object', () => {
+  const action = sortByDate();
+  expect(action).toEqual({
+    type:'SORT_BY_DATE'
+  })
+})
