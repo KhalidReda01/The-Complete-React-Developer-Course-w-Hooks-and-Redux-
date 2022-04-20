bcbce15b6ef507d4c287d75db5b4c35c016cd119
khalidreda <khalidaziz2198@gmail.com>
2022-04-20 08:39:13 +0200

    3rd part 12 Setting up jest

--- 00/#CSMM/12.Testing Your Application/PG/expensify-app/src/tests/add.test.js
+++ 00/#CSMM/12.Testing Your Application/PG/expensify-app/src/tests/add.test.js
@@ -1,8 +1,6 @@
-const add = (a, b) => a + b+1;
+const add = (a, b) => a + b;
 test('should add two numbers',()=> {
   const result = add(3, 4);
-  if (result !== 7) {
-    throw new Error(`You added 4 and 3 . the result ${result}. Expect 7`)
-  }
+  expect(result).toBe(7)
 
 })
