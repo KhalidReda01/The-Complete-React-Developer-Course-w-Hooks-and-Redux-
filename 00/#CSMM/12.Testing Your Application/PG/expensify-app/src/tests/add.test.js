9bfe68d8ed64a2f5b2c729caf1a9f7b05b8001ab
khalidreda <khalidaziz2198@gmail.com>
2022-04-20 09:06:26 +0200

    Solved challenge I think this isn't bad for this lecture It's the easiest one at this section

--- 00/#CSMM/12.Testing Your Application/PG/expensify-app/src/tests/add.test.js
+++ 00/#CSMM/12.Testing Your Application/PG/expensify-app/src/tests/add.test.js
@@ -1,7 +1,10 @@
-const add = (a, b) => a + b+1;
+const add = (a, b) => a + b;
+const generateGreeting=(name)=>`Hello ${name}!`
 test('should add two numbers', () => {
   const result = add(3, 4);
-  if (result !== 7) {
-    throw new Error(`You add 4 and 3 . The result was ${result}. Expect 7`)
-  }
+  expect(result).toBe(7)
+})
+test('should generate greeting for name', () => {
+  const result = generateGreeting('Khalid')
+  expect(result).toBe('Hello Khalid!')
 })
