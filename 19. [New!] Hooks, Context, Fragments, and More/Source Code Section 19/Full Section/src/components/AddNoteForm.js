9000fea58183cc1f4803030a58634d10bc1c0979
khalidreda <khalidaziz2198@gmail.com>
2022-05-02 02:12:19 +0200

    Lecture 200 Creating  Customn Hooks

--- 19. [New!] Hooks, Context, Fragments, and More/Source Code Section 19/Full Section/src/components/AddNoteForm.js
+++ 19. [New!] Hooks, Context, Fragments, and More/Source Code Section 19/Full Section/src/components/AddNoteForm.js
@@ -1,10 +1,12 @@
 import React, { useState, useContext } from 'react'
 import NotesContext from '../context/notes-context'
+import useMousePosition from '../hooks/useMousePosition'
 
 const AddNoteForm = () => {
     const { dispatch } = useContext(NotesContext)
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
+    const position = useMousePosition()
 
     const addNote = (e) => {
         e.preventDefault()
@@ -19,7 +21,7 @@ const AddNoteForm = () => {
 
     return (
         <>
-            <p>Add note</p>
+            <p>Add note {position.x} - {position.y}</p>
             <form onSubmit={addNote}>
                 <input value={title} onChange={(e) => setTitle(e.target.value)} />
                 <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
